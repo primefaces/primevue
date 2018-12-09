f8edaffc7fe92e7cc773ee1e57bdfbf2f217af7d
cagataycivici <cagatay.civici@gmail.com>
2018-12-09 23:25:26 +0300

    Toggleable Panel

--- src/components/panel/Panel.vue
+++ src/components/panel/Panel.vue
@@ -4,12 +4,17 @@
             <slot name="header">
                 <span class="p-panel-title" v-if="header">{{header}}</span>
             </slot>
+            <a v-if="toggleable" tabindex="0" class="p-panel-titlebar-icon p-panel-titlebar-toggler" @click="toggle">
+                <span :class="{'pi pi-minus': !d_collapsed, 'pi pi-plus': d_collapsed}"></span>
+            </a>
         </div>
-        <div class="p-panel-content-wrapper">
-            <div class="p-panel-content">
-                <slot></slot>
+        <transition name="p-panel-content-wrapper">
+            <div class="p-panel-content-wrapper" v-show="!d_collapsed">
+                <div class="p-panel-content">
+                    <slot></slot>
+                </div>
             </div>
-        </div>
+        </transition>
     </div>
 </template>
 
@@ -17,8 +22,19 @@
 export default {
     props: {
         header: String,
-        toggleable: Boolean
-    }
+        toggleable: Boolean,
+        collapsed: Boolean
+    },
+    data() {
+        return {
+           d_collapsed: this.collapsed
+        }
+    },
+    methods: {
+        toggle() {
+            this.d_collapsed = !this.d_collapsed;
+        }
+    } 
 }
 </script>
 
--- src/views/panel/PanelDemo.vue
+++ src/views/panel/PanelDemo.vue
@@ -8,6 +8,7 @@
         </div>
 
         <div class="content-section implementation">
+            <h3 class="first">Regular</h3>
             <p-panel header="Godfather I">
                 The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
                 His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
@@ -15,7 +16,8 @@
                 kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
             </p-panel>
 
-            <p-panel header="Godfather I" style="margin-top:2em" :toggleable="true">
+            <h3>Toggleable</h3>
+            <p-panel header="Godfather I" :toggleable="true">
                 The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
                 His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
                 Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
