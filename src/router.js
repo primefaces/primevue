0149aba5cc188463665f8a39940d281cc980f4ba
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-12-14 17:37:28 +0300

    Typo fixing

--- src/router.js
+++ src/router.js
@@ -39,7 +39,7 @@ export default new Router({
     {
         path: '/listbox',
         name: 'listbox',
-        component: () => import('./views/listbox/ListboxDemo.vue')
+        component: () => import('./views/listbox/ListBoxDemo.vue')
     },
     {
         path: '/panel',
