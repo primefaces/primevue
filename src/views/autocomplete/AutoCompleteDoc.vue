83933a6753c88898515d7dbae275949262350c6f
cagataycivici <cagatay.civici@gmail.com>
2019-05-22 16:39:24 +0300

    Code format

--- src/views/autocomplete/AutoCompleteDoc.vue
+++ src/views/autocomplete/AutoCompleteDoc.vue
@@ -209,40 +209,40 @@ export default {
 				<div class="doc-tablewrapper">
 					<table class="doc-table">
 						<thead>
-						<tr>
-							<th>Name</th>
-							<th>Element</th>
-						</tr>
+                            <tr>
+                                <th>Name</th>
+                                <th>Element</th>
+                            </tr>
 						</thead>
 						<tbody>
-						<tr>
-							<td>p-autocomplete</td>
-							<td>Container element</td>
-						</tr>
-						<tr>
-							<td>p-autocomplete-panel</td>
-							<td>Overlay panel of suggestions.</td>
-						</tr>
-						<tr>
-							<td>p-autocomplete-items</td>
-							<td>List container of suggestions.</td>
-						</tr>
-						<tr>
-							<td>p-autocomplete-item</td>
-							<td>List item of a suggestion.</td>
-						</tr>
-						<tr>
-							<td>p-autocomplete-token</td>
-							<td>Element of a selected item in multiple mode.</td>
-						</tr>
-						<tr>
-							<td>p-autocomplete-token-icon</td>
-							<td>Close icon element of a selected item in multiple mode.</td>
-						</tr>
-						<tr>
-							<td>p-autocomplete-token-label</td>
-							<td>Label of a selected item in multiple mode.</td>
-						</tr>
+                            <tr>
+                                <td>p-autocomplete</td>
+                                <td>Container element</td>
+                            </tr>
+                            <tr>
+                                <td>p-autocomplete-panel</td>
+                                <td>Overlay panel of suggestions.</td>
+                            </tr>
+                            <tr>
+                                <td>p-autocomplete-items</td>
+                                <td>List container of suggestions.</td>
+                            </tr>
+                            <tr>
+                                <td>p-autocomplete-item</td>
+                                <td>List item of a suggestion.</td>
+                            </tr>
+                            <tr>
+                                <td>p-autocomplete-token</td>
+                                <td>Element of a selected item in multiple mode.</td>
+                            </tr>
+                            <tr>
+                                <td>p-autocomplete-token-icon</td>
+                                <td>Close icon element of a selected item in multiple mode.</td>
+                            </tr>
+                            <tr>
+                                <td>p-autocomplete-token-label</td>
+                                <td>Label of a selected item in multiple mode.</td>
+                            </tr>
 						</tbody>
 					</table>
 				</div>
