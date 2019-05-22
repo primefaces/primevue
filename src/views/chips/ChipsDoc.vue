602e06ef9a54688d2090daf52b93d774c331c0df
cagataycivici <cagatay.civici@gmail.com>
2019-05-22 17:33:43 +0300

    Doc update for props

--- src/views/chips/ChipsDoc.vue
+++ src/views/chips/ChipsDoc.vue
@@ -29,6 +29,7 @@ import Chips from 'primevue/chips';
 </CodeHighlight>
 
 				<h3>Properties</h3>
+                <p>Any attribute such as style and class are passed to the main container element. Following are the additional properties to configure the component.</p>
 				<div class="doc-tablewrapper">
 					<table class="doc-table">
 						<thead>
