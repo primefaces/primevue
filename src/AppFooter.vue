1406a23407f759e7c4836cefe41c48891b0615ec
cagataycivici <cagatay.civici@gmail.com>
2019-05-23 17:30:26 +0300

    Add vue config

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content-section layout-footer clearfix">
-        <span>Released under the MIT License,  Copyright © 2018 <a href="http://www.primetek.com.tr" target="_blank" rel="noopener noreferrer">PrimeTek</a></span>
+        <span>PrimeVUE 1.0.0-beta.1</span>
         <div class="footer-links">
             <a href="https://github.com/primefaces/primevue"><i class=" icon-github fa fa-github-square"></i></a>
             <a href="https://twitter.com/primevue"><i class="icon-twitter fa fa-twitter-square"></i></a>
