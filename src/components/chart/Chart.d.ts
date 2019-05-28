3109a591366082043a5de9ff1b99b2b8ed2e6930
mertsincan <sincan.mert@gmail.com>
2019-05-28 13:37:51 +0300

    Created Chart.d.ts

---
+++ src/components/chart/Chart.d.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+
+export declare class Chart extends Vue {
+    type?: string;
+    data?: object;
+    options?: object;
+    width?: number;
+    height?: number;
+    $emit(eventName: 'select', e: {originalEvent: Event, element: any, dataset: any}): this;
+}
