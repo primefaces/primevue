1f0ffa5f64071223d3aeec6b945d16c12cb753a1
mertsincan <sincan.mert@gmail.com>
2019-05-28 13:43:12 +0300

    Created Checkbox.d.ts

---
+++ src/components/checkbox/Checkbox.d.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+export declare class Checkbox extends Vue {
+    value?: null;
+    modelValue?: null;
+    binary?: boolean;
+    $emit(eventName: 'click', event: Event): this;
+    $emit(eventName: 'input', value: any): this;
+    $emit(eventName: 'change', event: Event): this;
+    $emit(eventName: 'focus', event: Event): this;
+    $emit(eventName: 'blur', event: Event): this;
+}
