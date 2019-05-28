d603613e0e8c8789b2005776bd0a0e1c677e5624
mertsincan <sincan.mert@gmail.com>
2019-05-28 14:08:32 +0300

    Created DataView.d.ts

---
+++ src/components/dataview/DataView.d.ts
@@ -0,0 +1,29 @@
+import Vue, { VNode } from 'vue';
+
+export declare class DataView extends Vue {
+    value?: any[];
+    layout?: string;
+    rows?: number;
+    first?: number;
+    totalRecords?: number;
+    paginator?: boolean;
+    paginatorPosition?: string;
+    alwaysShowPaginator?: boolean;
+    paginatorTemplate?: string;
+    pageLinkSize?: number;
+    rowsPerPageOptions?: number[];
+    currentPageReportTemplate?: string;
+    emptyMessage?: string;
+    sortField?: string;
+    sortOrder?: number;
+    lazy?: boolean;
+    $emit(eventName: 'page', event: Event): this;
+    $slots: {
+        header: VNode[];
+        paginatorLeft: VNode[];
+        paginatorRight: VNode[];
+        list: VNode[];
+        grid: VNode[];
+        footer: VNode[];
+    }
+}
