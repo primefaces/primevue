58bae69fbc28b8f8db86d255246d02434315f4e4
mertsincan <sincan.mert@gmail.com>
2019-05-28 15:12:03 +0300

    Created Dialog.d.ts

---
+++ src/components/dialog/Dialog.d.ts
@@ -0,0 +1,21 @@
+import Vue, {VNode} from 'vue';
+
+export declare class Dialog extends Vue {
+    header?: any;
+    footer?: any;
+    visible?: boolean;
+    modal?: boolean;
+    contentStyle?: string;
+    rtl?: boolean;
+    closable?: boolean;
+    showHeader?: boolean;
+    baseZIndex?: number;
+    autoZIndex?: boolean;
+    $emit(eventName: 'show'): this;
+    $emit(eventName: 'hide'): this;
+    $slots: {
+        '': VNode[];
+        header: VNode[];
+        footer: VNode[];
+    }
+}
