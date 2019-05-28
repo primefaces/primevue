6f5992fc400b0be536c3a16d6710ff79e3ea6b70
mertsincan <sincan.mert@gmail.com>
2019-05-28 13:19:15 +0300

    Cosmetics

--- src/components/accordion/Accordion.d.ts
+++ src/components/accordion/Accordion.d.ts
@@ -2,8 +2,8 @@ import Vue, { VNode } from 'vue';
 
 export declare class Accordion extends Vue {
     multiple?: boolean;
-    $emit(eventName: "tab-open", e: {originalEvent: Event, tab: any}): this;
-    $emit(eventName: "tab-close", e: {originalEvent: Event, tab: any}): this;
+    $emit(eventName: 'tab-open', e: {originalEvent: Event, tab: any}): this;
+    $emit(eventName: 'tab-close', e: {originalEvent: Event, tab: any}): this;
     $slots: {
         '': VNode[];
     }
