bdde45ac1d0ca937dcc4975edd26dd226293b0a3
mertsincan <sincan.mert@gmail.com>
2019-05-29 09:29:05 +0300

    Created Editor.d.ts

---
+++ src/components/editor/Editor.d.ts
@@ -0,0 +1,14 @@
+import Vue, { VNode } from 'vue';
+
+export declare class Editor extends Vue {
+    value?: string;
+    placeholder?: string;
+    readonly?: boolean;
+    formats?: any[];
+    editorStyle?: string;
+    $emit(eventName: 'input', event: string): this;
+    $emit(eventName: 'text-change', e: { htmlValue: string, textValue: any, delta: any, source: string}): this;
+    $slot: {
+        toolbar: VNode[];
+    }
+}
