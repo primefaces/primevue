1328c4ab55e0447d5fd38a4bb211a2e0d29b21a7
mertsincan <sincan.mert@gmail.com>
2019-05-29 12:12:40 +0300

    Created Menu.d.ts

---
+++ src/components/menu/Menu.d.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+
+export declare class Menu extends Vue {
+    popup?: boolean;
+    model?: any[];
+    appendTo?: string;
+    autoZIndex?: boolean;
+    baseZIndex?: number;
+}
