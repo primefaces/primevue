1bee06f912f4cef184bc56d3e04fdb224fcaaef8
mertsincan <sincan.mert@gmail.com>
2019-05-29 12:08:24 +0300

    Created Listbox.d.ts

---
+++ src/components/listbox/Listbox.d.ts
@@ -0,0 +1,20 @@
+import Vue, { VNode } from 'vue';
+
+export declare class Listbox extends Vue {
+    value?: any;
+    options?: any[];
+    optionLabel?: string;
+    optionValue?: any;
+    optionDisabled?: boolean;
+    listStyle?: string;
+    disabled?: boolean;
+    dataKey?: string;
+    multiple?: boolean;
+    metaKeySelection?: boolean;
+    filter?: boolean;
+    $emit(eventName: 'input', value: any): this;
+    $emit(eventName: 'change', e: { originalEvent: Event, value: any }): this;
+    $slots: {
+        option: VNode[];
+    }
+}
