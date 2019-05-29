9a4b95534d71726e81bf0efecc09e852d5cbd8cc
mertsincan <sincan.mert@gmail.com>
2019-05-29 11:59:16 +0300

    Created InputSwitch.d.ts

---
+++ src/components/inputswitch/InputSwitch.d.ts
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+export declare class InputSwitch extends Vue {
+    value?: boolean;
+    inputId?: string;
+    name?: string;
+    disabled?: boolean;
+    $emit(eventName: 'click', event: Event): this;
+    $emit(eventName: 'input', value: any): this;
+    $emit(eventName: 'change', event: Event): this;
+    $emit(eventName: 'focus', event: Event): this;
+    $emit(eventName: 'blur', event: Event): this;
+}
