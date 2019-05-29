ac6292adb370d83ed0cf6e51d5ec38544f0d981c
mertsincan <sincan.mert@gmail.com>
2019-05-29 13:53:16 +0300

    Created SelectButton.d.ts

---
+++ src/components/selectbutton/SelectButton.d.ts
@@ -0,0 +1,18 @@
+import Vue, { VNode } from 'vue';
+
+export declare class SelectButton extends Vue {
+    value?: any;
+    options?: any[];
+    optionLabel?: string;
+    optionValue?: any;
+    optionDisabled?: boolean;
+    multiple?: boolean;
+    disabled?: boolean;
+    dataKey?: string;
+    $emit(eventName: 'input', value: any): this;
+    $emit(eventName: 'focus', event: Event): this;
+    $emit(eventName: 'blur', event: Event): this;
+    $slots: {
+        option: VNode[];
+    }
+}
