d5a662e3c1900310f28b89579cee1852f35813ed
mertsincan <sincan.mert@gmail.com>
2019-05-29 13:09:39 +0300

    Created RadioButton.d.ts

---
+++ src/components/radiobutton/RadioButton.d.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+export declare class RadioButton extends Vue {
+    value: any;
+    modelValue: any;
+    $emit(eventName: 'click', event: Event): this;
+    $emit(eventName: 'input', value: any): this;
+    $emit(eventName: 'change', event: Event): this;
+    $emit(eventName: 'focus', event: Event): this;
+    $emit(eventName: 'blur', event: Event): this;
+}
