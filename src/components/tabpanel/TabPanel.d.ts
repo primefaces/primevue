64f6c8a4c1ec3c310d6b664c30239676dabdde8e
mertsincan <sincan.mert@gmail.com>
2019-05-29 14:09:27 +0300

    Created TabPanel.d.ts

---
+++ src/components/tabpanel/TabPanel.d.ts
@@ -0,0 +1,10 @@
+import Vue, { VNode } from 'vue';
+
+export declare class TabPanel extends Vue {
+    header?: any;
+    active?: boolean;
+    disabled?: boolean;
+    $slots: {
+        '': VNode[];
+    }
+}
