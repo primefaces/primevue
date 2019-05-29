cd2f39661831b51f552d3704bb029e259ae2bc70
mertsincan <sincan.mert@gmail.com>
2019-05-29 12:33:31 +0300

    Created MultiSelect.d.ts

---
+++ src/components/multiselect/MultiSelect.d.ts
@@ -0,0 +1,22 @@
+import Vue, {VNode} from 'vue';
+
+export declare class MultiSelect extends Vue {
+    value?: any;
+    options?: any[];
+    optionLabel?: string;
+    optionValue?: any;
+    optionDisabled?: boolean;
+    scrollHeight?: string;
+    placeholder?: string;
+    disabled?: boolean;
+    filter?: boolean;
+    tabindex?: string;
+    dataKey?: string;
+    filterPlaceholder?: string;
+    $emit(eventName: 'input', value: any): this;
+    $emit(eventName: 'change', e: {originalEvent: Event, value: any}): this;
+    $slots: {
+        value: VNode[];
+        option: VNode[];
+    }
+}
