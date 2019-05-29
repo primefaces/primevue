79db8ce211983cefa7033284c5d40046c51bba37
mertsincan <sincan.mert@gmail.com>
2019-05-29 14:17:58 +0300

    Created ToggleButton.d.ts

---
+++ src/components/togglebutton/ToggleButton.d.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+
+export declare class ToggleButton extends Vue {
+    value?: boolean;
+    onIcon?: string;
+    offIcon?: string;
+    onLabel?: string;
+    offLabel?: string;
+    inputId?: string;
+    name?: string;
+    iconPos?: string;
+    disabled?: boolean;
+    $emit(eventName: 'click', event: Event): this;
+    $emit(eventName: 'input', value: any): this;
+    $emit(eventName: 'change', event: Event): this;
+    $emit(eventName: 'focus', event: Event): this;
+    $emit(eventName: 'blur', event: Event): this;
+}
