81cb6e5475b3d8fa972bcdf25ed6ffd6d1c04035
mertsincan <sincan.mert@gmail.com>
2019-05-30 11:27:21 +0300

    Created ToastService.d.ts

---
+++ src/components/toast/ToastService.d.ts
@@ -0,0 +1,15 @@
+import Vue, { PluginFunction } from 'vue';
+
+export const install: PluginFunction<{}>;
+
+interface ToastServiceMethods {
+  add(message: any): any;
+  removeGroup(group: any): void;
+  removeAllGroups(): void;
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $toast: ToastServiceMethods;
+  }
+}
