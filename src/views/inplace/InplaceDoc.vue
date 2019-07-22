0eb71a8bd6f62460328ce6f5553b450270b7c10b
cagataycivici <cagatay.civici@gmail.com>
2019-07-22 14:12:31 +0300

    Link fix

--- src/views/inplace/InplaceDoc.vue
+++ src/views/inplace/InplaceDoc.vue
@@ -166,7 +166,7 @@ export default {
 			</TabPanel>
 
 			<TabPanel header="Source">
-				<a href="https://github.com/primefaces/primevue/tree/master/src/views/panel" class="btn-viewsource" target="_blank" rel="noopener noreferrer">
+				<a href="https://github.com/primefaces/primevue/tree/master/src/views/inplace" class="btn-viewsource" target="_blank" rel="noopener noreferrer">
 					<span>View on GitHub</span>
 				</a>
 <CodeHighlight>
