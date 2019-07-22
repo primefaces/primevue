c9d765b78d3dafb169eaaab911f44c539fc66905
cagataycivici <cagatay.civici@gmail.com>
2019-07-22 17:11:05 +0300

    Doc update

--- src/views/progressspinner/ProgressSpinnerDemo.vue
+++ src/views/progressspinner/ProgressSpinnerDemo.vue
@@ -14,8 +14,6 @@
             <h3>Custom</h3>
             <ProgressSpinner style="width:50px;height:50px" strokeWidth="8" fill="#EEEEEE" animationDuration=".5s"/>
         </div>
-
-        <ProgressSpinnerDoc/>
     </div>
 </template>
 
