c7572be841b9fce48189efb5d564a1f3442a1fdf
cagataycivici <cagatay.civici@gmail.com>
2019-07-28 16:53:11 +0300

    d ts for Org Chart

---
+++ src/components/organizationchart/OrganizationChart.d.ts
@@ -0,0 +1,16 @@
+import Vue, {VNode} from 'vue';
+
+export declare class OrderList extends Vue {
+    value?: any;
+    selectionKeys?: any;
+    selectionMode?: string;
+    collapsedKeys?: any;
+    collapsible?: boolean;
+    $emit(eventName: 'node-select', node: any): this;
+    $emit(eventName: 'node-unselect', node: any): this;
+    $emit(eventName: 'node-expand', node: any): this;
+    $emit(eventName: 'node-collapsed', node: any): this;
+    $slots: {
+        [key: string]: VNode[];
+    }
+}
