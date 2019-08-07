b6e91552bb5d2c7ea74f986f7fd520f7502eaeb0
cagataycivici <cagatay.civici@gmail.com>
2019-08-07 11:32:05 +0300

    Update submenu

--- src/views/treetable/TreeTableSubMenu.vue
+++ src/views/treetable/TreeTableSubMenu.vue
@@ -6,6 +6,11 @@
             <li><router-link to="/treetable/paginator">&#9679; Paginator</router-link></li>
             <li><router-link to="/treetable/sort">&#9679; Sort</router-link></li>
             <li><router-link to="/treetable/filter">&#9679; Filter</router-link></li>
+            <li><router-link to="/treetable/selection">&#9679; Selection</router-link></li>
+            <li><router-link to="/treetable/lazy">&#9679; Lazy</router-link></li>
+            <li><router-link to="/treetable/coltoggle">&#9679; ColToggle</router-link></li>
+            <li><router-link to="/treetable/resize">&#9679; Resize</router-link></li>
+            <li><router-link to="/treetable/responsive">&#9679; Responsive</router-link></li>
         </ul>
     </div>    
 </template>
