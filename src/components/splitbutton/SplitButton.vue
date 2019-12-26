5e2e72b7dfd8fb82c502ed5cff2590be0a68ccdf
cagataycivici <cagatay.civici@gmail.com>
2019-12-26 14:28:53 +0300

    Lint fixes

--- src/components/splitbutton/SplitButton.vue
+++ src/components/splitbutton/SplitButton.vue
@@ -1,7 +1,7 @@
 <template>
     <div ref="container" class="p-splitbutton p-buttonset p-component">
         <PVSButton type="button" :icon="icon" :label="label" @click="onClick" :disabled="disabled" :tabindex="tabindex" />
-        <PVSButton type="button" class="p-splitbutton-menubutton" icon="pi pi-caret-down" @click="onDropdownButtonClick" :disabled="disabled" 
+        <PVSButton type="button" class="p-splitbutton-menubutton" icon="pi pi-caret-down" @click="onDropdownButtonClick" :disabled="disabled"
             :aria-expanded="overlayVisible" aria-haspopup="true" :aria-owns="ariaId + '_overlay'"/>
         <transition name="p-input-overlay" @enter="onOverlayEnter" @leave="onOverlayLeave">
             <div id="ariaId + '_overlay'" ref="overlay" class="p-menu p-menu-dynamic p-component" v-if="overlayVisible">
