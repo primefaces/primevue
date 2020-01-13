11ff9e7a5c0b58ff163f6c5f20a4d69e2e4f45d5
cagataycivici <cagatay.civici@gmail.com>
2020-01-13 11:45:52 +0300

    Type file for ColorPicker

--- src/components/colorpicker/ColorPicker.d.ts
+++ src/components/colorpicker/ColorPicker.d.ts
@@ -1,5 +1,13 @@
 import Vue, { VNode } from 'vue';
 
 export declare class ColorPicker extends Vue {
-    value: any;
+    value?: any;
+    defaultColor?: any;
+    inline?: boolean;
+    format?: string;
+    disabled?: boolean;
+    tabindex?: string;
+    autoZIndex?: boolean;
+    baseZIndex?: number;
+    $emit(eventName: 'input', value: any): this;
 }
