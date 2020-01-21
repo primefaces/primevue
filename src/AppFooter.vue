0e4bad50857a8d7f454e68c51463bbab1895a174
cagataycivici <cagatay.civici@gmail.com>
2020-01-21 09:57:58 +0300

    Set version

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content-section layout-footer clearfix">
-        <span>PrimeVue 1.0.1-SNAPSHOT</span>
+        <span>PrimeVue 1.0.1</span>
         <div class="footer-links">
             <a href="https://github.com/primefaces/primevue"><i class=" icon-github fa fa-github-square"></i></a>
             <a href="https://twitter.com/primevue"><i class="icon-twitter fa fa-twitter-square"></i></a>
