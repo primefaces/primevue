539dc812db5adbaa0f98a872a05be72444a98149
cagataycivici <cagatay.civici@gmail.com>
2020-03-03 18:46:17 +0300

    Lint fix

--- src/views/datatable/DataTableDemo.vue
+++ src/views/datatable/DataTableDemo.vue
@@ -12,7 +12,7 @@
 		<div class="content-section implementation">
             <div class="p-card">
                 <div class="p-card-body" style="padding:0">
-                    <DataTable :value="customers" :paginator="true" class="p-datatable-responsive p-datatable-customers" :rows="10" 
+                    <DataTable :value="customers" :paginator="true" class="p-datatable-responsive p-datatable-customers" :rows="10"
                         dataKey="id" :rowHover="true" :selection.sync="selectedCustomers"
                         paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown" :rowsPerPageOptions="[10,25,50]">
                         <template #header>
