d859434b29f1ed1cf05dc090d6633db7111b3368
cagataycivici <cagatay.civici@gmail.com>
2020-03-15 21:50:13 +0300

    Update deployment path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/primevue' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/primevue/showcase/' : '/',
     productionSourceMap: false,
     configureWebpack: {
         output: {
