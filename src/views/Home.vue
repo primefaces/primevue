a49c2befd55c2f8887a656b898c483be29f2c9ed
cagataycivici <cagatay.civici@gmail.com>
2020-03-17 12:30:29 +0300

    Wording update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,16 @@
     <div class="home">
         <div class="introduction">
             <span class="introduction-promo"><img alt="Gold Sponsor" src="../assets/images/home/vue-gold.png" />
-                <span>Vue.js Gold Sponsor</span></span>
-            <h1>The Most Complete UI Framework</h1>
-            <h2>for VUE</h2>
+            <span>Vue.js Gold Sponsor</span></span>
+            <h1>The Ultimate UI Component Library</h1>
+            <h2>for Vue</h2>
 
-            <router-link to="/setup" class="home-button">Get Started</router-link>
+            <router-link to="/setup" class="home-button">GET STARTED</router-link>
         </div>
 
         <div class="features">
-            <h3>Why PrimeVue?</h3>
-            <p class="features-tagline">Congratulations! <span role="img" aria-label="celebrate">🎉</span> Your quest to find the UI library for Vue.js is complete.</p>
-
-            <p class="features-description">PrimeVue is a collection of rich UI components for Vue. All widgets are open source and free to use under MIT License. PrimeVue is developed by PrimeTek Informatics,
-                a vendor with years of expertise in developing open source UI solutions. For project news and updates, please <a href="https://twitter.com/primevue">follow us on twitter</a> and <a href="https://www.primefaces.org/category/primevue/">visit our blog</a>.</p>
+            <h3>Features</h3>
+            <p class="features-tagline">Congratulations! <span role="img" aria-label="celebrate">🎉</span> Your quest to find the UI library for Vue.js is now complete.</p>
 
             <div class="p-grid">
                 <div class="p-col-12 p-md-4">
@@ -22,7 +19,7 @@
                         <img alt="components" src="../assets/images/home/vue-components.png" />
                         <div class="feature-card-detail">
                             <span class="feature-name">50+ COMPONENTS</span>
-                            <p>The most complete set of native widgets featuring 50+ easy to use components for all your UI requirements.</p>
+                            <p>The ultimate set of UI Components to assist you with 50+ impressive Vue Components.</p>
                         </div>
                     </div>
                 </div>
@@ -31,7 +28,7 @@
                         <img alt="opensource" src="../assets/images/home/vue-opensource.png" />
                         <div class="feature-card-detail">
                             <span class="feature-name">OPEN SOURCE</span>
-                            <p>Hosted at <a href="https://github.com/primefaces/primevue">GitHub</a>, all widgets are open source and free to use under MIT license. Feel the power of open source.</p>
+                            <p>The complete UI Suite is free to based on the MIT license and the source code is available at GitHub.</p>
                         </div>
                     </div>
                 </div>
@@ -40,7 +37,7 @@
                         <img alt="themes" src="../assets/images/home/vue-themes.png" />
                         <div class="feature-card-detail">
                             <span class="feature-name">THEMES</span>
-                            <p>Don’t get tied up in just one look&feel. Choose from a variety of options including material and bootstrap design.</p>
+                            <p>Build on a design-agnostic infrastructure, choose from a vast amount of themes such as material, bootstrap, custom or develop your own.</p>
                         </div>
                     </div>
                 </div>
@@ -49,7 +46,7 @@
                         <img alt="templates" src="../assets/images/home/vue-templates.png" />
                         <div class="feature-card-detail">
                             <span class="feature-name">TEMPLATES</span>
-                            <p>Professionally designed highly customizable Vue application templates to get started in no time.</p>
+                            <p>Fascinating Vue-CLI application templates designed by professional designers and crafted by Vue Experts.</p>
                         </div>
                     </div>
                 </div>
@@ -58,7 +55,7 @@
                         <img alt="accesibility" src="../assets/images/home/vue-accesibility.png" />
                         <div class="feature-card-detail">
                             <span class="feature-name">ACCESSIBILITY</span>
-                            <p>Fully accessible and in compliance with Section 508 standards.</p>
+                            <p>UI Components for everyone with full support for Section 508 compliance.</p>
                         </div>
                     </div>
                 </div>
@@ -67,7 +64,7 @@
                         <img alt="pro" src="../assets/images/home/vue-pro.png" />
                         <div class="feature-card-detail">
                             <span class="feature-name">PRO SUPPORT</span>
-                            <p>With the exclusive services of Pro account, you no longer need to post your questions in the community forum and your issues to community issue tracker.</p>
+                            <p>Exceptional support service featuring response within 1 business day and option to request enhancements and new features for the library.</p>
                         </div>
                     </div>
                 </div>
@@ -76,7 +73,7 @@
                         <img alt="productivity" src="../assets/images/home/vue-productivity.png" />
                         <div class="feature-card-detail">
                             <span class="feature-name">PRODUCTIVITY</span>
-                            <p>Allocate your valuable time on business logic rather than dealing with the complex user interface requirements.</p>
+                            <p>Boost your productivity by achieving more in less time and accomplish amazing results.</p>
                         </div>
                     </div>
                 </div>
@@ -85,7 +82,7 @@
                         <img alt="community" src="../assets/images/home/vue-community.png" />
                         <div class="feature-card-detail">
                             <span class="feature-name">COMMUNITY</span>
-                            <p>Join PrimeVue community to become a part of an active, vibrant and growing open source foundation.</p>
+                            <p>Connect with the other open source community members, collaborate and have a voice in the project roadmap.</p>
                         </div>
                     </div>
                 </div>
@@ -94,7 +91,7 @@
                         <img alt="mobile" src="../assets/images/home/vue-mobile.png" />
                         <div class="feature-card-detail">
                             <span class="feature-name">MOBILE</span>
-                            <p>Enhanced mobile user experience with touch optimized responsive design elements.</p>
+                            <p>First class support for responsive design led by touch optimized elements.</p>
                         </div>
                     </div>
                 </div>
@@ -103,8 +100,7 @@
 
         <div class="whouses">
             <h3>Key Users</h3>
-            <p>Open source products of <a href="https://www.primetek.com.tr">PrimeTek</a> are used all around the world by 1M+ developers in Fortune 500 companies, corporations, government and educational
-                institutions.</p>
+            <p>The creator of PrimeVue is PrimeTek Informatics, the distinguished component library vendor who built popular open source projects such as PrimeFaces, PrimeNG and PrimeReact over the past years.</p>
             <div class="p-grid">
                 <div class="p-col-6 p-md-2"><img alt="airbus" src="../assets/images/home/airbus.svg"/></div>
                 <div class="p-col-6 p-md-2"><img alt="amex" src="../assets/images/home/amex.svg"/></div>
@@ -123,7 +119,7 @@
         <div class="templates">
 
             <h3>Premium Application Templates</h3>
-            <p>Powered by <a href="https://cli.vuejs.org">Vue-CLI</a>, create awesome applications in no time using the premium templates of PrimeVue and impress your users.</p>
+            <p>Gorgeous Vue-CLI application templates designed by professional designers and implemented by Vue Experts to help you reach your goals in no time.</p>
 
             <div class="p-grid">
                 <div class="p-col-12 p-md-4">
@@ -175,10 +171,10 @@
             <div class="p-grid">
                 <div className="p-col-12">
                     <img alt="PrimeReact Designer" src="../assets/images/primevue-designer.jpg" />
-                    <p><a href="https://www.primefaces.org/designer/primevue">Designer API</a> is a SASS based theme engine to create PrimeVue themes easily featuring over 500 variables,
-                        a demo application and a base sample theme. Whether you have your own style guide or just need a custom theme, Designer API is the right tool to design and bring them to existence.</p>
+                    <p>PrimeVue is a design-agnostic library and the theming system is based on the Theme Designer, the official tool to create themes for the components. Designer includes a SASS enabled infrastructure that has over 500 customizable variables, a live editor 
+                            and a Vue-CLI application to test your themes with ease along with built-in theme samples that are embedded in PrimeVue Core.</p>
 
-                    <p>Visit <a href="https://www.primefaces.org/designer/primevue">Designer API HomePage</a> for more information, live demos and access to a trial version.</p>
+                    <p>Visit <a href="https://www.primefaces.org/designer/primevue">Designer API HomePage</a> to learn more about the features and interact with the live editor.</p>
                 </div>
             </div>
         </div>
@@ -186,8 +182,8 @@
             <div class="p-grid">
                 <div class="p-col-12 p-md-6">
                     <h3>PrimeVue PRO Support</h3>
-                    <p>With PrimeVue PRO, it is easy to support, tune and add features to PrimeVue as if it were an in-house framework.</p>
-                    <p>PrimeVue PRO is a term based commercial support service. With the exclusive services of Pro account, you no longer need to post your questions in the community forum and your issues to community issue tracker.</p>
+                    <p>No time to wait for a response at forum or issue tracker? Then PrimeVue PRO Support is definitely for you.</p>
+                    <p>PrimeVUE PRO is an exceptional support service where you'll contact the PrimeTek via a private channel and receive assistance just in a couple of hours. Optionally PrimeVue PRO offers the ability to request changes and new features for the component library.</p>
                     <a class="home-button2" href="mailto:contact@primetek.com.tr">
                         Get a Quote
                     </a>
