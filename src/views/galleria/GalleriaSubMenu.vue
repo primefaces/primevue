47b497ff632eb7db3d086204b8a73d0660b2a31d
mertsincan <sincan.mert@gmail.com>
2020-03-31 16:30:59 +0300

    Added galleriaSubMenu for demo pages

---
+++ src/views/galleria/GalleriaSubMenu.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="content-section content-submenu p-clearfix">
+        <ul>
+            <li><router-link to="/galleria">&#9679; Documentation</router-link></li>
+            <li><router-link to="/galleria/basic">&#9679; Basic</router-link></li>
+            <li><router-link to="/galleria/indicator">&#9679; Indicator</router-link></li>
+            <li><router-link to="/galleria/thumbnail">&#9679; Thumbnail</router-link></li>
+            <li><router-link to="/galleria/preview">&#9679; Preview</router-link></li>
+            <li><router-link to="/galleria/responsive">&#9679; Responsive</router-link></li>
+            <li><router-link to="/galleria/fullscreen">&#9679; FullScreen</router-link></li>
+            <li><router-link to="/galleria/circular">&#9679; Circular</router-link></li>
+            <li><router-link to="/galleria/caption">&#9679; Caption</router-link></li>
+        </ul>
+    </div>
+</template>
