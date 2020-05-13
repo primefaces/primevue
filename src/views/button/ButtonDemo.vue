e3c4f4eea1a6591735d703147a45f5b97748dfed
cagataycivici <cagatay.civici@gmail.com>
2020-05-13 15:44:31 +0300

    Button demo cosmetics

--- src/views/button/ButtonDemo.vue
+++ src/views/button/ButtonDemo.vue
@@ -77,16 +77,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-button {
-    margin-right: .5em;
+.p-button {
+    margin-right: .5rem;
+}
+
+.p-buttonset {
+    .p-button {
+        margin-right: 0;
+    }
 }
 
 .sizes {
-    .p-button {
+    .button {
         margin-bottom: .5rem;
         display: block;
 
         &:last-child {
+            margin-bottom: 0;
+        }
+    }
+}
+
+@media screen and (max-width: 960px) {
+    .p-button {
+        margin-bottom: .5rem;
+
+        &:not(.p-button-icon-only) {
+            display: flex;
+            width: 100%;
+        }
+    }
+
+    .p-buttonset {
+        .p-button {
             margin-bottom: 0;
         }
     }
