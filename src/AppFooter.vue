93c82c73e5559deb2da43e8facff69ba947f36e8
cagataycivici <cagatay.civici@gmail.com>
2020-05-28 17:19:10 +0300

    Dev version

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="content-section layout-footer">
-        <span>PrimeVue 2.0.0-rc.1 by </span><a href="https://www.primetek.com.tr">PrimeTek</a>
+        <span>PrimeVue 2.0.0 by </span><a href="https://www.primetek.com.tr">PrimeTek</a>
     </div>
 </template>
