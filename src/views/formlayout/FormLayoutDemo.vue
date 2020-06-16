a0334bc43d1db05a5a9d547f090ca4b0445fe618
cagataycivici <cagatay.civici@gmail.com>
2020-06-16 23:37:35 +0300

    Add spacing

--- src/views/formlayout/FormLayoutDemo.vue
+++ src/views/formlayout/FormLayoutDemo.vue
@@ -50,13 +50,13 @@
             <h3>Horizontal and Fluid</h3>
             <div class="p-fluid">
                  <div class="p-field p-grid">
-                    <label for="firstname4" class="p-col-12 p-md-2">Firstname</label>
+                    <label for="firstname4" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">Firstname</label>
                     <div class="p-col-12 p-md-10">
                         <InputText id="firstname4" type="text" />
                     </div>
                 </div>
                 <div class="p-field p-grid">
-                    <label for="lastname4" class="p-col-12 p-md-2">Lastname</label>
+                    <label for="lastname4" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">Lastname</label>
                     <div class="p-col-12 p-md-10">
                         <InputText id="lastname4" type="text" />
                     </div>
