a8b20d274b1d10fc7755c68e4119c546ed7ffa7c
cagataycivici <cagatay.civici@gmail.com>
2020-07-06 12:01:43 +0300

    Update footer

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -1,5 +1,20 @@
 <template>
     <div class="content-section layout-footer">
-        <span>PrimeVue 2.0.0-rc.3 by </span><a href="https://www.primetek.com.tr">PrimeTek</a>
+        <div class="layout-footer-left">
+            <span>PrimeVue 2.0.0 by </span>
+            <a href="https://www.primetek.com.tr">PrimeTek</a>
+        </div>
+
+        <div class="layout-footer-right">
+            <a href="https://github.com/primefaces/primevue" class="p-mr-3">
+                <i class="pi pi-github"></i>
+            </a>
+            <a href="https://twitter.com/primevue">
+                <i class="pi pi-twitter"></i>
+            </a>
+        </div>
+        
+        
+
     </div>
 </template>
