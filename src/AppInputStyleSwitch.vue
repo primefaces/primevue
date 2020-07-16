0b983f6ce585e5e4119cdbe743a22b7f76ce582f
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-07-16 13:51:38 +0300

    Refactor on AppInputStyleSwitch

--- src/AppInputStyleSwitch.vue
+++ src/AppInputStyleSwitch.vue
@@ -2,17 +2,16 @@
     <div class="app-inputstyleswitch">
         <h4>Input Style</h4>
         <div class="p-formgroup-inline">
-        <div class="p-field-radiobutton">
-            <RadioButton id="input_outlined" name="inputstyle" value="outlined" :modelValue="value" @input="onChange" />
-            <label for="input_outlined">Outlined</label>
-        </div>
-        <div class="p-field-radiobutton">
-            <RadioButton id="input_filled" name="inputstyle" value="filled" :modelValue="value" @input="onChange" />
-            <label for="input_filled">Filled</label>
+            <div class="p-field-radiobutton">
+                <RadioButton id="input_outlined" name="inputstyle" value="outlined" :modelValue="value" @input="onChange" />
+                <label for="input_outlined">Outlined</label>
+            </div>
+            <div class="p-field-radiobutton">
+                <RadioButton id="input_filled" name="inputstyle" value="filled" :modelValue="value" @input="onChange" />
+                <label for="input_filled">Filled</label>
+            </div>
         </div>
     </div>
-    </div>
-
 </template>
 
 <script>
