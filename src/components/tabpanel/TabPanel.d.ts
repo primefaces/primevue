2902cc2b81b27e38b0a581c04c85ea35871d6586
Cagatay Civici <cagatay.civici@gmail.com>
2020-09-22 11:24:00 +0300

    Type update

--- src/components/tabpanel/TabPanel.d.ts
+++ src/components/tabpanel/TabPanel.d.ts
@@ -2,7 +2,6 @@ import Vue, { VNode } from 'vue';
 
 export declare class TabPanel extends Vue {
     header?: any;
-    active?: boolean;
     disabled?: boolean;
     $slots: {
         '': VNode[];
--- src/components/tabview/TabView.d.ts
+++ src/components/tabview/TabView.d.ts
@@ -1,6 +1,7 @@
 import Vue, { VNode } from 'vue';
 
 export declare class TabView extends Vue {
+    activeIndex?: number;
     $emit(eventName: 'tab-change', e: { originalEvent: Event, tab: any }): this;
     $slots: {
         '': VNode[];
