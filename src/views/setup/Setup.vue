63233f265ca93788c36c662b9a72c505b5bafeb0
Cagatay Civici <cagatay.civici@gmail.com>
2020-10-11 22:26:13 +0300

    Change severity

--- src/views/setup/Setup.vue
+++ src/views/setup/Setup.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="content-section documentation">
-            <Message severity="info">This guide is for Vue 3 and PrimeVue 3, visit <a href="https://www.primefaces.org/primevue/showcase-v2">PrimeVue 2.x website</a> if you are using Vue 2.</Message>
+            <Message severity="warn">This guide is for Vue 3 and PrimeVue 3, visit <a href="https://www.primefaces.org/primevue/showcase-v2">PrimeVue 2.x website</a> if you are using Vue 2.</Message>
 
             <h1>Setup</h1>
             <p>PrimeVue is a rich set of open source native components for Vue.</p>
