41074b0e7e97401f387f70c4df684d49c75792bf
Cagatay Civici <cagatay.civici@gmail.com>
2020-10-21 13:58:07 +0300

    Fixed #586 - Dropdown inside datable using showClear causing: Cannot read property 'classList' of null

--- src/components/utils/DomHandler.js
+++ src/components/utils/DomHandler.js
@@ -143,10 +143,14 @@ export default class DomHandler {
     }
 
     static hasClass(element, className) {
-        if (element.classList)
-            return element.classList.contains(className);
-        else
-            return new RegExp('(^| )' + className + '( |$)', 'gi').test(element.className);
+        if (element) {
+            if (element.classList)
+                return element.classList.contains(className);
+            else
+                return new RegExp('(^| )' + className + '( |$)', 'gi').test(element.className);
+        }
+        
+        return false;
     }
 
     static find(element, selector) {
