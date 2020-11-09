78edbc2795647aa2ffe4f02f4a3c6546f8fddc98
devilwacause <james@thejamestimes.com>
2020-11-09 15:18:24 -0600

    Update MultiSelect.vue
    Line 167 - added $refs to fix dropdown disappearing on item select

--- src/components/multiselect/MultiSelect.vue
+++ src/components/multiselect/MultiSelect.vue
@@ -167,7 +167,7 @@ export default {
             this.headerCheckboxFocused = false;
         },
         onClick(event) {
-            if (!this.disabled && (!this.overlay || !this.overlay.contains(event.target)) && !DomHandler.hasClass(event.target, 'p-multiselect-close')) {
+            if (!this.disabled && (!this.$refs.overlay || !this.$refs.overlay.contains(event.target)) && !DomHandler.hasClass(event.target, 'p-multiselect-close')) {
                 DomHandler.hasClass(event.target, 'p-multiselect-close');
                 if (this.overlayVisible)
                     this.hide();
