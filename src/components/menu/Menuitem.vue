3ddde8e9f96a9b837d7785ba852dbc4ad730afb6
makurtu <mateullull88@gmail.com>
2020-11-13 08:44:05 +0100

    Added dynamic text to labeltext on MenuItem

--- src/components/menu/Menuitem.vue
+++ src/components/menu/Menuitem.vue
@@ -3,12 +3,12 @@
         <router-link v-if="item.to && !item.disabled" :to="item.to" custom v-slot="{navigate, href}">
             <a :href="href" @click="onClick($event, navigate)" :class="linkClass" v-ripple role="menuitem">
                 <span :class="['p-menuitem-icon', item.icon]"></span>
-                <span class="p-menuitem-text">{{item.label}}</span>
+                <span class="p-menuitem-text">{{label()}}</span>
             </a>
         </router-link>
         <a v-else :href="item.url" :class="linkClass" @click="onClick" :target="item.target" role="menuitem" :tabindex="item.disabled ? null : '0'" v-ripple>
             <span :class="['p-menuitem-icon', item.icon]"></span>
-            <span class="p-menuitem-text">{{item.label}}</span>
+            <span class="p-menuitem-text">{{label()}}</span>
         </a>
     </li>
 </template>
@@ -28,6 +28,9 @@ export default {
                 item: this.item,
                 navigate: navigate
             });
+        },
+        label() {
+            return (typeof this.item.label === 'function' ? this.item.label() : this.item.label !== false);
         },
         visible() {
             return (typeof this.item.visible === 'function' ? this.item.visible() : this.item.visible !== false);
