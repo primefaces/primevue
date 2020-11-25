04bdc70afc324a03e168ada30f767eb213f5e3a8
Cagatay Civici <cagatay.civici@gmail.com>
2020-11-25 17:26:52 +0300

    Fixed #606 - DataTable multiple click RadioButton focus previous row

--- src/components/datatable/RowRadioButton.vue
+++ src/components/datatable/RowRadioButton.vue
@@ -1,9 +1,6 @@
 <template>
-    <div class="p-radiobutton p-component" @click="onClick">
-        <div class="p-hidden-accessible">
-           <input ref="input" type="radio" :checked="checked" @focus="onFocus($event)" @blur="onBlur($event)" :disabled="$attrs.disabled">
-        </div>
-        <div ref="box" :class="['p-radiobutton-box p-component', {'p-highlight': checked, 'p-disabled': disabled, 'p-focus': focused}]" role="radio" :aria-checked="checked">
+    <div class="p-radiobutton p-component" @click="onClick" tabindex="0" @focus="onFocus($event)" @blur="onBlur($event)">
+        <div ref="box" :class="['p-radiobutton-box p-component', {'p-highlight': checked, 'p-disabled': $attrs.disabled, 'p-focus': focused}]" role="radio" :aria-checked="checked">
             <div class="p-radiobutton-icon"></div>
         </div>
     </div>
@@ -15,7 +12,6 @@ export default {
     emits: ['change'],
     props: {
 		value: null,
-        disabled: null,
         checked: null
     },
     data() {
@@ -32,8 +28,6 @@ export default {
                         data: this.value
                     });
                 }
-
-                this.$refs.input.focus();
             }
         },
         onFocus() {
