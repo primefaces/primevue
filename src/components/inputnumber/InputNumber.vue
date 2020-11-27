5ffb2a3682edb1bf2185f67cbfc2e124ac4a6f73
Cagatay Civici <cagatay.civici@gmail.com>
2020-11-27 14:22:36 +0300

    Fixed #705 - InputNumber's onInputBlur fails when component gets destroyed

--- src/components/inputnumber/InputNumber.vue
+++ src/components/inputnumber/InputNumber.vue
@@ -771,10 +771,12 @@ export default {
         onInputBlur(event) {
             this.focused = false;
 
-            let newValue = this.validateValue(this.parseValue(this.$refs.input.$el.value));
-            this.$refs.input.$el.value = this.formatValue(newValue);
-            this.$refs.input.$el.setAttribute('aria-valuenow', newValue);
-            this.updateModel(event, newValue);
+            if (this.$refs && this.$refs.input) {
+                let newValue = this.validateValue(this.parseValue(this.$refs.input.$el.value));
+                this.$refs.input.$el.value = this.formatValue(newValue);
+                this.$refs.input.$el.setAttribute('aria-valuenow', newValue);
+                this.updateModel(event, newValue);
+            }
 
             this.$emit('blur', event);
         },
