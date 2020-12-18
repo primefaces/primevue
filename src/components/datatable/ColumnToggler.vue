8c97a89abc831d38f47dcee70b5f43fbb954d908
sglover <vitruvainhuman@yandex.ru>
2020-12-18 19:54:00 +0300

    Init

---
+++ src/components/datatable/ColumnToggler.vue
@@ -0,0 +1,147 @@
+<template>
+    <div>
+        <div :style="d_position" class="p-datatable-toggler">
+            <i
+                v-if="!panelProps.visible"
+                class="pi pi-plus p-toggler-icon"
+                @click="show($event)"
+            ></i>
+            <i
+                v-if="panelProps.visible"
+                class="pi pi-minus p-toggler-icon"
+                @click="hide()"
+            ></i>
+        </div>
+        <div
+            ref="togglePanel"
+            tabindex="0"
+            v-if="panelProps.visible"
+            class="p-toggler-panel"
+            :style="{
+                top: '0px',
+                left: '0px'
+            }"
+        >
+            <div v-for="(column, index) in columns" :key="index">
+                <div class="p-field-checkbox">
+                    <Checkbox
+                        :id="'chkBx' + column.props.field + index"
+                        :value="column.props.header"
+                        v-model="checkedColumns"
+                    />
+                    <label label :for="'chkBx' + column.props.field + index">
+                        {{ column.props.header }}
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Checkbox from "../checkbox/Checkbox.vue";
+
+export default {
+    props: {
+        position: {
+            type: [Boolean, Object],
+            default: null
+        },
+        columns: {
+            type: Array,
+            default: () => []
+        }
+    },
+    data() {
+        return {
+            panelProps: {},
+            checkedColumns: []
+        };
+    },
+    computed: {
+        d_position() {
+            return typeof this.position === "object"
+                ? this.position
+                : { top: "-1rem", left: "-1rem" };
+        }
+    },
+    methods: {
+        show(event) {
+            console.log(this.panelProps.visible);
+            const rect = event.target.getBoundingClientRect();
+            console.log(rect);
+            this.panelProps.left = rect.left + rect.width + 4;
+            this.panelProps.top = rect.top;
+            this.panelProps.visible = true;
+            document.addEventListener("mouseup", this.hideOnMouseClickOutSide);
+            // this.addScrollListener(this.$refs.toggleButton);
+
+            /*setTimeout(() => {
+                this.panelProps.left -= this.$refs.togglePanel.offsetWidth + 6;
+                this.$refs.togglePanel.focus();
+                this.panelProps.opacity = 1;
+                document.addEventListener('mouseup', this.hideOnMouseClickOutSide);
+            }, 0);*/
+        },
+        hide() {
+            console.log(this.panelProps.visible);
+            this.panelProps.visible = false;
+            // this.menuProps.opacity = 0;
+            // this.removeScrollListener(this.$refs.toggleButton);
+            document.removeEventListener(
+                "mouseup",
+                this.hideOnMouseClickOutSide
+            );
+        },
+        hideOnMouseClickOutSide(e) {
+            let el = e.target;
+            while (el !== this.$refs.togglePanel && el != null) {
+                el = el.parentElement;
+            }
+            if (!el) this.hide();
+        }
+    },
+    watch: {
+        columns() {
+            console.log(this.columns);
+        }
+    },
+    created() {
+        console.log(this.columns);
+    },
+    components: {
+        Checkbox: Checkbox
+    }
+};
+</script>
+
+<style lang="scss">
+.p-datatable-toggler {
+    position: absolute;
+    z-index: 996;
+    user-select: none; /* standard syntax */
+    -webkit-user-select: none; /* webkit (safari, chrome) browsers */
+    -moz-user-select: none; /* mozilla browsers */
+    -khtml-user-select: none; /* webkit (konqueror) browsers */
+    -ms-user-select: none; /* IE10+ */
+}
+
+.p-toggler-icon {
+    padding: 0.2rem;
+    font-size: 1.5rem;
+    border: 1px solid var(--surface-a);
+    border-radius: 35%;
+    background-color: var(--primary-color);
+    color: var(--surface-a);
+    cursor: pointer;
+}
+
+.p-toggler-panel {
+    position: absolute;
+    padding: 10px 10px 0px 10px;
+    background-color: var(--surface-a);
+    border: 1px solid var(--primary-color);
+    z-index: 995;
+    outline: none;
+}
+</style>
