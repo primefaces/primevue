754e2ee938720b9eea051819eacbf1b7a68e561b
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-01-14 16:36:42 +0300

    add usetoast and useconfirm

---
+++ src/components/useconfirm/useconfirm.d.ts
@@ -0,0 +1,19 @@
+export declare function useConfirm(): { 
+    require(args:{ 
+        message?: string;
+        group?: string;
+        icon?: string;
+        header?: string;
+        accept?: Function;
+        reject?: Function;
+        acceptLabel?: string;
+        rejectLabel?: string;
+        acceptIcon?: string;
+        rejectIcon?: string;
+        blockScroll?: boolean;
+        acceptClass?: string;
+        rejectClass?: string;
+    }): void 
+
+    close(): void 
+}
---
+++ src/components/usetoast/usetoast.js
@@ -0,0 +1,12 @@
+import { inject } from 'vue';
+
+export const PrimeVueToastSymbol = Symbol();
+
+export function useToast() {
+    const PrimeVueToast = inject(PrimeVueToastSymbol);
+    if (!PrimeVueToast) {
+        throw new Error('No PrimeVue Toast provided!');
+    } 
+
+    return PrimeVueToast;
+}
