fe4b338d09025b70b08701d51468de016706526a
Pascal <niedermeyer.pascal@gmail.com>
2021-01-16 15:40:21 +0100

    Bugfix: Accordion didn't accept activeIndex. Update Type definition

--- src/components/accordion/Accordion.d.ts
+++ src/components/accordion/Accordion.d.ts
@@ -2,6 +2,7 @@ import Vue, { VNode } from 'vue';
 
 declare class Accordion extends Vue {
     multiple?: boolean;
+    activeIndex?: Number|[Number];
     $emit(eventName: 'tab-open', e: {originalEvent: Event, tab: any}): this;
     $emit(eventName: 'tab-close', e: {originalEvent: Event, tab: any}): this;
     $slots: {
