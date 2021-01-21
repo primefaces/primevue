ffcdcbbb7f1434e3b6d8864fe7491cb05ea7f48c
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-01-21 11:55:32 +0300

    csb added

--- src/views/contextmenu/ContextMenuDoc.vue
+++ src/views/contextmenu/ContextMenuDoc.vue
@@ -313,9 +313,12 @@ export default {
 			</TabPanel>
 
 			<TabPanel header="Source">
-				<a href="https://github.com/primefaces/primevue/tree/master/src/views/contextmenu" class="btn-viewsource" target="_blank" rel="noopener noreferrer">
-					<span>View on GitHub</span>
-				</a>
+            <div class="p-d-flex p-jc-between">
+               <a href="https://github.com/primefaces/primevue/tree/master/src/views/contextmenu" class="btn-viewsource" target="_blank" rel="noopener noreferrer">
+                  <span>View on GitHub</span>
+               </a>
+               <LiveEditor name="ContextMenuDemo" :sources="sources" />
+            </div>
 <pre v-code>
 <code><template v-pre>
 &lt;img alt="logo" src="demo/images/nature/nature3.jpg" @contextmenu="onImageRightClick" aria-haspopup="true"&gt;
@@ -473,3 +476,174 @@ export default {
 		</TabView>
 	</div>
 </template>
+
+<script>
+import LiveEditor from '../liveeditor/LiveEditor';
+export default {
+   data() {
+      return {
+         sources: {
+            'template': {
+               content: `<template>
+    <div class="layout-content">
+        <div class="content-section implementation">
+            <div class="card">
+                <img alt="logo" src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" @contextmenu="onImageRightClick" aria-haspopup="true">
+                <ContextMenu ref="menu" :model="items" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                   label:'File',
+                   icon:'pi pi-fw pi-file',
+                   items:[
+                      {
+                         label:'New',
+                         icon:'pi pi-fw pi-plus',
+                         items:[
+                            {
+                               label:'Bookmark',
+                               icon:'pi pi-fw pi-bookmark'
+                            },
+                            {
+                               label:'Video',
+                               icon:'pi pi-fw pi-video'
+                            },
+
+                         ]
+                      },
+                      {
+                         label:'Delete',
+                         icon:'pi pi-fw pi-trash'
+                      },
+                      {
+                         separator:true
+                      },
+                      {
+                         label:'Export',
+                         icon:'pi pi-fw pi-external-link'
+                      }
+                   ]
+                },
+                {
+                   label:'Edit',
+                   icon:'pi pi-fw pi-pencil',
+                   items:[
+                      {
+                         label:'Left',
+                         icon:'pi pi-fw pi-align-left'
+                      },
+                      {
+                         label:'Right',
+                         icon:'pi pi-fw pi-align-right'
+                      },
+                      {
+                         label:'Center',
+                         icon:'pi pi-fw pi-align-center'
+                      },
+                      {
+                         label:'Justify',
+                         icon:'pi pi-fw pi-align-justify'
+                      },
+
+                   ]
+                },
+                {
+                   label:'Users',
+                   icon:'pi pi-fw pi-user',
+                   items:[
+                      {
+                         label:'New',
+                         icon:'pi pi-fw pi-user-plus',
+
+                      },
+                      {
+                         label:'Delete',
+                         icon:'pi pi-fw pi-user-minus',
+
+                      },
+                      {
+                         label:'Search',
+                         icon:'pi pi-fw pi-users',
+                         items:[
+                            {
+                               label:'Filter',
+                               icon:'pi pi-fw pi-filter',
+                               items:[
+                                  {
+                                     label:'Print',
+                                     icon:'pi pi-fw pi-print'
+                                  }
+                               ]
+                            },
+                            {
+                               icon:'pi pi-fw pi-bars',
+                               label:'List'
+                            }
+                         ]
+                      }
+                   ]
+                },
+                {
+                   label:'Events',
+                   icon:'pi pi-fw pi-calendar',
+                   items:[
+                      {
+                         label:'Edit',
+                         icon:'pi pi-fw pi-pencil',
+                         items:[
+                            {
+                               label:'Save',
+                               icon:'pi pi-fw pi-calendar-plus'
+                            },
+                            {
+                               label:'Delete',
+                               icon:'pi pi-fw pi-calendar-minus'
+                            },
+
+                         ]
+                      },
+                      {
+                         label:'Archieve',
+                         icon:'pi pi-fw pi-calendar-times',
+                         items:[
+                            {
+                               label:'Remove',
+                               icon:'pi pi-fw pi-calendar-minus'
+                            }
+                         ]
+                      }
+                   ]
+                },
+                {
+                   separator:true
+                },
+                {
+                   label:'Quit',
+                   icon:'pi pi-fw pi-power-off'
+                }
+             ]
+        }
+    },
+    methods: {
+        onImageRightClick(event) {
+            this.$refs.menu.show(event);
+        }
+    }
+}`
+            }
+         }
+      }
+   },
+   components: {
+      LiveEditor
+   }
+}
+</script>
--- src/views/floatlabel/FloatLabelDemo.vue
+++ src/views/floatlabel/FloatLabelDemo.vue
@@ -104,6 +104,9 @@
          <div class="content-section documentation">
             <TabView>
                 <TabPanel header="Source">
+                    <div class="p-d-flex p-jc-end">
+                        <LiveEditor name="FloatLabelDemo" :sources="sources" service="CountryService" data="countries" :components="['InputText', 'AutoComplete', 'Calendar', 'Chips', 'InputMask', 'InputNumber', 'CascadeSelect', 'Dropdown', 'MultiSelect', 'Textarea']" />
+                    </div>
 <pre v-code>
 <code><template v-pre>
 &lt;div class="p-fluid p-grid"&gt;
@@ -335,6 +338,7 @@ export default {
 
 <script>
 import CountryService from '../../service/CountryService';
+import LiveEditor from '../liveeditor/LiveEditor';
 
 export default {
     data() {
@@ -436,6 +440,206 @@ export default {
                         }
                     ]
                 }
+            ],
+            sources: {
+                'template': {
+                    content: `<template>
+    <div class="layout-content">
+        <div class="content-section implementation">
+            <div class="card">
+                <div class="p-fluid p-grid">
+                    <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label">
+                            <InputText id="inputtext" type="text" v-model="value1" />
+                            <label for="inputtext">InputText</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label p-input-icon-left">
+                            <i class="pi pi-search" />
+                            <InputText id="inputtext-left" type="text" v-model="valueIconLeft" />
+                            <label for="inputtext-left">Left Icon</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                       <span class="p-float-label p-input-icon-right">
+                            <i class="pi pi-search" />
+                            <InputText id="inputtext-right" type="text" v-model="valueIconRight" />
+                            <label for="inputtext-right">Right Icon</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label">
+                            <AutoComplete v-model="value2" :suggestions="filteredCountries" @complete="searchCountry($event)" field="name" />
+                            <label for="autocomplete">AutoComplete</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label">
+                            <Calendar id="calendar" v-model="value3" />
+                            <label for="calendar">Calendar</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label">
+                            <Chips id="chips" v-model="value4" />
+                            <label for="chips">Chips</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label">
+                            <InputMask id="inputmask" v-model="value5" mask="99/99/9999" slotChar="mm/dd/yyyy" />
+                            <label for="inputmask">InputMask</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label">
+                            <InputNumber id="inputnumber" v-model="value6" />
+                            <label for="inputnumber">InputNumber</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                        <div class="p-inputgroup">
+                            <span class="p-inputgroup-addon">
+                                <i class="pi pi-user"></i>
+                            </span>
+                            <span class="p-float-label">
+                                <InputText id="inputgroup" type="text" v-model="value7" />
+                                <label for="inputgroup">InputGroup</label>
+                            </span>
+                        </div>
+                    </div>
+                     <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label">
+                            <CascadeSelect id="cascadeSelect" v-model="selectedCity" :options="cascadeCountries" optionLabel="cname" optionGroupLabel="name"
+                                :optionGroupChildren="['states', 'cities']" />
+                            <label for="multiselect">CascadeSelect</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label">
+                            <Dropdown id="dropdown" v-model="value8" :options="cities" optionLabel="name" />
+                            <label for="dropdown">Dropdown</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12 p-md-4">
+                        <span class="p-float-label">
+                            <MultiSelect id="multiselect" v-model="value9" :options="cities" optionLabel="name" />
+                            <label for="multiselect">MultiSelect</label>
+                        </span>
+                    </div>
+                    <div class="p-field p-col-12">
+                        <span class="p-float-label">
+                            <Textarea id="textarea" v-model="value10" rows="3" />
+                            <label for="textarea">Textarea</label>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>                    
+</template>
+
+<script>
+import CountryService from '../service/CountryService';
+export default {
+    data() {
+        return {
+            countries: null,
+            filteredCountries: null,
+            cities: [
+                {name: 'New York', code: 'NY'},
+                {name: 'Rome', code: 'RM'},
+                {name: 'London', code: 'LDN'},
+                {name: 'Istanbul', code: 'IST'},
+                {name: 'Paris', code: 'PRS'}
+            ],
+            value1: null,
+            value2: null,
+            value3: null,
+            value4: null,
+            value5: null,
+            value6: null,
+            value7: null,
+            value8: null,
+            value9: null,
+            value10: null,
+            selectedCity: null,
+            cascadeCountries: [
+                {
+                    name: 'Australia',
+                    code: 'AU',
+                    states: [
+                        {
+                            name: 'New South Wales',
+                            cities: [
+                                {cname: 'Sydney', code: 'A-SY'},
+                                {cname: 'Newcastle', code: 'A-NE'},
+                                {cname: 'Wollongong', code: 'A-WO'}
+                            ]
+                        },
+                        {
+                            name: 'Queensland',
+                            cities: [
+                                {cname: 'Brisbane', code: 'A-BR'},
+                                {cname: 'Townsville', code: 'A-TO'}
+                            ]
+                        },
+                        
+                    ]
+                },
+                {
+                    name: 'Canada', 
+                    code: 'CA',
+                    states: [
+                        {
+                            name: 'Quebec',
+                            cities: [
+                                {cname: 'Montreal', code: 'C-MO'},
+                                {cname: 'Quebec City', code: 'C-QU'}
+                            ]
+                        },
+                        {
+                            name: 'Ontario',
+                            cities: [
+                                {cname: 'Ottawa', code: 'C-OT'},
+                                {cname: 'Toronto', code: 'C-TO'}
+                            ]
+                        },
+                        
+                    ]
+                },
+                {
+                    name: 'United States',
+                    code: 'US',
+                    states: [
+                        {
+                            name: 'California',
+                            cities: [
+                                {cname: 'Los Angeles', code: 'US-LA'},
+                                {cname: 'San Diego', code: 'US-SD'},
+                                {cname: 'San Francisco', code: 'US-SF'}
+                            ]
+                        },
+                        {
+                            name: 'Florida',
+                            cities: [
+                                {cname: 'Jacksonville', code: 'US-JA'},
+                                {cname: 'Miami', code: 'US-MI'},
+                                {cname: 'Tampa', code: 'US-TA'},
+                                {cname: 'Orlando', code: 'US-OR'}
+                            ]
+                        },
+                        {
+                            name: 'Texas',
+                            cities: [
+                                {cname: 'Austin', code: 'US-AU'},
+                                {cname: 'Dallas', code: 'US-DA'},
+                                {cname: 'Houston', code: 'US-HO'}
+                            ]
+                        }
+                    ]
+                }
             ]
         }
     },
@@ -459,6 +663,35 @@ export default {
                 }
             }, 250);
         }
+    }
+}`
+                }
+            }
+        }
+    },
+    countryService: null,
+    created() {
+        this.countryService = new CountryService();
+    },
+    mounted() {
+        this.countryService.getCountries().then(data => this.countries = data);
+    },
+    methods: {
+        searchCountry(event) {
+            setTimeout(() => {
+                if (!event.query.trim().length) {
+                    this.filteredCountries = [...this.countries];
+                }
+                else {
+                    this.filteredCountries = this.countries.filter((country) => {
+                        return country.name.toLowerCase().startsWith(event.query.toLowerCase());
+                    });
+                }
+            }, 250);
+        }
+    },
+    components: {
+        LiveEditor
     }
 }
 </script>
