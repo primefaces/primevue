710c692594d261747aa9eca22917d415131643a2
Cagatay Civici <cagatay.civici@gmail.com>
2021-02-10 15:36:27 +0300

    Cosmetics

--- src/views/datatable/DataTableScrollDemo.vue
+++ src/views/datatable/DataTableScrollDemo.vue
@@ -68,19 +68,19 @@
             <div class="card">
                 <h5>Horizontal and Vertical with Footer</h5>
                 <DataTable :value="customers" :scrollable="true" scrollHeight="400px" :loading="loading" scrollDirection="both">
-                    <Column field="id" header="Id" footer="Id" :style="{width:'250px'}"></Column>
-                    <Column field="name" header="Name" footer="Name" :style="{width:'250px'}"></Column>
-                    <Column field="country.name" header="Country" footer="Country" :style="{width:'250px'}"></Column>
-                    <Column field="date" header="Date" footer="Date" :style="{width:'250px'}"></Column>
-                    <Column field="balance" header="Balance" footer="Balance" :style="{width:'250px'}">
+                    <Column field="id" header="Id" footer="Id" :style="{width:'200px'}"></Column>
+                    <Column field="name" header="Name" footer="Name" :style="{width:'200px'}"></Column>
+                    <Column field="country.name" header="Country" footer="Country" :style="{width:'200px'}"></Column>
+                    <Column field="date" header="Date" footer="Date" :style="{width:'200px'}"></Column>
+                    <Column field="balance" header="Balance" footer="Balance" :style="{width:'200px'}">
                         <template #body="{data}">
                             {{formatCurrency(data.balance)}}
                         </template>
                     </Column>
-                    <Column field="company" header="Company" footer="Company" :style="{width:'250px'}"></Column>
-                    <Column field="status" header="Status" footer="Status" :style="{width:'250px'}"></Column>
-                    <Column field="activity" header="Activity" footer="Activity" :style="{width:'250px'}"></Column>
-                    <Column field="representative.name" header="Representative" footer="Representative" :style="{width:'250px'}"></Column>
+                    <Column field="company" header="Company" footer="Company" :style="{width:'200px'}"></Column>
+                    <Column field="status" header="Status" footer="Status" :style="{width:'200px'}"></Column>
+                    <Column field="activity" header="Activity" footer="Activity" :style="{width:'200px'}"></Column>
+                    <Column field="representative.name" header="Representative" footer="Representative" :style="{width:'200px'}"></Column>
                 </DataTable>
             </div>
        
