ad3e3825c3b80aae1f335c51fc8daaaf822a880d
Ahmed Kandil <sa@tomorrowsystem.com>
2021-02-24 18:35:33 +0100

    Convert date string to date object while restoring state from storage
    Fixes ##1020

--- src/components/datatable/DataTable.vue
+++ src/components/datatable/DataTable.vue
@@ -559,7 +559,7 @@ export default {
                         else {
                             localMatch = this.executeLocalFilter(filterField, data[i], filterMeta);
                         }
-                        
+
                         if (!localMatch) {
                             break;
                         }
@@ -1408,8 +1408,11 @@ export default {
             const storage = this.getStorage();
             const stateString = storage.getItem(this.stateKey);
 
+            const iso8601 = /(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?/;
+            const dateParser = (key, value) => iso8601.test(value) ? new Date(value) : value;
+
             if (stateString) {
-                let restoredState = JSON.parse(stateString);
+                let restoredState = JSON.parse(stateString, dateParser);
 
                 if (this.paginator) {
                     this.d_first = restoredState.first;
@@ -1950,7 +1953,7 @@ export default {
     justify-content: center;
 }
 
-.p-column-filter-add-button .p-button-label, 
+.p-column-filter-add-button .p-button-label,
 .p-column-filter-remove-button .p-button-label {
     flex-grow: 0;
 }
