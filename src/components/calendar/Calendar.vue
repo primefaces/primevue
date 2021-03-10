732d83ce96df382f7a0981cadb74c9c8aa50e2ef
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-03-10 17:33:50 +0300

    Fix #875 - Calendar: prop showOtherMonths = false does not hide neighbour months` dates

--- src/components/calendar/Calendar.vue
+++ src/components/calendar/Calendar.vue
@@ -539,12 +539,16 @@ export default {
             }
         },
         onPrevButtonClick(event) {
-            this.navigationState = {backward: true, button: true};
-            this.navBackward(event);
+            if(this.showOtherMonths) {
+                this.navigationState = {backward: true, button: true};
+                this.navBackward(event);
+            }
         },
         onNextButtonClick(event) {
-            this.navigationState = {backward: false, button: true};
-            this.navForward(event);
+            if(this.showOtherMonths) {
+                this.navigationState = {backward: false, button: true};
+                this.navForward(event);
+            }
         },
         navBackward(event) {
             event.preventDefault();
