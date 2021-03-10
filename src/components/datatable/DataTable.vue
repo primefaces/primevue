b917e116729d0f2d44fa452d6594f0c1cee67df0
Cagatay Civici <cagatay.civici@gmail.com>
2021-03-10 13:33:21 +0300

    Better alignment for header content

--- src/components/datatable/DataTable.vue
+++ src/components/datatable/DataTable.vue
@@ -1939,7 +1939,7 @@ export default {
     border: 1px solid transparent;
 }
 
-.p-sortable-column .p-column-header-content {
+.p-datatable .p-column-header-content {
     display: flex;
     align-items: center;
 }
