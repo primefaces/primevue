81c0928fa03b51a7777f86e93dc82dba8d4525e5
Cagatay Civici <cagatay.civici@gmail.com>
2021-05-12 15:25:12 +0300

    Fixed #1207 - Slot support for Badge component

--- src/components/badge/Badge.vue
+++ src/components/badge/Badge.vue
@@ -1,5 +1,7 @@
 <template>
-    <span :class="badgeClass">{{value}}</span>
+    <span :class="badgeClass">
+        <slot>{{value}}</slot>
+    </span>
 </template>
 
 <script>
@@ -17,7 +19,7 @@ export default {
         badgeClass() {
             return ['p-badge p-component', {
                 'p-badge-no-gutter': this.value && String(this.value).length === 1,
-                'p-badge-dot': !this.value,
+                'p-badge-dot': !this.value && !this.$slots.default,
                 'p-badge-lg': this.size === 'large',
                 'p-badge-xl': this.size === 'xlarge',
                 'p-badge-info': this.severity === 'info',
