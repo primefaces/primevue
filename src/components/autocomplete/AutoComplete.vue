3c0ceb7dd3736445b9b690d44a99a492dd8c8476
Matthew Richardson <M.Richardson@ed.ac.uk>
2021-05-27 20:48:33 +0100

    Add 'chip' slot for custom template for multiple mode.

--- src/components/autocomplete/AutoComplete.vue
+++ src/components/autocomplete/AutoComplete.vue
@@ -4,8 +4,10 @@
             type="text" autoComplete="off" v-if="!multiple" role="searchbox" aria-autocomplete="list" :aria-controls="listId">
         <ul ref="multiContainer" :class="multiContainerClass" v-if="multiple" @click="onMultiContainerClick">
             <li v-for="(item, i) of modelValue" :key="i" class="p-autocomplete-token">
+              <slot name="chip" :value="item">
                 <span class="p-autocomplete-token-label">{{getItemContent(item)}}</span>
                 <span class="p-autocomplete-token-icon pi pi-times-circle" @click="removeItem($event, i)"></span>
+              </slot>
             </li>
             <li class="p-autocomplete-input-token">
                 <input ref="input" type="text" autoComplete="off" v-bind="$attrs" @input="onInput" @focus="onFocus" @blur="onBlur" @keydown="onKeyDown"  @change="onChange"
