77d74350d17a536f85cc14eab242296b6d700b5a
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-08-09 12:35:32 +0300

    Fixed #1431 - Speed Dial component v2

---
+++ src/components/speeddial/SpeedDial.d.ts
@@ -0,0 +1,30 @@
+import Vue, { VNode } from 'vue';
+
+declare class SpeedDial extends Vue {
+    model?: any[];
+    visible?: boolean;
+    direction?: string;
+    transitionDelay?: number;
+    type?: string;
+    radius?: number;
+    mask?: boolean;
+    disabled?: boolean;
+    hideOnClickOutside?: boolean;
+    buttonClassName?: string;
+    maskStyle?: string;
+    maskClassName?: string;
+    showIcon?: string;
+    hideIcon?: string;
+    rotateAnimation?: boolean;
+    className?: string;
+    styles?: any;
+    $emit(eventName: 'click', event: Event): this;
+    $emit(eventName: 'show'): this;
+    $emit(eventName: 'hide'): this;
+    $slots: {
+        item: VNode[];
+        button: VNode[];
+    };
+}
+
+export default SpeedDial;
