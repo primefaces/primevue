655bc561356c6987276b5bef86c881ed14120fb1
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-08-09 10:44:17 +0300

    Fixed #1430 - FullCalendar-Vue theme update

--- src/views/fullcalendar/FullCalendarDemo.vue
+++ src/views/fullcalendar/FullCalendarDemo.vue
@@ -31,11 +31,14 @@ export default {
                 plugins:[dayGridPlugin, timeGridPlugin, interactionPlugin],
                 initialDate: '2019-01-01',
                 headerToolbar: {
-                    left: 'prev,next',
+                    left: 'prev,next today',
                     center: 'title',
                     right: 'dayGridMonth,timeGridWeek,timeGridDay'
                 },
-                editable: true
+                editable: true,
+                selectable:true,
+                selectMirror: true,
+                dayMaxEvents: true
             },
             events: null
         };
@@ -52,3 +55,12 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    @media screen and (max-width: 960px) {
+        ::v-deep .fc-header-toolbar {
+            display: flex;
+            flex-wrap: wrap;
+        }
+    }
+</style>
