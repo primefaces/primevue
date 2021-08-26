0f82cd92155b308eb3fe1afc3509af05b7d88eb6
mertsincan <sincan.mert@gmail.com>
2021-08-26 11:11:11 +0300

    Fixed #1390 - Dropdown onkeydown item search not working properly

--- src/components/dropdown/Dropdown.vue
+++ src/components/dropdown/Dropdown.vue
@@ -534,7 +534,7 @@ export default {
                 clearTimeout(this.searchTimeout);
             }
 
-            const char = String.fromCharCode(event.keyCode);
+            const char = event.key;
             this.previousSearchChar = this.currentSearchChar;
             this.currentSearchChar = char;
 
