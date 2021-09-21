2306273b8f3a9cb87957813202683d1c51c31eb5
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-09-21 17:49:11 +0300

    Fixed #1577 - Filtered Dropdown for Windows Tablet

--- src/components/dropdown/Dropdown.vue
+++ src/components/dropdown/Dropdown.vue
@@ -373,7 +373,7 @@ export default {
         bindResizeListener() {
             if (!this.resizeListener) {
                 this.resizeListener = () => {
-                    if (this.overlayVisible && !DomHandler.isAndroid()) {
+                    if (this.overlayVisible && !DomHandler.isTouchDevice()) {
                         this.hide();
                     }
                 };
--- src/components/utils/DomHandler.js
+++ src/components/utils/DomHandler.js
@@ -493,4 +493,8 @@ export default class DomHandler {
     static isAndroid() {
         return /(android)/i.test(navigator.userAgent);
     }
+
+    static isTouchDevice() {
+        return (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+    }
 }
