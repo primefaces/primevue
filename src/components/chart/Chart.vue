1ff66625057bfbf7cce87f6adbf4e154e02c0deb
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-09-22 14:42:56 +0300

    Fixed #1588 - Chart update throws an exception v2

--- src/components/chart/Chart.vue
+++ src/components/chart/Chart.vue
@@ -39,6 +39,11 @@ export default {
     methods: {
         initChart() {
             import('chart.js/auto').then((module) => {
+                if (this.chart) {
+                    this.chart.destroy();
+                    this.chart = null;
+                }
+
                 if (module && module.default) {
                     this.chart = new module.default(this.$refs.canvas, {
                         type: this.type,
@@ -60,10 +65,7 @@ export default {
             }
         },
         reinit() {
-            if (this.chart) {
-                this.chart.destroy();
-                this.initChart();
-            }
+            this.initChart();
         },
         onCanvasClick(event) {
             if (this.chart) {
