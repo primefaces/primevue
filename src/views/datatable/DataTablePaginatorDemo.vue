6e2782dac6cc5323e0573e740c85b1d284ce5d52
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-09-27 11:28:35 +0300

    refactor

--- src/views/datatable/DataTablePaginatorDemo.vue
+++ src/views/datatable/DataTablePaginatorDemo.vue
@@ -1,51 +1,94 @@
 <template>
-  <div id="app">
-    <InputText v-model="filters.global" type="search" placeholder="type"></InputText>
-    <p v-if="bug">pagination will NOT work</p>
-    <p v-else>pagination will work</p>
-    <Button type="button" @click="clear">clear filter</Button>
-    <DataTable class="p-datatable-striped p-datatable-sm" :value="values" :paginator="true" :rows="8" :filters="filters">
-      <Column v-for="column in ['id', 'title']" :key="column" :header="column" :field="column">
-        <template #body="{ data }">{{ data[column] }}</template>
-      </Column>
-    </DataTable>
-  </div>
+	<div>
+        <div class="content-section introduction">
+			<div class="feature-intro">
+				<h1>DataTable <span>Paginator</span></h1>
+				<p>Pagination is enabled by setting paginator property to true and defining the rows attribute as the number of rows per page.</p>
+			</div>
+		</div>
+
+		<div class="content-section implementation">
+            <div class="card">
+                <DataTable :value="customers" :paginator="true" :rows="10"
+                    paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+                    :rowsPerPageOptions="[10,20,50]"
+                    currentPageReportTemplate="Showing {first} to {last} of {totalRecords}">
+                    <Column field="name" header="Name"></Column>
+                    <Column field="country.name" header="Country"></Column>
+                    <Column field="company" header="Company"></Column>
+                    <Column field="representative.name" header="Representative"></Column>
+                    <template #paginatorLeft>
+                        <Button type="button" icon="pi pi-refresh" class="p-button-text" />
+                    </template>
+                    <template #paginatorRight>
+                        <Button type="button" icon="pi pi-cloud" class="p-button-text" />
+                    </template>
+                </DataTable>
+            </div>
+		</div>
+
+        <div class="content-section documentation">
+            <TabView>
+                <TabPanel header="Source">
+<CodeHighlight>
+<template v-pre>
+&lt;DataTable :value="customers" :paginator="true" :rows="10"
+    paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+    :rowsPerPageOptions="[10,20,50]"
+    currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"&gt;
+    &lt;Column field="name" header="Name"&gt;&lt;/Column&gt;
+    &lt;Column field="country.name" header="Country"&gt;&lt;/Column&gt;
+    &lt;Column field="company" header="Company"&gt;&lt;/Column&gt;
+    &lt;Column field="representative.name" header="Representative"&gt;&lt;/Column&gt;
+    &lt;template #paginatorLeft&gt;
+        &lt;Button type="button" icon="pi pi-refresh" class="p-button-text" /&gt;
+    &lt;/template&gt;
+    &lt;template #paginatorRight&gt;
+        &lt;Button type="button" icon="pi pi-cloud" class="p-button-text" /&gt;
+    &lt;/template&gt;
+&lt;/DataTable&gt;
+</template>
+</CodeHighlight>
+
+<CodeHighlight lang="javascript">
+import CustomerService from '../../service/CustomerService';
+
+export default {
+    data() {
+        return {
+            customers: null
+        }
+    },
+    customerService: null,
+    created() {
+        this.customerService = new CustomerService();
+    },
+    mounted() {
+        this.customerService.getCustomersLarge().then(data => this.customers = data);
+    }
+}
+</CodeHighlight>
+                </TabPanel>
+            </TabView>
+        </div>
+	</div>
 </template>
 
 <script>
-const values = [];
+import CustomerService from '../../service/CustomerService';
 
-for (let id = 32; id--; ) {
-  values.push({
-    id,
-    title: `Title ${id}`,
-  });
-}
 export default {
-  data() {
-    return {
-      filters: {},
-      values,
-    };
-  },
-  computed: {
-    bug() {
-        console.log(Object.keys(this.filters))
-      return !!Object.keys(this.filters).length;
+    data() {
+        return {
+            customers: null
+        }
     },
-  },
-  methods: {
-    clear() {
-      this.filters = {};
+    customerService: null,
+    created() {
+        this.customerService = new CustomerService();
     },
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  color: #2c3e50;
-  padding: 4rem;
+    mounted() {
+        this.customerService.getCustomersLarge().then(data => this.customers = data);
+    }
 }
-</style>
+</script>
