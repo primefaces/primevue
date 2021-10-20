edc1f49178268c48645b8e1f02566594a82f1f84
Javier Peletier <jm@epiclabs.io>
2021-10-20 09:49:52 +0200

    VirtualScroller: add missing method typings

--- src/components/virtualscroller/VirtualScroller.d.ts
+++ src/components/virtualscroller/VirtualScroller.d.ts
@@ -15,8 +15,21 @@ interface VirtualScrollerProps {
     class?: string;
 }
 
+interface Range {
+    first: number;
+    last: number;
+    viewport: {
+        first: number;
+        last: number;
+    }
+}
+
 declare class VirtualScroller {
     $props: VirtualScrollerProps;
+    scrollTo(options?: ScrollToOptions): void;
+    scrollToIndex(index: number, behavior: ScrollBehavior): void;
+    scrollInView(index: number, to: "to-start" | "to-end", behavior: ScrollBehavior): void;
+    getRenderedRange(): Range
     $emit(eventName: 'update:numToleratedItems', value: number): this;
     $emit(eventName: 'scroll-index-change', value: { first: number, last: number }): this;
     $emit(eventName: 'lazy-load', value: { first: number, last: number }): this;
