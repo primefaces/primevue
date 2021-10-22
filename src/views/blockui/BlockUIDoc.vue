16cae6c8ca0608fccb6919da629abc98a7e9b017
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-10-22 12:34:31 +0300

    refactor

--- src/views/blockui/BlockUIDoc.vue
+++ src/views/blockui/BlockUIDoc.vue
@@ -1,6 +1,6 @@
 <template>
 	<AppDoc name="BlockUIDemo" :sources="sources" github="blockui/BlockUIDemo.vue">
-        <h5>Import</h5>
+        <h5>Import via Module</h5>
 <pre v-code.script><code>
 import BlockUI from 'primevue/blockui';
 
