3044e32effd3ea0b4206ee7dd3b2e062245dcc60
Cagatay Civici <cagatay.civici@gmail.com>
2021-11-08 17:30:55 +0300

    Fixed typos

--- src/AppConfigurator.vue
+++ src/AppConfigurator.vue
@@ -133,25 +133,25 @@
                     <div class="p-grid free-themes">
                          <div class="p-col-3">
                             <button class="p-link" type="button" @click="changeTheme($event, 'lara-light-indigo')">
-                                <img src="demo/images/themes/saga-blue.png" alt="Saga Blue" />
+                                <img src="demo/images/themes/saga-blue.png" alt="Lara Light Indigo" />
                             </button>
-                            <span>Lara Indigo</span>
+                            <span>Lara Light Indigo</span>
                         </div>
                         <div class="p-col-3">
                             <button class="p-link" type="button" @click="changeTheme($event, 'lara-dark-indigo')">
-                                <img src="demo/images/themes/saga-green.png" alt="Saga Green" />
-                            </button>
-                            <span>Lara Purple</span>
-                        </div>
-                        <div class="p-col-3">
-                            <button class="p-link" type="button" @click="changeTheme($event, 'lara-light-purple')">
-                                <img src="demo/images/themes/saga-orange.png" alt="Saga Orange" />
+                                <img src="demo/images/themes/saga-green.png" alt="Lara Dark Indigo" />
                             </button>
                             <span>Lara Dark Indigo</span>
                         </div>
                         <div class="p-col-3">
+                            <button class="p-link" type="button" @click="changeTheme($event, 'lara-light-purple')">
+                                <img src="demo/images/themes/saga-orange.png" alt="Lara Light Purple" />
+                            </button>
+                            <span>Lara Light Purple</span>
+                        </div>
+                        <div class="p-col-3">
                             <button class="p-link" type="button" @click="changeTheme($event, 'lara-dark-purple')">
-                                <img src="demo/images/themes/saga-purple.png" alt="Saga Purple" />
+                                <img src="demo/images/themes/saga-purple.png" alt="Lara Dark Purple" />
                             </button>
                             <span>Lara Dark Purple</span>
                         </div>
