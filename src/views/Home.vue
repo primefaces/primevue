24f7f0fa91eeb97882e0880994f2c7231e90d2fe
Cagatay Civici <cagatay.civici@gmail.com>
2021-11-16 10:12:41 +0300

    Changed tag

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <div class="home">
         <div :class="introductionClass">
             <div class="introduction-promo">Now with Vue 3 Support</div>
-            <div class="introduction-title">The Ultimate UI Component Library</div>
+            <div class="introduction-title">The NextGen UI Component Library</div>
             <div class="introduction-subtitle">for Vue</div>
 
             <router-link to="/setup" class="action-button">GET STARTED</router-link>
