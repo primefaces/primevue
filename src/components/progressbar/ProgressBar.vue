f7ec60d7571e73e57873ed73018770eb849b6408
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-11-22 15:16:01 +0300

    Fixed #1810 - ProgressBar: color text accessibility issue - v2

--- src/components/progressbar/ProgressBar.vue
+++ src/components/progressbar/ProgressBar.vue
@@ -1,8 +1,9 @@
 <template>
     <div role="progressbar" :class="containerClass" aria-valuemin="0" :aria-valuenow="value" aria-valuemax="100">
-        <div v-if="determinate" class="p-progressbar-value p-progressbar-value-animate" :style="progressStyle"></div>
-        <div v-if="determinate && (value !== null) && showValue" class="p-progressbar-label">
-            <slot>{{value + '%'}}</slot>
+        <div v-if="determinate" class="p-progressbar-value p-progressbar-value-animate" :style="progressStyle">
+          <div v-if="(value != null && value !== 0) && showValue" class="p-progressbar-label">
+              <slot>{{value + '%'}}</slot>
+          </div>
         </div>
         <div v-if="indeterminate" class="p-progressbar-indeterminate-container">
             <div class="p-progressbar-value p-progressbar-value-animate"></div>
@@ -12,10 +13,11 @@
 
 <script>
 export default {
+    name: 'ProgressBar',
     props: {
         value: {
-          type: Number,
-          default: null
+            type: Number,
+            default: null
         },
         mode: {
             type: String,
@@ -39,7 +41,7 @@ export default {
         progressStyle() {
             return {
                 width: this.value + '%',
-                display: 'block'
+                display: 'flex'
             };
         },
         indeterminate() {
@@ -64,18 +66,18 @@ export default {
     position: absolute;
     display: none;
     border: 0 none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+}
+
+.p-progressbar-determinate .p-progressbar-label {
+    display: inline-flex;
 }
 
 .p-progressbar-determinate .p-progressbar-value-animate {
     transition: width 1s ease-in-out;
-}
-
-.p-progressbar-determinate .p-progressbar-label {
-    text-align: center;
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    font-weight: bold;
 }
 
 .p-progressbar-indeterminate .p-progressbar-value::before {
@@ -115,6 +117,7 @@ export default {
     left: 100%;
     right: -90%; }
 }
+
 @keyframes p-progressbar-indeterminate-anim {
   0% {
     left: -35%;
@@ -138,6 +141,7 @@ export default {
     left: 107%;
     right: -8%; }
 }
+
 @keyframes p-progressbar-indeterminate-anim-short {
   0% {
     left: -200%;
