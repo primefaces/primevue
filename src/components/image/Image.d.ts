def2d4b2a94cd518a7a51207ef2349ca21247ad2
mertsincan <sincan.mert@gmail.com>
2021-12-01 16:42:41 +0300

    Fixed #1836 - For Image

--- src/components/image/Image.d.ts
+++ src/components/image/Image.d.ts
@@ -1,14 +1,59 @@
 import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
-interface ImageProps {
-    preview?: boolean;
+export interface ImageProps {
+    /**
+     * Controls the preview functionality.
+     */
+    preview?: boolean | undefined;
+    /**
+     * Inline style of the image element.
+     */
+    imageStyle?: any;
+    /**
+     * Style class of the image element.
+     */
+    imageClass?: string | undefined;
 }
 
-declare class Image {
-    $props: ImageProps;
-    $slots: {
-        indicator: VNode[];
+export interface ImageSlots {
+    /**
+     * Custom indicator template.
+     */
+    indicator: () => VNode[];
+}
+
+export declare type ImageEmits = {
+}
+
+declare class Image extends ClassComponent<ImageProps, ImageSlots, ImageEmits> {
+    /**
+     * Triggered when the preview overlay is shown.
+     *
+     * @memberof Image
+     */
+    show: () => void;
+    /**
+     * Triggered when the preview overlay is hidden.
+     *
+     * @memberof Image
+     */
+    hide: () => void;
+}
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        Image: GlobalComponentConstructor<Image>
     }
 }
 
+/**
+ *
+ * Displays an image with preview and tranformation options. For multiple image, see Galleria.
+ *
+ * Demos:
+ *
+ * - [Image](https://www.primefaces.org/primevue/showcase/#/image)
+ *
+ */
 export default Image;
