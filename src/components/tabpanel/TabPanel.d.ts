6d7d5a4d3a3629fd49633c0a7ce022c1ee6eeac6
mertsincan <sincan.mert@gmail.com>
2021-12-01 23:55:24 +0300

    Fixed #1836 - For TabPanel

--- src/components/tabpanel/TabPanel.d.ts
+++ src/components/tabpanel/TabPanel.d.ts
@@ -1,15 +1,42 @@
 import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
-interface TabPanelProps {
-    header?: any;
-    disabled?: boolean;
+export interface TabPanelProps {
+    /**
+     * Orientation of tab headers.
+     */
+    header?: string | undefined;
+    /**
+     * Whether the tab is disabled.
+     */
+    disabled?: boolean | undefined;
 }
 
-declare class TabPanel {
-    $props: TabPanelProps;
-    $slots: {
-        '': VNode[];
+export interface TabPanelSlots {
+    /**
+     * Custom content template.
+     */
+    default: () => VNode[];
+}
+
+export declare type TabPanelEmits = {
+}
+
+declare class TabPanel extends ClassComponent<TabPanelProps, TabPanelSlots, TabPanelEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        TabPanel: GlobalComponentConstructor<TabPanel>
     }
 }
 
+/**
+ *
+ * TabPanel is a helper component for TabView component.
+ *
+ * Demos:
+ *
+ * - [TabPanel](https://www.primefaces.org/primevue/showcase/#/tabpanel)
+ *
+ */
 export default TabPanel;
