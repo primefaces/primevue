94195f8c346d5d07d0886276c0e6a3ab52363640
mertsincan <sincan.mert@gmail.com>
2021-12-01 23:57:18 +0300

    Fixed #1836 - For Tag

--- src/components/tag/Tag.d.ts
+++ src/components/tag/Tag.d.ts
@@ -1,17 +1,53 @@
 import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
-interface TagProps {
+type TagSeverityType = 'success' | 'info' | 'warning' | 'danger';
+
+export interface TagProps {
+    /**
+     * Value to display inside the tag.
+     */
     value?: any;
-    severity?: string;
-    rounded?: boolean;
-    icon?: string;
+    /**
+     * Severity type of the tag.
+     * @see TagSeverityType
+     */
+    severity?: TagSeverityType;
+    /**
+     * Whether the corners of the tag are rounded.
+     */
+    rounded?: boolean | undefined;
+    /**
+     * Icon of the tag to display next to the value.
+     */
+    icon?: string | undefined;
 }
 
-declare class Tag {
-    $props: TagProps;
-    $slots: {
-        '': VNode[];
+export interface TagSlots {
+    /**
+     * Custom content template
+     */
+    default: () => VNode[];
+}
+
+export declare type TagEmits = {
+}
+
+declare class Tag extends ClassComponent<TagProps, TagSlots, TagEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        Tag: GlobalComponentConstructor<Tag>
     }
 }
 
+/**
+ *
+ * Tag component is used to categorize content.
+ *
+ * Demos:
+ *
+ * - [Tag](https://www.primefaces.org/primevue/showcase/#/tag)
+ *
+ */
 export default Tag;
