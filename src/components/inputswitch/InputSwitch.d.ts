0ab72941f49ed3de1e19aec638c15c54a34f1307
mertsincan <sincan.mert@gmail.com>
2021-12-01 16:45:53 +0300

    Fixed #1836 - For InputSwitch

--- src/components/inputswitch/InputSwitch.d.ts
+++ src/components/inputswitch/InputSwitch.d.ts
@@ -1,17 +1,69 @@
-interface InputSwitchProps {
-    modelValue?: boolean;
-    class?: string;
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+export interface InputSwitchProps {
+    /**
+     * Specifies whether a inputswitch should be checked or not.
+     */
+    modelValue?: boolean | undefined;
+    /**
+     * Inline style of the component.
+     */
+    class?: string | undefined;
+    /**
+     * Style class of the component input field.
+     */
     style?: any;
+    /**
+     * Value in checked state.
+     */
     trueValue?: any;
+    /**
+     * Value in unchecked state.
+     */
     falseValue?: any;
 }
 
-declare class InputSwitch {
-    $props: InputSwitchProps;
-    $emit(eventName: 'update:modelValue', value: boolean): this;
-    $emit(eventName: 'click', event: Event): this;
-    $emit(eventName: 'change', event: Event): this;
-    $emit(eventName: 'input', value: boolean): this;
+export interface InputSwitchSlots {
 }
 
+export declare type InputSwitchEmits = {
+    /**
+     * Emitted when the value changes.
+     * @param {boolean} value - New value.
+     */
+    'update:modelValue': (value: boolean) => void;
+    /**
+     * Callback to invoke on click.
+     * @param {Event} event - Browser event.
+     */
+    'click': (event: Event) => void;
+    /**
+     * Callback to invoke on value change.
+     * @param {Event} event - Browser event.
+     */
+    'change': (event: Event) => void;
+    /**
+     * Callback to invoke on value change.
+     * @param {boolean} value - New value.
+     */
+    'input': (value: boolean) => void;
+}
+
+declare class InputSwitch extends ClassComponent<InputSwitchProps, InputSwitchSlots, InputSwitchEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        InputSwitch: GlobalComponentConstructor<InputSwitch>
+    }
+}
+
+/**
+ *
+ * InputSwitch is used to select a boolean value.
+ *
+ * Demos:
+ *
+ * - [InputSwitch](https://www.primefaces.org/primevue/showcase/#/inputswitch)
+ *
+ */
 export default InputSwitch;
