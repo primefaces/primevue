889b4a06148549c8e5bacc2475064b5b4251bbc2
mertsincan <sincan.mert@gmail.com>
2021-12-01 17:13:52 +0300

    Fixed #1836 - For Row

--- src/components/row/Row.d.ts
+++ src/components/row/Row.d.ts
@@ -1,7 +1,29 @@
-interface RowProps {}
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
-declare class Row {
-    $props: RowProps;
+export interface RowProps {
 }
 
+export interface RowSlots {
+}
+
+export declare type RowEmits = {
+}
+
+declare class Row extends ClassComponent<RowProps, RowSlots, RowEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        Row: GlobalComponentConstructor<Row>
+    }
+}
+
+/**
+ *
+ * Row is a helper component to create column group.
+ *
+ * Demos:
+ *
+ * - [DataTable](https://www.primefaces.org/primevue/showcase/#/datatable/colgroup)
+ *
+ */
 export default Row;
