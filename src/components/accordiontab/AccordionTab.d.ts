aa32ab6eca52f21489e99a015073e3f3fd3c78ce
mertsincan <sincan.mert@gmail.com>
2021-12-01 15:06:35 +0300

    Fixed #1836 - For AccordionTab

--- src/components/accordiontab/AccordionTab.d.ts
+++ src/components/accordiontab/AccordionTab.d.ts
@@ -1,16 +1,45 @@
 import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
 interface AccordionTabProps {
-    header?: string;
-    disabled?: boolean;
+    /**
+     * Orientation of tab headers.
+     */
+    header?: string | undefined;
+    /**
+     * Whether the tab is disabled.
+     */
+    disabled?: boolean | undefined;
 }
 
-declare class AccordionTab {
-    $props: AccordionTabProps;
-    $slot: {
-        '': VNode[];
-        header: VNode[];
+interface AccordionTabSlots {
+    /**
+     * Default slot for content.
+     */
+    default: () => VNode[];
+    /**
+     * Custom content for the title section of a panel is defined using the header template.
+     */
+    header: () => VNode[];
+}
+
+type AccordionTabEmits = { }
+
+declare class AccordionTab extends ClassComponent<AccordionTabProps, AccordionTabSlots, AccordionTabEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        AccordionTab: GlobalComponentConstructor<AccordionTab>
     }
 }
 
+/**
+ *
+ * AccordionTab is a helper component for Accordion
+ *
+ * Demos:
+ *
+ * - [Accordion](https://www.primefaces.org/primevue/showcase/#/accordion)
+ *
+ */
 export default AccordionTab;
