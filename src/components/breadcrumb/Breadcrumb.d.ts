7ef0fee49df33ddf8f192191f99163aedbe4ad14
mertsincan <sincan.mert@gmail.com>
2021-12-01 15:26:43 +0300

    Fixed #1836 - For Breadcrumb

--- src/components/breadcrumb/Breadcrumb.d.ts
+++ src/components/breadcrumb/Breadcrumb.d.ts
@@ -1,18 +1,58 @@
-interface BreadcrumbProps {
-    home?: any;
-    model?: any[];
+import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+import { MenuItem } from '../menuitem';
+
+export interface BreadcrumbProps {
+    /**
+     * An array of menuitems.
+     */
+    model?: MenuItem[];
+    /**
+     * Configuration for the home icon.
+     */
+    home?: MenuItem;
+    /**
+     * Whether to apply 'router-link-active-exact' class if route exactly matches the item path.
+     * Default value is true.
+     */
     exact?: boolean;
 }
 
-interface BreadcrumbItemSlotInterface {
-    item: any;
+export interface BreadcrumbSlots {
+    /**
+     * Custom item template.
+     * @param {Object} scope - item slot's params.
+     */
+    item: (scope: {
+        /**
+         * Menuitem instance
+         */
+        item: MenuItem;
+    }) => VNode[];
 }
 
-declare class Breadcrumb {
-    $props: BreadcrumbProps;
-    $slots: {
-        item: BreadcrumbItemSlotInterface
+export declare type BreadcrumbEmits = {
+}
+
+declare class Breadcrumb extends ClassComponent<BreadcrumbProps, BreadcrumbSlots, BreadcrumbEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        Breadcrumb: GlobalComponentConstructor<Breadcrumb>
     }
 }
 
+/**
+ *
+ * Breadcrumb provides contextual information about page hierarchy.
+ *
+ * Helper API:
+ *
+ * - [MenuItem](https://www.primefaces.org/primevue/showcase/#/menumodel)
+ *
+ * Demos:
+ *
+ * - [Breadcrumb](https://www.primefaces.org/primevue/showcase/#/breadcrumb)
+ *
+ */
 export default Breadcrumb;
