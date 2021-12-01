f30e8bcafb41ebab106200522b7726b14ffb8b27
mertsincan <sincan.mert@gmail.com>
2021-12-01 23:57:47 +0300

    Fixed #1836 - For Terminal

--- src/components/terminal/Terminal.d.ts
+++ src/components/terminal/Terminal.d.ts
@@ -1,10 +1,41 @@
-interface TerminalProps {
-    welcomeMessage?: string;
-    prompt?: string;
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+export interface TerminalProps {
+    /**
+     * Initial text to display on terminal.
+     */
+    welcomeMessage?: string | undefined;
+    /**
+     * Prompt text for each command.
+     */
+    prompt?: string | undefined;
 }
 
-declare class Terminal {
-    $props: TerminalProps;
+export interface TerminalSlots {
 }
 
+export declare type TerminalEmits = {
+}
+
+declare class Terminal extends ClassComponent<TerminalProps, TerminalSlots, TerminalEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        Terminal: GlobalComponentConstructor<Terminal>
+    }
+}
+
+/**
+ *
+ * Terminal is a text based user interface.
+ *
+ * Helper API:
+ *
+ * - TerminalService
+ *
+ * Demos:
+ *
+ * - [Terminal](https://www.primefaces.org/primevue/showcase/#/terminal)
+ *
+ */
 export default Terminal;
