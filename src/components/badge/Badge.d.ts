2b81d6c3065ac4453fe470b51b17d849588cbb64
mertsincan <sincan.mert@gmail.com>
2021-12-01 15:24:45 +0300

    Fixed #1836 - For Badge

--- src/components/badge/Badge.d.ts
+++ src/components/badge/Badge.d.ts
@@ -1,11 +1,52 @@
-interface BadgeProps {
+import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+type BadgeSeverityType = 'info' | 'success' | 'warning' | 'danger';
+
+type BadgeSizeType = 'large' | 'xlarge' | undefined;
+
+export interface BadgeProps {
+    /**
+     * Value to display inside the badge.
+     */
     value?: any;
-    severity?: string;
-    size?: string;
+    /**
+     * Severity type of the badge.
+     * @see BadgeSeverityType
+     */
+    severity?: BadgeSeverityType;
+    /**
+     * Size of the badge, valid options are "large" and "xlarge".
+     * @see BadgeSizeType
+     */
+    size?: BadgeSizeType;
 }
 
-declare class Badge {
-    $props: BadgeProps;
+export interface BadgeSlots {
+    /**
+     * Content can easily be customized with the default slot instead of using the built-in display.
+     */
+     default: () => VNode[];
 }
 
+export declare type BadgeEmits = {
+}
+
+declare class Badge extends ClassComponent<BadgeProps, BadgeSlots, BadgeEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        Badge: GlobalComponentConstructor<Badge>
+    }
+}
+
+/**
+ *
+ * Badge is a small status indicator for another element.
+ *
+ * Demos:
+ *
+ * - [Badge](https://www.primefaces.org/primevue/showcase/#/badge)
+ *
+ */
 export default Badge;
