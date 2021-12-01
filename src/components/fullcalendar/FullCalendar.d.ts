791cdd34a1156a50cd4a0835f5fdf4ecac6f482f
mertsincan <sincan.mert@gmail.com>
2021-12-01 16:40:20 +0300

    Fixed #1836 - For FullCalendar

--- src/components/fullcalendar/FullCalendar.d.ts
+++ src/components/fullcalendar/FullCalendar.d.ts
@@ -1,10 +1,36 @@
-interface FullCalendarProps {
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+export interface FullCalendarProps {
     events?: any[];
     options?: object;
 }
 
-declare class FullCalendar {
-    $props: FullCalendarProps;
+export interface FullCalendarSlots {
 }
 
+export declare type FullCalendarEmits = {
+}
+
+declare class FullCalendar extends ClassComponent<FullCalendarProps, FullCalendarSlots, FullCalendarEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        FullCalendar: GlobalComponentConstructor<FullCalendar>
+    }
+}
+
+/**
+ *
+ * PrimeVue provides theming for the FullCalendar Vue component.
+ *
+ * Helper API:
+ *
+ * - [FullCalendar](https://fullcalendar.io/docs/vue)
+ *
+ * Demos:
+ *
+ * - [FullCalendar](https://www.primefaces.org/primevue/showcase/#/fullcalendar)
+ *
+ * @deprecated
+ */
 export default FullCalendar;
