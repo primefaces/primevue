36def09cd8710d1c0aeebfcaec45ceddf2a0458b
mertsincan <sincan.mert@gmail.com>
2021-12-01 15:23:49 +0300

    Fixed #1836 - For AvatarGroup

--- src/components/avatargroup/AvatarGroup.d.ts
+++ src/components/avatargroup/AvatarGroup.d.ts
@@ -1,7 +1,33 @@
-interface AvatarGroupProps {}
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
-declare class AvatarGroup {
-    $props: AvatarGroupProps;
+export interface AvatarGroupProps {
 }
 
+export interface AvatarGroupSlots {
+}
+
+export declare type AvatarGroupEmits = {
+}
+
+declare class AvatarGroup extends ClassComponent<AvatarGroupProps, AvatarGroupSlots, AvatarGroupEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        AvatarGroup: GlobalComponentConstructor<AvatarGroup>
+    }
+}
+
+/**
+ *
+ * A set of Avatars can be displayed together using the AvatarGroup component.
+ *
+ * Helper Components:
+ *
+ * - Avatar
+ *
+ * Demos:
+ *
+ * - [AvatarGroup](https://www.primefaces.org/primevue/showcase/#/avatar)
+ *
+ */
 export default AvatarGroup;
