ab924a64b5c40d9968465b15000c4044565162fd
mertsincan <sincan.mert@gmail.com>
2021-12-01 16:23:32 +0300

    Fixed #1836 - For ConfirmPopup

--- src/components/confirmpopup/ConfirmPopup.d.ts
+++ src/components/confirmpopup/ConfirmPopup.d.ts
@@ -1,16 +1,47 @@
-interface ConfirmPopupProps {
+import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+import { ConfirmationOptions } from '../confirmationoptions';
+
+export interface ConfirmPopupProps {
+    /**
+     * Optional key to match the key of the confirmation, useful to target a specific confirm dialog instance.
+     */
     group?: string;
 }
 
-interface ConfirmPopupMessageSlot {
-    message: any;
+export interface ConfirmPopupSlots {
+    /**
+     * Custom message template.
+     * @param {Object} scope - message slot's params.
+     */
+    message: (scope: {
+        message: ConfirmationOptions;
+    }) => VNode[];
 }
 
-declare class ConfirmPopup {
-    $props: ConfirmPopupProps;
-    $slots: {
-        message: ConfirmPopupMessageSlot
+export declare type ConfirmPopupEmits = {
+}
+
+declare class ConfirmPopup extends ClassComponent<ConfirmPopupProps, ConfirmPopupSlots, ConfirmPopupEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        ConfirmPopup: GlobalComponentConstructor<ConfirmPopup>
     }
 }
 
+/**
+ *
+ * ConfirmPopup displays a confirmation overlay displayed relatively to its target.
+ *
+ * Helper API:
+ *
+ * - Confirmation API
+ * - ConfirmationService
+ *
+ * Demos:
+ *
+ * - [ConfirmPopup](https://www.primefaces.org/primevue/showcase/#/confirmpopup)
+ *
+ */
 export default ConfirmPopup;
