36d07020406d24eb6d34ef50f092dd3bc0f34c8f
mertsincan <sincan.mert@gmail.com>
2021-12-01 16:19:54 +0300

    Fixed #1836 - For ColumnGroup

--- src/components/columngroup/ColumnGroup.d.ts
+++ src/components/columngroup/ColumnGroup.d.ts
@@ -1,9 +1,35 @@
-interface ColumnGroupProps {
-    type?: string;
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+type ColumnGroupType = 'header' | 'footer';
+
+export interface ColumnGroupProps {
+    /**
+     * Type of column group
+     */
+    type?: ColumnGroupType;
 }
 
-declare class ColumnGroup {
-    $props: ColumnGroupProps;
+export interface ColumnGroupSlots {
 }
 
+export declare type ColumnGroupEmits = {
+}
+
+declare class ColumnGroup extends ClassComponent<ColumnGroupProps, ColumnGroupSlots, ColumnGroupEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        ColumnGroup: GlobalComponentConstructor<ColumnGroup>
+    }
+}
+
+/**
+ *
+ * ColumnGroup is a helper component to create grouped header and footer on DataTable.
+ *
+ * Demos:
+ *
+ * - [ColumnGroup](https://www.primefaces.org/primevue/showcase/#/datatable/colgroup)
+ *
+ */
 export default ColumnGroup;
