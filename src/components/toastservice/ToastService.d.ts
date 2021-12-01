7832b3ac0bc89a15e29e409c55a15b47e1098eca
mertsincan <sincan.mert@gmail.com>
2021-12-02 00:02:48 +0300

    Fixed #1836 - For ToastService

--- src/components/toastservice/ToastService.d.ts
+++ src/components/toastservice/ToastService.d.ts
@@ -1,12 +1,24 @@
-import Vue, { Plugin } from 'vue';
+import { Plugin } from 'vue';
+import { ToastMessageOptions } from '../toast';
 
 declare const plugin: Plugin;
 export default plugin;
 
-interface ToastServiceMethods {
-    add(message: any): any;
-    removeGroup(group: any): void;
-    removeAllGroups(): void;
+export interface ToastServiceMethods {
+    /**
+     * Displays the message in a suitable Toast component.
+     * @param {ToastMessageOptions} message - Message instance.
+     */
+    add: (message: ToastMessageOptions) => void;
+    /**
+     * Clears the messages that belongs to the group.
+     * @param {string} group - Name of the message group.
+     */
+    removeGroup: (group: string) => void;
+    /**
+     * Clears all the messages.
+     */
+    removeAllGroups: () => void;
 }
 
 declare module 'vue/types/vue' {
