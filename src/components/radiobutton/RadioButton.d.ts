ce0c8d6f967f2fa4c8cab6f5875680a6d3dc5150
mertsincan <sincan.mert@gmail.com>
2021-12-01 17:10:35 +0300

    Fixed #1836 - For RadioButton

--- src/components/radiobutton/RadioButton.d.ts
+++ src/components/radiobutton/RadioButton.d.ts
@@ -1,15 +1,60 @@
-interface RadioButtonProps {
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+export interface RadioButtonProps {
+    /**
+     * Value of the checkbox.
+     */
     value?: any;
+    /**
+     * Value binding of the checkbox.
+     */
     modelValue?: any;
+    /**
+     * Style class of the component input field.
+     */
     class?: string;
+    /**
+     * Inline style of the component.
+     */
     style?: any;
 }
 
-declare class RadioButton {
-    $props: RadioButtonProps;
-    $emit(eventName: 'update:modelValue', value: any): this;
-    $emit(eventName: 'click', event: Event): this;
-    $emit(eventName: 'change', event: Event): this;
+export interface RadioButtonSlots {
 }
 
+export declare type RadioButtonEmits = {
+    /**
+     * Emitted when the value changes.
+     * @param {*} value - New value.
+     */
+    'update:modelValue': (value: any) => void;
+    /**
+     * Callback to invoke on radio button click.
+     * @param {Event} event - Browser event.
+     */
+    'click': (event: Event) => void;
+    /**
+     * Callback to invoke on radio button value change.
+     * @param {Event} event - Browser event.
+     */
+    'change': (event: Event) => void;
+}
+
+declare class RadioButton extends ClassComponent<RadioButtonProps, RadioButtonSlots, RadioButtonEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        RadioButton: GlobalComponentConstructor<RadioButton>
+    }
+}
+
+/**
+ *
+ * RadioButton is an extension to standard radio button element with theming.
+ *
+ * Demos:
+ *
+ * - [RadioButton](https://www.primefaces.org/primevue/showcase/#/radiobutton)
+ *
+ */
 export default RadioButton;
