8a1987cb026b11089fa2a9cd0b608d49addec8ee
mertsincan <sincan.mert@gmail.com>
2021-12-02 00:12:13 +0300

    Fixed #1836 - For TriStateCheckbox

--- src/components/tristatecheckbox/TriStateCheckbox.d.ts
+++ src/components/tristatecheckbox/TriStateCheckbox.d.ts
@@ -1,12 +1,46 @@
-interface TriStateCheckboxProps {
-    modelValue?: any;
+import { ClassComponent, GlobalComponentConstructor, Nullable } from '../ts-helpers';
+
+export interface TriStateCheckboxProps {
+    /**
+     * Value of the component.
+     */
+    modelValue?: Nullable<boolean>;
+    /**
+     * 	Inline style of the component.
+     */
     style?: any;
+    /**
+     * Style class of the component.
+     */
     class?: string;
 }
 
-declare class TriStateCheckbox {
-    $props: TriStateCheckboxProps;
-    $emit(eventName: 'update:modelValue', value: any): this;
+export interface TriStateCheckboxSlots {
 }
 
+export declare type TriStateCheckboxEmits = {
+    /**
+     * Emitted when the value changes.
+     * @param {boolean|null|undefined} value - New value.
+     */
+    'update:modelValue': (value: Nullable<boolean>) => void;
+}
+
+declare class TriStateCheckbox extends ClassComponent<TriStateCheckboxProps, TriStateCheckboxSlots, TriStateCheckboxEmits> { }
+
+declare module '@vue/runtime-core' {
+    interface GlobalComponents {
+        TriStateCheckbox: GlobalComponentConstructor<TriStateCheckbox>
+    }
+}
+
+/**
+ *
+ * TriStateCheckbox is used to select either "true", "false" or "null" as the value.
+ *
+ * Demos:
+ *
+ * - [TriStateCheckbox](https://www.primefaces.org/primevue/showcase/#/tristatecheckbox)
+ *
+ */
 export default TriStateCheckbox;
