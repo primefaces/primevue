380d3b5c122e5ce68658dc8146a6348ca93b3d11
Cagatay Civici <cagatay.civici@gmail.com>
2021-12-07 11:05:28 +0300

    Text update

--- src/views/datatable/DataTableLazyDemo.vue
+++ src/views/datatable/DataTableLazyDemo.vue
@@ -6,7 +6,7 @@
 				<p>Lazy mode is handy to deal with large datasets, instead of loading the entire data, small chunks of data is loaded by invoking corresponding callbacks everytime paging, sorting and filtering happens.
                     Sample belows imitates lazy paging by using an in memory list. It is also important to assign the logical number of rows to totalRecords by doing a projection query for paginator configuration
                     so that paginator displays the UI assuming there are actually records of totalRecords size although in reality they aren't as in lazy mode, only the records that are displayed on the current page exist.
-                    Also, the implementation of <b>checkbox selection</b> in lazy tables is left entirely to the user. Since the DataTable does not know what will happen to the data on the next page or whether there are instant data changes, the selection array can be implemented in several ways. One of them is as in the example below.
+                    In addition, the implementation of <b>checkbox selection</b> in lazy tables is left entirely to the user since the DataTable does not have access to the whole dataset in order to define the checked state.
                 </p>
 			</div>
             <AppDemoActions />
