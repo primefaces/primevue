4ef6e968808d1b2c57c3de95d84740bcf49f3c9c
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-12-13 17:06:38 +0300

    Update FilterServiceDoc.vue

--- src/views/filterservice/FilterServiceDoc.vue
+++ src/views/filterservice/FilterServiceDoc.vue
@@ -4,7 +4,7 @@
 			<TabPanel header="Documentation">
                 <h5>Import</h5>
 <CodeHighlight lang="javascript">
-import FilterService from 'primevue/api';
+import {FilterService} from 'primevue/api';
 </CodeHighlight>
 
                 <h5>Getting Started</h5>
