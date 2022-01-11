e722303ad0c7eff8dd2cf027ff9c76c0f3d9410a
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-01-11 10:50:55 +0300

    Fixed #1967 - DataTable, TreeTable | Checkbox and RadioButton Focus Missing on Material

--- src/components/treetable/BodyCell.vue
+++ src/components/treetable/BodyCell.vue
@@ -1,9 +1,9 @@
 <template>
-   <td :style="containerStyle" :class="containerClass">
+    <td :style="containerStyle" :class="containerClass">
         <button type="button" class="p-treetable-toggler p-link" @click="toggle" v-if="columnProp('expander')" :style="togglerStyle" tabindex="-1" v-ripple>
             <i :class="togglerIcon"></i>
         </button>
-        <div class="p-checkbox p-treetable-checkbox p-component" @click="toggleCheckbox" v-if="checkboxSelectionMode && columnProp('expander')" role="checkbox" :aria-checked="checked">
+        <div :class="['p-checkbox p-treetable-checkbox p-component', {'p-checkbox-focused': checkboxFocused}]" @click="toggleCheckbox" v-if="checkboxSelectionMode && columnProp('expander')" role="checkbox" :aria-checked="checked">
             <div class="p-hidden-accessible">
                 <input type="checkbox" @focus="onCheckboxFocus" @blur="onCheckboxBlur" />
             </div>
@@ -112,7 +112,7 @@ export default {
             this.$emit('checkbox-toggle');
         },
         onCheckboxFocus() {
-           this.checkboxFocused = true;
+            this.checkboxFocused = true;
         },
         onCheckboxBlur() {
             this.checkboxFocused = false;
