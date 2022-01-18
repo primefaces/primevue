6d8821584ed69937d08b1411ca22eab5034d0b21
Cagatay Civici <cagatay.civici@gmail.com>
2022-01-18 15:16:04 +0300

    Set version

--- src/views/setup/Setup.vue
+++ src/views/setup/Setup.vue
@@ -12,7 +12,7 @@
             <p>PrimeVue is available at <a href="https://www.npmjs.com/package/primevue">npm</a>, if you have an existing application run the following commands to download PrimeVue and PrimeIcons to your project.</p>
 
 <pre v-code.script><code>
-npm install primevue@^3.10.0 --save
+npm install primevue@^3.11.0 --save
 npm install primeicons --save
 
 </code></pre>
