51092ea81baa1ca287588c7f15793ba62176ed7b
Dennis H <dennis.haegler@gmail.com>
2022-01-20 16:44:23 +0100

    Fix quotation mark in doc

--- src/views/menubar/MenubarDoc.vue
+++ src/views/menubar/MenubarDoc.vue
@@ -183,7 +183,7 @@ export default {
 &lt;Menubar :model="items"&gt;
     &lt;template #item="{item}"&gt;
         &lt;router-link :to="item.to" custom v-slot="{href, route, navigate, isActive, isExactActive}"&gt;
-            &lt;a :href="href" @click="navigate" :class="{'active-link': isActive, 'active-link-exact": isExactActive}&gt;{{route.fullPath}}&lt;/a&gt;
+            &lt;a :href="href" @click="navigate" :class="{'active-link': isActive, 'active-link-exact': isExactActive}"&gt;{{route.fullPath}}&lt;/a&gt;
         &lt;/router-link&gt;
     &lt;/template&gt;
 &lt;/Menubar&gt;
