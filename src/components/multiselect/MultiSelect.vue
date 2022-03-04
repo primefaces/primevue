94b6b651d022d3edb93c9f7ed70b3c823e5391f4
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-03-04 16:11:48 +0300

    Fixed #2241 - Pressing Enter/Return key submits form when there is a single MultiSelect and it has focus

--- src/components/multiselect/MultiSelect.vue
+++ src/components/multiselect/MultiSelect.vue
@@ -333,8 +333,7 @@ export default {
                     }
                 break;
 
-                //enter and escape
-                case 13:
+                //escape
                 case 27:
                     if (this.overlayVisible) {
                         this.hide();
