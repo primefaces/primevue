e17bbe0da81d5214818a7cadcc0acf5483ea7863
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-03-07 09:33:08 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  "env": {
+    "jest": true
   }
 }
