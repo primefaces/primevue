61cbbfb634424a663fae26488b518a5a0cd16e99
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-03-10 09:16:28 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    jest: true
   },
   'extends': [
     'plugin:vue/vue3-essential',
@@ -13,8 +14,5 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  "env": {
-    "jest": true
   }
 }
