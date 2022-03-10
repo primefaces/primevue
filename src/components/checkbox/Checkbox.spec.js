f6f38ff165719e28bfcaf90e7d778920eab16f33
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-03-10 13:14:00 +0300

    Checkbox test added

---
+++ src/components/checkbox/Checkbox.spec.js
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils';
+import Checkbox from './Checkbox.vue';
+
+describe('Checkbox.vue', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = mount(Checkbox, {
+            props: {
+                modelValue: false,
+                binary: true
+            }
+        });
+    });
+
+    it('should exist', () => {
+        expect(wrapper.find('.p-checkbox.p-component').exists()).toBe(true);
+        expect(wrapper.find('.p-checkbox-icon.pi.pi-check').exists()).toBe(false);
+    });
+
+    it('should exist', async () => {
+        await wrapper.setProps({ modelValue: true });
+
+        expect(wrapper.find('.p-checkbox-checked').exists()).toBe(true);
+        expect(wrapper.find('.p-checkbox-box.p-highlight').exists()).toBe(true);
+        expect(wrapper.find('.p-checkbox-icon.pi.pi-check').exists()).toBe(true);
+    });
+});
