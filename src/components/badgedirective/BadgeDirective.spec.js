7a25bd89fe74aa171d6957edbf06c5a4c466e62d
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-03-10 12:17:29 +0300

    Badge test added

---
+++ src/components/badgedirective/BadgeDirective.spec.js
@@ -0,0 +1,17 @@
+import { mount } from '@vue/test-utils';
+import BadgeDirective from './BadgeDirective';
+
+describe('directive badge should exist', () => {
+    it('positioned badge', () => {
+        const wrapper = mount({
+            template: '<i class="pi pi-bell mr-4 p-text-secondary" style="font-size: 2rem" v-badge="2"></i>',
+            directives: {
+                'badge': BadgeDirective
+            }
+        });
+
+        expect(wrapper.find('.p-overlay-badge').exists()).toBe(true);
+        expect(wrapper.find('.p-badge.p-component').exists()).toBe(true);
+        expect(wrapper.find('.p-badge.p-component').text()).toBe('2');
+    });
+});
