83c00a2181d09df860c45ab552d2743da693806b
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-03-18 16:40:50 +0300

    Create Panel.spec.js

---
+++ src/components/panel/Panel.spec.js
@@ -0,0 +1,33 @@
+import { mount } from '@vue/test-utils';
+import Panel from './Panel.vue';
+
+describe('Panel.vue', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = mount(Panel, {
+            props: {
+                header: 'PrimeVue'
+            },
+            slots: {
+                default: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>'
+            }
+        });
+    });
+
+    it('should exist', () => {
+        expect(wrapper.find('.p-panel.p-component').exists()).toBe(true);
+        expect(wrapper.find('.p-panel-content').text()).toBe('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt');
+        expect(wrapper.find('.p-panel-title').text()).toBe('PrimeVue');
+    });
+
+    it('should be toggleable', async () => {
+        await wrapper.setProps({ toggleable: true });
+
+        expect(wrapper.find('.p-panel.p-component').classes()).toContain('p-panel-toggleable');
+
+        await wrapper.vm.toggle({});
+
+        expect(wrapper.emitted().toggle[0]).toEqual([{originalEvent: {}, value: true}]);
+    });
+});
