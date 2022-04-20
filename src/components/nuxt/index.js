cc6300b09b3f38660dec95e1f4875def1ef016f7
yeligoth <yeligoth@gmail.com>
2022-04-20 16:27:15 -0500

    Add support for nuxt custom themes

--- src/components/nuxt/index.js
+++ src/components/nuxt/index.js
@@ -1,9 +1,16 @@
 import path from 'path';
+const fs = require('fs');
 
 export default function (moduleOptions) {
     const config = Object.assign({}, this.options.primevue, moduleOptions);
     const theme = config.theme || 'saga-blue';
-    this.options.css.push('primevue/resources/themes/' + theme + '/theme.css');
+    let themePath = 'primevue/resources/themes/' + theme + '/theme.css';
+
+    if (fs.existsSync(theme)) {
+        themePath = theme;
+    }
+
+    this.options.css.push(themePath);
     this.options.css.push('primevue/resources/primevue.min.css');
     this.options.css.push('primeicons/primeicons.css');
 
