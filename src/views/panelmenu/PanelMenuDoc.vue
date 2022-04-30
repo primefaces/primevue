3660c00629f606df07c538d6a59cd66a3e259e56
hakiiver2 <uotnnizum12@gmail.com>
2022-04-30 21:20:11 +0900

    Fix panelmenu demo quotes

--- src/views/panelmenu/PanelMenuDoc.vue
+++ src/views/panelmenu/PanelMenuDoc.vue
@@ -168,7 +168,7 @@ export default {
 &lt;PanelMenu :model="items"&gt;
     &lt;template #item="{item}"&gt;
         &lt;router-link :to="item.to" custom v-slot="{href, route, navigate, isActive, isExactActive}"&gt;
-            &lt;a :href="href" @click="navigate" :class="{'active-link': isActive, 'active-link-exact": isExactActive}&gt;{{route.fullPath}}&lt;/a&gt;
+            &lt;a :href="href" @click="navigate" :class="{'active-link': isActive, 'active-link-exact': isExactActive}"&gt;{{route.fullPath}}&lt;/a&gt;
         &lt;/router-link&gt;
     &lt;/template&gt;
 &lt;/PanelMenu&gt;
