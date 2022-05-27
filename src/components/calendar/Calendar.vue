ba623453a7ff553986e64b285534093a56fe28d4
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-05-27 17:00:31 +0300

    Fixed #2509 - Calender resize listener bug

--- src/components/calendar/Calendar.vue
+++ src/components/calendar/Calendar.vue
@@ -767,7 +767,7 @@ export default {
         bindResizeListener() {
             if (!this.resizeListener) {
                 this.resizeListener = () => {
-                    if (this.overlayVisible) {
+                    if (this.overlayVisible && !DomHandler.isAndroid()) {
                         this.overlayVisible = false;
                     }
                 };
