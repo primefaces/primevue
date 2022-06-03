fc7c5679e99033d75b3b16a64c0eba5b2bd5e40d
Ray Foss <ray@vblip.com>
2022-06-03 16:03:05 -0500

    Real issue is with the aria-checked value

--- src/components/radiobutton/RadioButton.vue
+++ src/components/radiobutton/RadioButton.vue
@@ -3,7 +3,7 @@
         <div class="p-hidden-accessible">
             <input ref="input" type="radio" :checked="checked" :value="value" v-bind="$attrs" @focus="onFocus" @blur="onBlur">
         </div>
-        <div ref="box" :class="['p-radiobutton-box', {'p-highlight': checked, 'p-disabled': $attrs.disabled, 'p-focus': focused}]" role="radio" :aria-checked="checked">
+        <div ref="box" :class="['p-radiobutton-box', {'p-highlight': checked, 'p-disabled': $attrs.disabled, 'p-focus': focused}]" role="radio" :aria-checked="checked || null">
             <div class="p-radiobutton-icon"></div>
         </div>
     </div>
@@ -48,7 +48,7 @@ export default {
     },
     computed: {
         checked() {
-            return (this.modelValue != null && ObjectUtils.equals(this.modelValue, this.value)) || null;
+            return this.modelValue != null && ObjectUtils.equals(this.modelValue, this.value);
         },
         containerClass() {
             return ['p-radiobutton p-component', this.class, {'p-radiobutton-checked': this.checked, 'p-radiobutton-disabled': this.$attrs.disabled, 'p-radiobutton-focused': this.focused}];
