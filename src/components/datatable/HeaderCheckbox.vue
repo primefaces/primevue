8594715ecc4729a77dd56fc246516698e776b9f1
Néstor Pérez González <nestorrente@gmail.com>
2022-06-06 12:22:46 +0200

    Add disable class to the outer div of DTHeaderCheckbox
    At the moment, when the checkbox is disabled, the browser displays the "pointer" cursor because the root `div.p-checkbox` doesn't have the `p-disabled` class. Adding the `p-disabled` class to the root div solves the issue.

--- src/components/datatable/HeaderCheckbox.vue
+++ src/components/datatable/HeaderCheckbox.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="['p-checkbox p-component', {'p-checkbox-focused': focused}]" @click="onClick" @keydown.space.prevent="onClick">
+    <div :class="['p-checkbox p-component', {'p-checkbox-focused': focused, 'p-disabled': $attrs.disabled}]" @click="onClick" @keydown.space.prevent="onClick">
         <div ref="box" :class="['p-checkbox-box p-component', {'p-highlight': checked, 'p-disabled': $attrs.disabled, 'p-focus': focused}]"
             role="checkbox" :aria-checked="checked" :tabindex="$attrs.disabled ? null : '0'" @focus="onFocus($event)" @blur="onBlur($event)">
             <span :class="['p-checkbox-icon', {'pi pi-check': checked}]"></span>
