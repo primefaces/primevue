cca230371d0a2db4b5368a25f8584e8df296e04f
Ray Foss <ray@vblip.com>
2022-06-06 15:50:27 -0500

    Update RadioButton.vue
    Cast to string instead as recommended to improve accessibility. Before this the accessibility attribute would be REMOVED! when it was unchecked.

--- src/components/radiobutton/RadioButton.vue
+++ src/components/radiobutton/RadioButton.vue
@@ -3,7 +3,7 @@
         <div class="p-hidden-accessible">
             <input ref="input" type="radio" :checked="checked" :value="value" v-bind="$attrs" @focus="onFocus" @blur="onBlur">
         </div>
-        <div ref="box" :class="['p-radiobutton-box', {'p-highlight': checked, 'p-disabled': $attrs.disabled, 'p-focus': focused}]" role="radio" :aria-checked="checked || null">
+        <div ref="box" :class="['p-radiobutton-box', {'p-highlight': checked, 'p-disabled': $attrs.disabled, 'p-focus': focused}]" role="radio" :aria-checked="'' + checked">
             <div class="p-radiobutton-icon"></div>
         </div>
     </div>
