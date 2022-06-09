49f45bae0da2dc9d81405f059611b2843a644d6a
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-06-09 11:45:37 +0300

    Refactor #2653 - Demo added

---
+++ src/views/dynamicdialog/DynamicDialogDemo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Button label="Show" @click="onShow" />
+
+        <DynamicDialog v-model:visible="isShow"/>
+    </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+export default {
+    data() {
+        return {
+            isShow: false
+        }
+    },
+    methods:{
+        onShow() {
+            this.$dialog.open({
+                component: defineAsyncComponent(() => import('./ProductListDemo.vue')),
+                header: 'Dialog Header',
+                width: '50%',
+                height: '550px'
+            });
+        }
+    }
+}
+</script>
---
+++ src/views/dynamicdialog/ProductListDemo.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+    <DataTable :value="products" responsiveLayout="scroll">
+			<Column field="code" header="Code"></Column>
+			<Column field="name" header="Name"></Column>
+			<Column field="category" header="Category"></Column>
+			<Column field="quantity" header="Quantity"></Column>
+            <Column>
+                <template #body>
+                    <Button type="button" icon="pi pi-cog" @click="closeDialog"></Button>
+                </template>
+            </Column>
+		</DataTable>
+	</div>
+</template>
+
+<script>
+import ProductService from '../../service/ProductService';
+
+export default {
+    data() {
+        return {
+            products: null
+        }
+    },
+    productService: null,
+    created() {
+        this.productService = new ProductService();
+    },
+    mounted() {
+        this.productService.getProductsSmall().then(data => this.products = data);
+    },
+    methods: {
+        closeDialog() {
+            this.$dialog.close();
+        }
+    }
+}
+</script>
