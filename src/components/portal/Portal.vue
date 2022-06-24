7bc41a4639ca9bdedcb0cdde9ca6e2073031e351
mertsincan <sincan.mert@gmail.com>
2022-06-24 18:26:51 +0100

    Cosmetic

--- src/components/portal/Portal.vue
+++ src/components/portal/Portal.vue
@@ -26,8 +26,8 @@ export default {
         inline() {
             return this.disabled || this.appendTo === 'self';
         },
-        isClient () {
-            return DomHandler.isClient()
+        isClient() {
+            return DomHandler.isClient();
         }
     }
 }
