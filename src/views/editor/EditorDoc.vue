d0bdb13570cb08a52069cdb7feff696590aca55a
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-07-08 13:05:20 +0300

    Accessibility for Editor

--- src/views/editor/EditorDoc.vue
+++ src/views/editor/EditorDoc.vue
@@ -157,6 +157,12 @@ import Editor from 'primevue/editor';
 			</table>
 		</div>
 
+        <h5>Accessibility</h5>
+        <DevelopmentSection>
+            <p>Quill performs generally well in terms of accessibility. The elements in the toolbar can be tabbed and have the necessary ARIA roles/attributes for screen readers. One known limitation is the lack of arrow key support
+                for <a href="https://github.com/quilljs/quill/issues/1031">dropdowns</a> in the toolbar that may be overcome with a custom toolbar.</p>
+        </DevelopmentSection>
+
 		<h5>Dependencies</h5>
 		<p><a href="http://quilljs.com">Quill</a> Editor 1.3+.</p>
 		<p>Resources of quill needs to be added to your application.</p>
