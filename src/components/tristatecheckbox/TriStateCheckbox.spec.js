bc2de5eec345bf7b22acdd739158d22578588d32
Bahadir Sofuoglu <bahadirsofuoglu@gmail.com>
2022-08-27 17:41:10 +0300

    TriStateCheckbox component unit test completed

--- src/components/tristatecheckbox/TriStateCheckbox.spec.js
+++ src/components/tristatecheckbox/TriStateCheckbox.spec.js
@@ -1,47 +1,106 @@
-import { mount } from '@vue/test-utils';
-import TriStateCheckbox from './TriStateCheckbox.vue';
+import { mount } from '@vue/test-utils'
+import TriStateCheckbox from './TriStateCheckbox.vue'
+import { config } from '@vue/test-utils' //2.0.0-beta.5
 
-describe('TriStateCheckbox.vue', () => {
-    let wrapper;
-
-    beforeEach(() => {
-        wrapper = mount(TriStateCheckbox, {
-            props: {
-                modelValue: null
+config.global.mocks = {
+    $primevue: {
+        config: {
+            locale: {
+                aria: {
+                    trueLabel: 'trueLabel',
+                    falseLabel: 'falseLabel',
+                    nullLabel: 'nullLabel'
+                }
             }
-        });
-    });
+        }
+    }
+}
+let wrapper
+const modelValues = [true, false, null]
+const emittedResults = [false, null, true]
+describe('TriStateCheckbox.vue', () => {
+    beforeEach(() => {
+        wrapper = mount(TriStateCheckbox)
+    }),
+        describe('UpdateModel method tests', () => {
+            it('When disable props true updateModal should not triggered emit', async () => {
+                await wrapper.setProps({
+                    disabled: true,
+                    modelValue: null
+                })
 
-    it('is TriStateCheckbox exist', () => {
-        expect(wrapper.find('.p-checkbox.p-component').exists()).toBe(true);
-    });
+                wrapper.vm.updateModel()
 
-    it('is state changed', async () => {
-        await wrapper.vm.onClick({});
+                expect(wrapper.emitted()['update:modelValue']).toBeFalsy()
+            })
 
-        expect(wrapper.emitted()['update:modelValue'][0]).toEqual([true]);
+            it('When disable props false updateModal should triggered emit', () => {
+                wrapper.vm.updateModel()
 
-        await wrapper.setProps({modelValue: true});
-        await wrapper.vm.onClick({});
+                expect(wrapper.emitted()['update:modelValue']).toBeTruthy()
+            })
 
-        expect(wrapper.emitted()['update:modelValue'][1]).toEqual([false]);
+            modelValues.forEach((modelValue, index) => {
+                it('When modelValue changed update model emitted value should be change', async () => {
+                    await wrapper.setProps({
+                        modelValue
+                    })
 
-        await wrapper.setProps({modelValue: false});
-        await wrapper.vm.onClick({});
+                    wrapper.vm.updateModel()
 
-        expect(wrapper.emitted()['update:modelValue'][2]).toEqual([null]);
-    });
+                    expect(wrapper.emitted()['update:modelValue']).toEqual([
+                        [emittedResults[index]]
+                    ])
+                })
+            })
+        })
+    it('When onClick method triggered some methods effect', () => {
+        const mockUpdateModel = jest.fn()
+        wrapper.vm.updateModel = mockUpdateModel
 
-    it('is icon chenaged', async () => {
-        expect(wrapper.find('.p-checkbox-icon').classes()).not.toContain('pi-check');
-        expect(wrapper.find('.p-checkbox-icon').classes()).not.toContain('pi-times');
+        wrapper.vm.onClick('test')
 
-        await wrapper.setProps({modelValue: true});
+        expect(wrapper.vm.updateModel).toBeCalled()
+        expect(wrapper.emitted()['click']).toBeTruthy()
+        expect(wrapper.emitted()['change']).toBeTruthy()
+    })
 
-        expect(wrapper.find('.p-checkbox-icon').classes()).toContain('pi-check');
+    it('When event.code is not equal Enter methods should not be effected', async () => {
+        wrapper.vm.onKeyDown({ code: 'test' })
 
-        await wrapper.setProps({modelValue: false});
+        expect(wrapper.emitted().keydown).toBeFalsy()
+    })
 
-        expect(wrapper.find('.p-checkbox-icon').classes()).toContain('pi-times');
-    });
-});
+    it('When event.code is equal Enter some methods should be triggered', async () => {
+        const mockUpdateModel = jest.fn()
+        wrapper.vm.updateModel = mockUpdateModel
+
+        wrapper.vm.onKeyDown({ code: 'Enter', preventDefault: () => {} })
+
+        expect(wrapper.vm.updateModel).toBeCalled()
+        expect(wrapper.emitted().keydown).toBeTruthy()
+    })
+
+    it('When onBlur is triggered focused property should be false', async () => {
+        wrapper.vm.onBlur()
+
+        expect(wrapper.vm.focused).toBeFalsy()
+        expect(wrapper.emitted().blur).toBeTruthy()
+    })
+    it('is icon changed', async () => {
+        expect(wrapper.find('.p-checkbox-icon').classes()).not.toContain(
+            'pi-check'
+        )
+        expect(wrapper.find('.p-checkbox-icon').classes()).not.toContain(
+            'pi-times'
+        )
+
+        await wrapper.setProps({ modelValue: true })
+
+        expect(wrapper.find('.p-checkbox-icon').classes()).toContain('pi-check')
+
+        await wrapper.setProps({ modelValue: false })
+
+        expect(wrapper.find('.p-checkbox-icon').classes()).toContain('pi-times')
+    })
+})
