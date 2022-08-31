f38cbed6eab916f0334ae72987232b495b74ba51
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-08-31 10:04:36 +0300

    Fixed #2905 - Improve Deferred Content implementation for Accessibility

--- src/views/deferredcontent/DeferredContentDoc.vue
+++ src/views/deferredcontent/DeferredContentDoc.vue
@@ -67,6 +67,24 @@ import DeferredContent from 'primevue/deferredcontent';
 		<h5>Styling</h5>
 		<p>Component does not apply any styling.</p>
 
+        <h5>Accessibility</h5>
+    <DevelopmentSection>
+        <h6>Screen Reader</h6>
+        <p>DeferredContent can be utilized in many use cases as a result no role is enforced, in fact a role may not be necessary if the card is used for presentational purposes only.
+            Any valid attribute is passed to the container element so you have full control over the roles like <a href="https://www.w3.org/TR/wai-aria/#landmark" alt="Landmark Roles">landmark</a> and attributes like <i>aria-live</i>.
+        </p>
+
+<pre v-code><code>
+&lt;DeferredContent role="region" aria-live="polite" aria-label="Content loaded after page scrolled down"&gt;
+    Content
+&lt;/DeferredContent&gt;
+
+</code></pre>
+
+        <h5>Keyboard Support</h5>
+        <p>Component does not include any interactive elements.</p>
+    </DevelopmentSection>
+
 		<h5>Dependencies</h5>
 		<p>None.</p>
     </AppDoc>
