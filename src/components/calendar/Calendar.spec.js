5b93342a961e8ef7bd03ad0696fdf0ab0a8fbef9
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2022-10-05 20:44:21 +0200

    Allow custom icons for the calendar component

--- src/components/calendar/Calendar.spec.js
+++ src/components/calendar/Calendar.spec.js
@@ -18,6 +18,7 @@ describe('Calendar.vue', () => {
             }
         });
     });
+
     it('should exist', async () => {
         expect(wrapper.find('.p-calendar.p-component').exists()).toBe(true);
         expect(wrapper.find('.p-inputtext').exists()).toBe(true);
@@ -42,6 +43,7 @@ describe('Calendar.vue', () => {
         await wrapper.vm.onDateSelect({ currentTarget: { focus: () => {} } }, event);
         expect(onDateSelect).toHaveBeenCalled();
     });
+
     it('should calculate the correct view date when in range mode', async () => {
         const dateOne = new Date();
         const dateTwo = new Date();
@@ -50,5 +52,29 @@ describe('Calendar.vue', () => {
         await wrapper.setProps({ selectionMode: 'range', showTime: true, modelValue: [dateOne, dateTwo] });
 
         expect(wrapper.vm.viewDate).toEqual(dateTwo);
+    });
+
+    it('should respect custom icon settings', async () => {
+        await wrapper.setProps({
+            previousIcon: 'pi pi-discord',
+            nextIcon: 'pi pi-facebook',
+            incrementIcon: 'pi pi-linkedin',
+            decrementIcon: 'pi pi-microsoft',
+            inline: true
+        });
+
+        const previousIcon = wrapper.find('.p-datepicker-prev-icon')
+        const nextIcon = wrapper.find('.p-datepicker-next-icon')
+
+        expect(previousIcon.classes()).toContain('pi-discord')
+        expect(nextIcon.classes()).toContain('pi-facebook')
+
+        await wrapper.setProps({
+            timeOnly: true,
+            hourFormat: '12'
+        })
+
+        expect(wrapper.findAll('.pi-linkedin')).toHaveLength(3)
+        expect(wrapper.findAll('.pi-microsoft')).toHaveLength(3)
     });
 });
