5c9fb3cc6fdef45efb7da6677d29b16b18158362
mertsincan <sincan.mert@gmail.com>
2022-10-12 02:18:17 +0100

    Fixed #3100 - Improve TieredMenu implementation for Accessibility

--- src/components/tieredmenu/TieredMenuSub.vue
+++ src/components/tieredmenu/TieredMenuSub.vue
@@ -1,66 +1,88 @@
 <template>
-    <ul ref="element" :class="containerClass" role="'menubar' : 'menu'" aria-orientation="horizontal">
-        <template v-for="(item, i) of model" :key="label(item) + i.toString()">
-            <li v-if="visible(item) && !item.separator" :class="getItemClass(item)" :style="item.style" @mouseenter="onItemMouseEnter($event, item)" role="none">
-                <template v-if="!template">
-                    <router-link v-if="item.to && !disabled(item)" v-slot="{ navigate, href, isActive, isExactActive }" :to="item.to" custom>
-                        <a v-ripple :href="href" @click="onItemClick($event, item, navigate)" :class="linkClass(item, { isActive, isExactActive })" @keydown="onItemKeyDown($event, item)" role="menuitem">
-                            <span v-if="item.icon" :class="['p-menuitem-icon', item.icon]"></span>
-                            <span class="p-menuitem-text">{{ label(item) }}</span>
+    <ul>
+        <template v-for="(processedItem, index) of items" :key="getItemKey(processedItem)">
+            <li
+                v-if="isItemVisible(processedItem) && !getItemProp(processedItem, 'separator')"
+                :id="getItemId(processedItem)"
+                :style="getItemProp(processedItem, 'style')"
+                :class="getItemClass(processedItem)"
+                role="menuitem"
+                :aria-label="getItemLabel(processedItem)"
+                :aria-disabled="isItemDisabled(processedItem) || undefined"
+                :aria-expanded="isItemGroup(processedItem) ? isItemActive(processedItem) : undefined"
+                :aria-haspopup="isItemGroup(processedItem) && !getItemProp(processedItem, 'to') ? 'menu' : undefined"
+                :aria-level="level + 1"
+                :aria-setsize="getAriaSetSize()"
+                :aria-posinset="getAriaPosInset(index)"
+                @click="onItemClick($event, processedItem)"
+                @mouseenter="onItemMouseEnter($event, processedItem)"
+            >
+                <div class="p-menuitem-content">
+                    <template v-if="!template">
+                        <router-link v-if="getItemProp(processedItem, 'to') && !isItemDisabled(processedItem)" v-slot="{ navigate, href, isActive, isExactActive }" :to="getItemProp(processedItem, 'to')" custom>
+                            <a v-ripple :href="href" :class="getItemActionClass(processedItem, { isActive, isExactActive })" tabindex="-1" :aria-hidden="true" @click="onItemActionClick($event, navigate)">
+                                <span v-if="getItemProp(processedItem, 'icon')" :class="getItemIconClass(processedItem)"></span>
+                                <span class="p-menuitem-text">{{ getItemLabel(processedItem) }}</span>
+                            </a>
+                        </router-link>
+                        <a v-else v-ripple :href="getItemProp(processedItem, 'url')" :class="getItemActionClass(processedItem)" :target="getItemProp(processedItem, 'target')" tabindex="-1" :aria-hidden="true">
+                            <span v-if="getItemProp(processedItem, 'icon')" :class="getItemIconClass(processedItem)"></span>
+                            <span class="p-menuitem-text">{{ getItemLabel(processedItem) }}</span>
+                            <span v-if="getItemProp(processedItem, 'items')" class="p-submenu-icon pi pi-angle-right"></span>
                         </a>
-                    </router-link>
-                    <a
-                        v-else
-                        v-ripple
-                        :href="item.url"
-                        :class="linkClass(item)"
-                        :target="item.target"
-                        :aria-haspopup="item.items != null"
-                        :aria-expanded="item === activeItem"
-                        @click="onItemClick($event, item)"
-                        @keydown="onItemKeyDown($event, item)"
-                        role="menuitem"
-                        :tabindex="disabled(item) ? null : '0'"
-                    >
-                        <span v-if="item.icon" :class="['p-menuitem-icon', item.icon]"></span>
-                        <span class="p-menuitem-text">{{ label(item) }}</span>
-                        <span v-if="item.items" class="p-submenu-icon pi pi-angle-right"></span>
-                    </a>
-                </template>
-                <component v-else :is="template" :item="item"></component>
-                <TieredMenuSub v-if="visible(item) && item.items" :key="label(item) + '_sub_'" :model="item.items" :template="template" @leaf-click="onLeafClick" @keydown-item="onChildItemKeyDown" :parentActive="item === activeItem" :exact="exact" />
+                    </template>
+                    <component v-else :is="template" :item="processedItem.item"></component>
+                </div>
+                <TieredMenuSub
+                    v-if="isItemVisible(processedItem) && isItemGroup(processedItem)"
+                    role="menu"
+                    class="p-submenu-list"
+                    :menuId="menuId"
+                    :focusedItemId="focusedItemId"
+                    :items="processedItem.items"
+                    :template="template"
+                    :activeItemPath="activeItemPath"
+                    :exact="exact"
+                    :level="level + 1"
+                    @item-click="$emit('item-click', $event)"
+                    @item-mouseenter="$emit('item-mouseenter', $event)"
+                />
             </li>
-            <li v-if="visible(item) && item.separator" :key="'separator' + i.toString()" :class="['p-menu-separator', item.class]" :style="item.style" role="separator"></li>
+            <li v-if="isItemVisible(processedItem) && getItemProp(processedItem, 'separator')" :id="getItemId(processedItem)" :style="getItemProp(processedItem, 'style')" :class="getSeparatorItemClass(processedItem)" role="separator"></li>
         </template>
     </ul>
 </template>
 
 <script>
-import { DomHandler } from 'primevue/utils';
 import Ripple from 'primevue/ripple';
+import { ObjectUtils } from 'primevue/utils';
 
 export default {
     name: 'TieredMenuSub',
-    emits: ['leaf-click', 'keydown-item'],
+    emits: ['item-click', 'item-mouseenter'],
     props: {
-        model: {
+        menuId: {
+            type: String,
+            default: null
+        },
+        focusedItemId: {
+            type: String,
+            default: null
+        },
+        items: {
             type: Array,
             default: null
         },
-        root: {
-            type: Boolean,
-            default: false
-        },
-        popup: {
-            type: Boolean,
-            default: false
-        },
-        parentActive: {
-            type: Boolean,
-            default: false
+        level: {
+            type: Number,
+            default: 0
         },
         template: {
             type: Function,
+            default: null
+        },
+        activeItemPath: {
+            type: Object,
             default: null
         },
         exact: {
@@ -68,191 +90,79 @@ export default {
             default: true
         }
     },
-    documentClickListener: null,
-    data() {
-        return {
-            activeItem: null
-        };
-    },
-    watch: {
-        parentActive(newValue) {
-            if (!newValue) {
-                this.activeItem = null;
-            }
-        }
-    },
-    updated() {
-        if (this.root && this.activeItem) {
-            this.bindDocumentClickListener();
-        }
-    },
-    beforeUnmount() {
-        this.unbindDocumentClickListener();
-    },
     methods: {
-        onItemMouseEnter(event, item) {
-            if (this.disabled(item)) {
-                event.preventDefault();
-
-                return;
-            }
-
-            if (this.root) {
-                if (this.activeItem || this.popup) {
-                    this.activeItem = item;
-                }
-            } else {
-                this.activeItem = item;
-            }
+        getItemId(processedItem) {
+            return `${this.menuId}_${processedItem.key}`;
         },
-        onItemClick(event, item, navigate) {
-            if (this.disabled(item)) {
-                event.preventDefault();
-
-                return;
-            }
-
-            if (item.command) {
-                item.command({
-                    originalEvent: event,
-                    item: item
-                });
-            }
-
-            if (item.items) {
-                if (this.activeItem && item === this.activeItem) this.activeItem = null;
-                else this.activeItem = item;
-            }
-
-            if (!item.items) {
-                this.onLeafClick();
-            }
-
-            if (item.to && navigate) {
-                navigate(event);
-            }
+        getItemKey(processedItem) {
+            return this.getItemId(processedItem);
         },
-        onLeafClick() {
-            this.activeItem = null;
-            this.$emit('leaf-click');
+        getItemProp(processedItem, name) {
+            return processedItem && processedItem.item ? ObjectUtils.getItemValue(processedItem.item[name]) : undefined;
         },
-        onItemKeyDown(event, item) {
-            let listItem = event.currentTarget.parentElement;
-
-            switch (event.which) {
-                //down
-                case 40:
-                    var nextItem = this.findNextItem(listItem);
-
-                    if (nextItem) {
-                        nextItem.children[0].focus();
-                    }
-
-                    event.preventDefault();
-                    break;
-
-                //up
-                case 38:
-                    var prevItem = this.findPrevItem(listItem);
-
-                    if (prevItem) {
-                        prevItem.children[0].focus();
-                    }
-
-                    event.preventDefault();
-                    break;
-
-                //right
-                case 39:
-                    if (item.items) {
-                        this.activeItem = item;
-
-                        setTimeout(() => {
-                            listItem.children[1].children[0].children[0].focus();
-                        }, 50);
-                    }
-
-                    event.preventDefault();
-                    break;
-
-                default:
-                    break;
-            }
-
-            this.$emit('keydown-item', {
-                originalEvent: event,
-                element: listItem
-            });
+        getItemLabel(processedItem) {
+            return this.getItemProp(processedItem, 'label');
         },
-        onChildItemKeyDown(event) {
-            //left
-            if (event.originalEvent.which === 37) {
-                this.activeItem = null;
-                event.element.parentElement.previousElementSibling.focus();
-            }
+        isItemActive(processedItem) {
+            return this.activeItemPath.some((path) => path.key === processedItem.key);
         },
-        findNextItem(item) {
-            let nextItem = item.nextElementSibling;
+        isItemVisible(processedItem) {
+            return this.getItemProp(processedItem, 'visible') !== false;
+        },
+        isItemDisabled(processedItem) {
+            return this.getItemProp(processedItem, 'disabled');
+        },
+        isItemFocused(processedItem) {
+            return this.focusedItemId === this.getItemId(processedItem);
+        },
+        isItemGroup(processedItem) {
+            return ObjectUtils.isNotEmpty(processedItem.items);
+        },
+        onItemClick(event, processedItem) {
+            const command = this.getItemProp(processedItem, 'command');
 
-            if (nextItem) return DomHandler.hasClass(nextItem, 'p-disabled') || !DomHandler.hasClass(nextItem, 'p-menuitem') ? this.findNextItem(nextItem) : nextItem;
-            else return null;
-        },
-        findPrevItem(item) {
-            let prevItem = item.previousElementSibling;
+            command && command({ originalEvent: event, item: processedItem.item });
+            this.$emit('item-click', { originalEvent: event, processedItem, isFocus: true });
 
-            if (prevItem) return DomHandler.hasClass(prevItem, 'p-disabled') || !DomHandler.hasClass(prevItem, 'p-menuitem') ? this.findPrevItem(prevItem) : prevItem;
-            else return null;
+            event.stopPropagation();
         },
-        getItemClass(item) {
+        onItemMouseEnter(event, processedItem) {
+            this.$emit('item-mouseenter', { originalEvent: event, processedItem });
+        },
+        onItemActionClick(event, navigate) {
+            navigate && navigate(event);
+        },
+        getAriaSetSize() {
+            return this.items.filter((processedItem) => this.isItemVisible(processedItem) && !this.getItemProp(processedItem, 'separator')).length;
+        },
+        getAriaPosInset(index) {
+            return index - this.items.slice(0, index).filter((processedItem) => this.isItemVisible(processedItem) && this.getItemProp(processedItem, 'separator')).length + 1;
+        },
+        getItemClass(processedItem) {
             return [
                 'p-menuitem',
-                item.class,
+                this.getItemProp(processedItem, 'class'),
                 {
-                    'p-menuitem-active': this.activeItem === item
+                    'p-menuitem-active p-highlight': this.isItemActive(processedItem),
+                    'p-focus': this.isItemFocused(processedItem),
+                    'p-disabled': this.isItemDisabled(processedItem)
                 }
             ];
         },
-        linkClass(item, routerProps) {
+        getItemActionClass(processedItem, routerProps) {
             return [
-                'p-menuitem-link',
+                'p-menuitem-link p-menuitem-action',
                 {
-                    'p-disabled': this.disabled(item),
                     'router-link-active': routerProps && routerProps.isActive,
                     'router-link-active-exact': this.exact && routerProps && routerProps.isExactActive
                 }
             ];
         },
-        bindDocumentClickListener() {
-            if (!this.documentClickListener) {
-                this.documentClickListener = (event) => {
-                    if (this.$el && !this.$el.contains(event.target)) {
-                        this.activeItem = null;
-                        this.unbindDocumentClickListener();
-                    }
-                };
-
-                document.addEventListener('click', this.documentClickListener);
-            }
+        getItemIconClass(processedItem) {
+            return ['p-menuitem-icon', this.getItemProp(processedItem, 'icon')];
         },
-        unbindDocumentClickListener() {
-            if (this.documentClickListener) {
-                document.removeEventListener('click', this.documentClickListener);
-                this.documentClickListener = null;
-            }
-        },
-        visible(item) {
-            return typeof item.visible === 'function' ? item.visible() : item.visible !== false;
-        },
-        disabled(item) {
-            return typeof item.disabled === 'function' ? item.disabled() : item.disabled;
-        },
-        label(item) {
-            return typeof item.label === 'function' ? item.label() : item.label;
-        }
-    },
-    computed: {
-        containerClass() {
-            return { 'p-submenu-list': !this.root };
+        getSeparatorItemClass(processedItem) {
+            return ['p-menu-separator p-menuitem-separator', this.getItemProp(processedItem, 'class')];
         }
     },
     directives: {
--- src/views/tieredmenu/TieredMenuDoc.vue
+++ src/views/tieredmenu/TieredMenuDoc.vue
@@ -248,6 +248,30 @@ toggle(event) {
                         <td>true</td>
                         <td>Whether to apply 'router-link-active-exact' class if route exactly matches the item path.</td>
                     </tr>
+                    <tr>
+                        <td>disabled</td>
+                        <td>boolean</td>
+                        <td>false</td>
+                        <td>When present, it specifies that the component should be disabled.</td>
+                    </tr>
+                    <tr>
+                        <td>tabindex</td>
+                        <td>number</td>
+                        <td>0</td>
+                        <td>Index of the element in tabbing order.</td>
+                    </tr>
+                    <tr>
+                        <td>aria-label</td>
+                        <td>string</td>
+                        <td>null</td>
+                        <td>Defines a string value that labels an interactive element.</td>
+                    </tr>
+                    <tr>
+                        <td>aria-labelledby</td>
+                        <td>string</td>
+                        <td>null</td>
+                        <td>Establishes relationships between the component and label(s) where its value should be one or more element IDs.</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -277,6 +301,51 @@ toggle(event) {
                         <td>hide</td>
                         <td>-</td>
                         <td>Hides the overlay.</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <h5>Events</h5>
+        <div class="doc-tablewrapper">
+            <table class="doc-table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Parameters</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>focus</td>
+                        <td>event</td>
+                        <td>Callback to invoke when the component receives focus.</td>
+                    </tr>
+                    <tr>
+                        <td>blur</td>
+                        <td>event</td>
+                        <td>Callback to invoke when the component loses focus.</td>
+                    </tr>
+                    <tr>
+                        <td>before-show</td>
+                        <td>-</td>
+                        <td>Callback to invoke before the popup is shown.</td>
+                    </tr>
+                    <tr>
+                        <td>before-hide</td>
+                        <td>-</td>
+                        <td>Callback to invoke before the popup is hidden.</td>
+                    </tr>
+                    <tr>
+                        <td>show</td>
+                        <td>-</td>
+                        <td>Callback to invoke when the popup is shown.</td>
+                    </tr>
+                    <tr>
+                        <td>hide</td>
+                        <td>-</td>
+                        <td>Callback to invoke when the popup is hidden.</td>
                     </tr>
                 </tbody>
             </table>
@@ -338,6 +407,98 @@ toggle(event) {
                     <tr>
                         <td>p-submenu-icon</td>
                         <td>Arrow icon of a submenu.</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <h5>Accessibility</h5>
+        <h6>Screen Reader</h6>
+        <p>
+            TieredMenu component uses the <i>menubar</i> role with <i>aria-orientation</i> set to "vertical" and the value to describe the menu can either be provided with <i>aria-labelledby</i> or <i>aria-label</i> props. Each list item has a
+            <i>menuitem</i> role with <i>aria-label</i> referring to the label of the item and <i>aria-disabled</i> defined if the item is disabled. A submenu within a TieredMenu uses the <i>menu</i> role with an <i>aria-labelledby</i> defined as the
+            id of the submenu root menuitem label. In addition, menuitems that open a submenu have <i>aria-haspopup</i> and <i>aria-expanded</i> to define the relation between the item and the submenu.
+        </p>
+
+        <p>In popup mode, the component implicitly manages the <i>aria-expanded</i>, <i>aria-haspopup</i> and <i>aria-controls</i> attributes of the target element to define the relation between the target and the popup.</p>
+
+        <h6>Keyboard Support</h6>
+        <div class="doc-tablewrapper">
+            <table class="doc-table">
+                <thead>
+                    <tr>
+                        <th>Key</th>
+                        <th>Function</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <i>tab</i>
+                        </td>
+                        <td>Add focus to the first item if focus moves in to the menu. If the focus is already within the menu, focus moves to the next focusable item in the page tab sequence.</td>
+                    </tr>
+                    <tr>
+                        <td><i>shift</i> + <i>tab</i></td>
+                        <td>Add focus to the last item if focus moves in to the menu. If the focus is already within the menu, focus moves to the previous focusable item in the page tab sequence.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i>enter</i>
+                        </td>
+                        <td>If menuitem has a submenu, opens the submenu otherwise activates the menuitem and closes all open overlays.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i>space</i>
+                        </td>
+                        <td>If menuitem has a submenu, opens the submenu otherwise activates the menuitem and closes all open overlays.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i>escape</i>
+                        </td>
+                        <td>If focus is inside a popup submenu, closes the submenu and moves focus to the root item of the closed submenu.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i>down arrow</i>
+                        </td>
+                        <td>Moves focus to the next menuitem within the submenu.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i>up arrow</i>
+                        </td>
+                        <td>Moves focus to the previous menuitem within the submenu.</td>
+                    </tr>
+                    <tr>
+                        <td><i>alt</i> + <i>up arrow</i></td>
+                        <td>Closes the popup, then moves focus to the target element.</td>
+                    </tr>
+                    <tr>
+                        <td><i>right arrow</i></td>
+                        <td>If option is closed, opens the option otherwise moves focus to the first child option.</td>
+                    </tr>
+                    <tr>
+                        <td><i>left arrow</i></td>
+                        <td>If option is open, closes the option otherwise moves focus to the parent option.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i>home</i>
+                        </td>
+                        <td>Moves focus to the first menuitem within the submenu.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i>end</i>
+                        </td>
+                        <td>Moves focus to the last menuitem within the submenu.</td>
+                    </tr>
+                    <tr>
+                        <td><i>any printable character</i></td>
+                        <td>Moves focus to the menuitem whose label starts with the characters being typed.</td>
                     </tr>
                 </tbody>
             </table>
