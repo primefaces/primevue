0af5b0464cb364b7393355188d2e2c8a026f7790
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-10-19 10:27:36 +0300

    update doc

--- src/views/contextmenu/ContextMenuDoc.vue
+++ src/views/contextmenu/ContextMenuDoc.vue
@@ -499,6 +499,10 @@ export default {
                         </td>
                         <td>Moves focus to the last menuitem within the submenu.</td>
                     </tr>
+                    <tr>
+                        <td><i>any printable character</i></td>
+                        <td>Moves focus to the menuitem whose label starts with the characters being typed.</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
