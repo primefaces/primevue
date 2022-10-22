0148e2087621752be1802852d9163b923f16a462
Ryan Conceicao <ryoid@users.noreply.github.com>
2022-10-22 17:17:33 +0000

    docs: fix nuxt config setup

--- src/views/setup/Setup.vue
+++ src/views/setup/Setup.vue
@@ -191,7 +191,10 @@ export default defineNuxtConfig({
         'primevue/resources/themes/saga-blue/theme.css',
         'primevue/resources/primevue.css',
         'primeicons/primeicons.css'
-    ]
+    ],
+	build: {
+		transpile: ['primevue']
+	}
 })
 
 </code></pre>
