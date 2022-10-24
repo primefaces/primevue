93bc680812b8199929f1526bd52ab23b23fb7fcd
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-10-24 16:08:23 +0300

    new dev iteration

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="layout-footer">
         <div class="layout-footer-left">
-            <span>PrimeVue 3.17.1-SNAPSHOT on Vue 3 by </span>
+            <span>PrimeVue 3.18.0 on Vue 3 by </span>
             <a href="https://www.primetek.com.tr">PrimeTek</a>
         </div>
 
