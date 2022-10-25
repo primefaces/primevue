a6e511098810aaa47889d787b63b10e2e0529dd1
Bahadır <45143524+bahadirsofuoglu@users.noreply.github.com>
2022-10-25 11:47:52 +0300

    Cannot all select when has a disabled option issue fixed (#3117)

--- src/components/multiselect/MultiSelect.vue
+++ src/components/multiselect/MultiSelect.vue
@@ -845,7 +845,7 @@ export default {
             if (this.selectAll !== null) {
                 this.$emit('selectall-change', { originalEvent: event, checked: !this.allSelected });
             } else {
-                const value = this.allSelected ? [] : this.visibleOptions.filter((option) => !this.isOptionGroup(option)).map((option) => this.getOptionValue(option));
+                const value = this.allSelected ? [] : this.visibleOptions.filter((option) => !this.isOptionDisabled(option) && !this.isOptionGroup(option)).map((option) => this.getOptionValue(option));
 
                 this.updateModel(event, value);
             }
@@ -1109,7 +1109,9 @@ export default {
             return label;
         },
         allSelected() {
-            return this.selectAll !== null ? this.selectAll : ObjectUtils.isNotEmpty(this.visibleOptions) && this.visibleOptions.every((option) => this.isOptionGroup(option) || this.isValidSelectedOption(option));
+            return this.selectAll !== null
+                ? this.selectAll
+                : ObjectUtils.isNotEmpty(this.visibleOptions) && this.visibleOptions.filter((option) => !this.isOptionDisabled(option)).every((option) => this.isOptionGroup(option) || this.isValidSelectedOption(option));
         },
         hasSelectedOption() {
             return ObjectUtils.isNotEmpty(this.modelValue);
