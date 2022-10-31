9fcc5cb2a9be50e296bfe83de64f9207229d7602
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-10-31 16:10:50 +0300

    new dev iteration

--- src/AppTopBar.vue
+++ src/AppTopBar.vue
@@ -223,11 +223,11 @@
                 <a href="https://www.primefaces.org/primeblocks-vue/#/" target="_blank"><span>Blocks</span></a>
             </li>
             <li class="topbar-submenu">
-                <a tabindex="0" @click="toggleMenu($event, 3)">v3.18.0</a>
+                <a tabindex="0" @click="toggleMenu($event, 3)">v3.18.1</a>
                 <transition name="p-connected-overlay" @enter="onMenuEnter">
                     <ul v-show="activeMenuIndex === 3" style="width: 100%">
                         <li>
-                            <router-link to="/"><span class="m-0">v3.18.0</span></router-link>
+                            <router-link to="/"><span class="m-0">v3.18.1</span></router-link>
                         </li>
                         <li>
                             <a href="https://www.primefaces.org/primevue-v2"><span class="m-0">v2.10.0</span></a>
