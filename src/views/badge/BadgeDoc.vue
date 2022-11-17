43101adb652834e2db6d6d8d843ae6decfa96f22
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-11-17 15:11:03 +0300

    refactor

--- src/views/badge/BadgeDoc.vue
+++ src/views/badge/BadgeDoc.vue
@@ -192,7 +192,7 @@ app.directive('badge', BadgeDirective);
         <h6>Screen Reader</h6>
         <p>
             Badge does not include any roles and attributes by default, any attribute is passed to the root element so aria roles and attributes can be added if required. If the badges are dynamic,
-            <i>aria-live</i> may be utilized as well. In case badges need to be tabbable, <i>tabIndex</i> can be added to implement custom key handlers.
+            <i>aria-live</i> may be utilized as well. In case badges need to be tabbable, <i>tabindex</i> can be added to implement custom key handlers.
         </p>
 
         <h5>Keyboard Support</h5>
