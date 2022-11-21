d8722e677ea13ae8d7165b431841d6ec292e6e7b
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-11-21 20:53:40 +0300

    Update PricingService.js

--- src/service/PricingService.js
+++ src/service/PricingService.js
@@ -1,6 +1,5 @@
 export default class PricingService {
     fetchPricing() {
-        //return fetch('https://www.primefaces.org/cdn/pricing/uikit.json', { cache: 'no-store' }).then((res) => res.json());
-        return fetch('demo/data/pricing.json', { cache: 'no-store' }).then((res) => res.json());
+        return fetch('https://www.primefaces.org/cdn/pricing/uikit.json', { cache: 'no-store' }).then((res) => res.json());
     }
 }
