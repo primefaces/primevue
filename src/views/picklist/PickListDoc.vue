94f0cbca7869fc4f7b74e500ca98b4e1792d90e2
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2022-11-30 15:31:49 +0300

    Format update for doc

--- src/views/picklist/PickListDoc.vue
+++ src/views/picklist/PickListDoc.vue
@@ -413,7 +413,8 @@ import PickList from 'primevue/picklist';
         <p>
             Controls buttons are <i>button</i> elements with an <i>aria-label</i> that refers to the <i>aria.moveTop</i>, <i>aria.moveUp</i>, <i>aria.moveDown</i>, <i>aria.moveBottom</i>,<i>aria.moveToTarget</i>, <i>aria.moveAllToTarget</i>,
             <i>aria.moveToSource</i> and <i>aria.moveAllToSource</i> properties of the <Link href="/locale">locale</Link> API by default, alternatively you may use <i>moveTopButtonProps</i>, <i>moveUpButtonProps</i>, <i>moveDownButtonProps</i>,
-            <i>moveToButtonProps</i>, <i>moveAllToButtonProps</i>, <i>moveFromButtonProps</i>, <i>moveFromButtonProps</i> <i>moveAllFromButtonProps</i> <i>moveToTargetProps</i>, <i>moveAllToTargetProps</i>, <i>moveToSourceProps</i> and{' '} <i>moveAllToSourceProps</i> to customize the buttons like overriding the default <i>aria-label</i> attributes.
+            <i>moveToButtonProps</i>, <i>moveAllToButtonProps</i>, <i>moveFromButtonProps</i>, <i>moveFromButtonProps</i> <i>moveAllFromButtonProps</i> <i>moveToTargetProps</i>, <i>moveAllToTargetProps</i>, <i>moveToSourceProps</i> and{' '}
+            <i>moveAllToSourceProps</i> to customize the buttons like overriding the default <i>aria-label</i> attributes.
         </p>
         <pre v-code><code><template v-pre>
 &lt;span id="lb"&gt;Options&lt;/span&gt;
