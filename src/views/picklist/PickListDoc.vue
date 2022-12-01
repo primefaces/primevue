73a9d717cb3d3f771fbf37528c3c1f96ffefe094
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-12-01 12:05:31 +0300

    refactor

--- src/views/picklist/PickListDoc.vue
+++ src/views/picklist/PickListDoc.vue
@@ -407,14 +407,14 @@ import PickList from 'primevue/picklist';
         <h5>Accessibility</h5>
         <h6>Screen Reader</h6>
         <p>
-            Value to describe the source listbox and target listbox can be provided with <i>sourceListProps</i> and <i>targetListProps</i> by passing <i>aria-labelledby</i> or <i>aria-label</i> props. The list elements has a{' '} <i>listbox</i> role
-            with the <i>aria-multiselectable</i> attribute. Each list item has an <i>option</i> role with <i>aria-selected</i> and <i>aria-disabled</i> as their attributes.
+            Value to describe the source listbox and target listbox can be provided with <i>sourceListProps</i> and <i>targetListProps</i> by passing <i>aria-labelledby</i> or <i>aria-label</i> props. The list elements has a <i>listbox</i> role with
+            the <i>aria-multiselectable</i> attribute. Each list item has an <i>option</i> role with <i>aria-selected</i> and <i>aria-disabled</i> as their attributes.
         </p>
         <p>
             Controls buttons are <i>button</i> elements with an <i>aria-label</i> that refers to the <i>aria.moveTop</i>, <i>aria.moveUp</i>, <i>aria.moveDown</i>, <i>aria.moveBottom</i>,<i>aria.moveToTarget</i>, <i>aria.moveAllToTarget</i>,
-            <i>aria.moveToSource</i> and <i>aria.moveAllToSource</i> properties of the <Link href="/locale">locale</Link> API by default, alternatively you may use <i>moveTopButtonProps</i>, <i>moveUpButtonProps</i>, <i>moveDownButtonProps</i>,
-            <i>moveToButtonProps</i>, <i>moveAllToButtonProps</i>, <i>moveFromButtonProps</i>, <i>moveFromButtonProps</i> <i>moveAllFromButtonProps</i> <i>moveToTargetProps</i>, <i>moveAllToTargetProps</i>, <i>moveToSourceProps</i> and{' '}
-            <i>moveAllToSourceProps</i> to customize the buttons like overriding the default <i>aria-label</i> attributes.
+            <i>aria.moveToSource</i> and <i>aria.moveAllToSource</i> properties of the <router-link to="/locale">locale</router-link> API by default, alternatively you may use <i>moveTopButtonProps</i>, <i>moveUpButtonProps</i>,
+            <i>moveDownButtonProps</i>, <i>moveToButtonProps</i>, <i>moveAllToButtonProps</i>, <i>moveFromButtonProps</i>, <i>moveFromButtonProps</i> <i>moveAllFromButtonProps</i> <i>moveToTargetProps</i>, <i>moveAllToTargetProps</i>,
+            <i>moveToSourceProps</i> and <i>moveAllToSourceProps</i> to customize the buttons like overriding the default <i>aria-label</i> attributes.
         </p>
         <pre v-code><code><template v-pre>
 &lt;span id="lb"&gt;Options&lt;/span&gt;
@@ -424,8 +424,8 @@ import PickList from 'primevue/picklist';
 
 </template></code></pre>
         <h6>ListBox Keyboard Support</h6>
-        <div className="doc-tablewrapper">
-            <table className="doc-table">
+        <div class="doc-tablewrapper">
+            <table class="doc-table">
                 <thead>
                     <tr>
                         <th>Key</th>
@@ -504,8 +504,8 @@ import PickList from 'primevue/picklist';
         </div>
 
         <h6>Buttons Keyboard Support</h6>
-        <div className="doc-tablewrapper">
-            <table className="doc-table">
+        <div class="doc-tablewrapper">
+            <table class="doc-table">
                 <thead>
                     <tr>
                         <th>Key</th>
