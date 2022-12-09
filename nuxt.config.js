3e6476c735bbf2ef0ea383b262df2becc50b6093
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2022-12-09 16:12:32 +0300

    path removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,12 +14,12 @@ export default defineNuxtConfig({
                 {
                     id: 'home-table-link',
                     rel: 'stylesheet',
-                    href: baseUrl + '/styles/landing/themes/lara-light-blue/theme.css'
+                    href: baseUrl + 'styles/landing/themes/lara-light-blue/theme.css'
                 },
                 {
                     id: 'theme-link',
                     rel: 'stylesheet',
-                    href: baseUrl + `/themes/lara-light-blue/theme.css`
+                    href: baseUrl + `themes/lara-light-blue/theme.css`
                 }
             ]
         }
