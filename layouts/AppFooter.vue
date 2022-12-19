fbb8ac01047187a536b43eb488eb3bbb28f88629
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-12-19 17:38:18 +0300

    Update AppFooter.vue

--- layouts/AppFooter.vue
+++ layouts/AppFooter.vue
@@ -2,19 +2,19 @@
     <div class="layout-footer">
         <div class="layout-footer-left">
             <span>PrimeVue 3.21.0 on Vue 3 by </span>
-            <nuxt-link to="https://www.primetek.com.tr">PrimeTek</nuxt-link>
+            <a href="https://www.primetek.com.tr">PrimeTek</a>
         </div>
 
         <div class="layout-footer-right">
-            <nuxt-link to="https://github.com/primefaces/primevue" class="mr-3">
+            <a href="https://github.com/primefaces/primevue" class="mr-3">
                 <i class="pi pi-github"></i>
-            </nuxt-link>
-            <nuxt-link to="https://twitter.com/primevue" class="mr-3">
+            </a>
+            <a href="https://twitter.com/primevue" class="mr-3">
                 <i class="pi pi-twitter"></i>
-            </nuxt-link>
-            <nuxt-link to="https://discord.com/invite/gzKFYnpmCY">
+            </a>
+            <a href="https://discord.com/invite/gzKFYnpmCY">
                 <i class="pi pi-discord"></i>
-            </nuxt-link>
+            </a>
         </div>
     </div>
 </template>
