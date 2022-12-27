340f2652f83b8f680245a6e3207cf9f5f32d60a1
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-12-27 16:04:03 +0300

    refactor

--- middleware/route.global.js
+++ middleware/route.global.js
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     useNuxtApp().hook('page:finish', () => {
-        if (to.path !== from.path && history.state) {
+        if (to.path !== from.path && history.state.scroll) {
             window.scrollTo(history.state.scroll);
         } else {
             window.scrollTo(0, 0);
