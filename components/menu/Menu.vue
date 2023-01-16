ea4a2782e11e6dd57990d32eb0387bf144e84059
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-01-16 13:07:17 +0300

    Fixed #3513 - Menu: TypeError with only disabled menuitems

--- components/menu/Menu.vue
+++ components/menu/Menu.vue
@@ -252,7 +252,7 @@ export default {
             const links = DomHandler.find(this.container, 'li.p-menuitem:not(.p-disabled)');
             let order = index >= links.length ? links.length - 1 : index < 0 ? 0 : index;
 
-            this.focusedOptionIndex = links[order].getAttribute('id');
+            order > -1 && (this.focusedOptionIndex = links[order].getAttribute('id'));
         },
         toggle(event) {
             if (this.overlayVisible) this.hide();
