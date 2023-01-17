019cbabf106a6220552cdc13979cdf6a92702036
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-01-17 15:41:08 +0300

    set new version

--- pages/liveeditor/LiveEditor.vue
+++ pages/liveeditor/LiveEditor.vue
@@ -339,7 +339,7 @@ export default {
                             dependencies: {
                                 ...extDependencies,
                                 vue: '3.2.31',
-                                primevue: '^3.22.2',
+                                primevue: '^3.22.3',
                                 primeflex: dependencies.devDependencies['primeflex'],
                                 primeicons: dependencies.devDependencies['primeicons'],
                                 '@babel/cli': '^7.4.4',
