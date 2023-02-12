44751bdd2b7b3765ffa541599bfd5be79752d50f
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-02-12 15:20:36 +0300

    Replace link removed

--- components/usedynamictheme/UseDynamicTheme.js
+++ components/usedynamictheme/UseDynamicTheme.js
@@ -1,6 +1,6 @@
 export function useDynamicTheme() {
     const setTheme = (themeName) => {
-        replaceLink(themeName);
+        replaceHref(themeName);
     };
 
     return {
@@ -8,35 +8,8 @@ export function useDynamicTheme() {
     };
 }
 
-const replaceLink = (themeName) => {
+const replaceHref = (themeName) => {
     const linkElement = document.getElementById('theme-link');
-    const linkHrefLastIndex = linkElement.href.lastIndexOf('/');
-    const href = removeBaseUrl(linkElement.href.substring(0, linkHrefLastIndex) + '/') + themeName + '.css';
 
-    if (!linkElement || !href) {
-        return;
-    }
-
-    const id = linkElement.getAttribute('id');
-
-    const cloneLinkElement = linkElement.cloneNode(true);
-
-    cloneLinkElement.setAttribute('href', href);
-    cloneLinkElement.setAttribute('id', id + '-clone');
-
-    linkElement.parentNode.insertBefore(cloneLinkElement, linkElement.nextSibling);
-
-    cloneLinkElement.addEventListener('load', () => {
-        linkElement.remove();
-
-        const element = document.getElementById(id); // re-check
-
-        element && element.remove();
-
-        cloneLinkElement.setAttribute('id', id);
-    });
+    linkElement.setAttribute('href', themeName + '.css');
 };
-
-function removeBaseUrl(url) {
-    return url.replace(/^(?:\/\/|[^\/]+)*\//, '');
-}
