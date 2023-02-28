ed1ae7595f43e1668b26dc67995e1d44bd738249
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-02-28 18:38:13 +0300

    Deferredcontent d.ts updated

--- components/deferredcontent/DeferredContent.d.ts
+++ components/deferredcontent/DeferredContent.d.ts
@@ -1,23 +1,49 @@
+/**
+ *
+ * DeferredContent postpones the loading the content that is initially not in the viewport until it becomes visible on scroll.
+ *
+ * [Live Demo](https://www.primevue.org/deferredcontent/)
+ *
+ * @module deferredcontent
+ *
+ */
 import { VNode } from 'vue';
 import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
 export interface DeferredContentProps {}
 
+/**
+ * Defines valid slots in DeferredContent component.
+ */
 export interface DeferredContentSlots {
     /**
      * Default content slot.
      */
-    default: () => VNode[];
+    default(): VNode[];
 }
 
-export declare type DeferredContentEmits = {
+/**
+ * Defines valid emits in DeferredContent component.
+ */
+export interface DeferredContentEmits {
     /**
      * Callback to invoke when deferred content is loaded.
      */
-    load: () => void;
-};
+    load(): void;
+}
 
-declare class DeferredContent extends ClassComponent<DeferredContentProps, DeferredContentSlots, DeferredContentEmits> {}
+/**
+ * **PrimeVue - DeferredContent**
+ *
+ * _DeferredContent postpones the loading the content that is initially not in the viewport until it becomes visible on scroll._
+ *
+ * [Live Demo](https://www.primevue.org/deferredcontent/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo.svg)
+ *
+ * @group Component
+ */
+export declare class DeferredContent extends ClassComponent<DeferredContentProps, DeferredContentSlots, DeferredContentEmits> {}
 
 declare module '@vue/runtime-core' {
     interface GlobalComponents {
