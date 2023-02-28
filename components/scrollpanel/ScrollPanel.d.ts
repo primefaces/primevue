d74b3a20c536553f102385307894eb1e0734b2db
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-02-28 20:39:29 +0300

    ScrollPanel d.ts updated

--- components/scrollpanel/ScrollPanel.d.ts
+++ components/scrollpanel/ScrollPanel.d.ts
@@ -1,14 +1,29 @@
+/**
+ *
+ * ScrollPanel is a cross browser, lightweight and themable alternative to native browser scrollbar.
+ *
+ * [Live Demo](https://www.primevue.org/scrollpanel/)
+ *
+ * @module scrollpanel
+ *
+ */
 import { VNode } from 'vue';
 import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
+/**
+ * Defines valid properties in ScrollPanel component.
+ */
 export interface ScrollPanelProps {
     /**
      * Step factor to scroll the content while pressing the arrow keys.
-     * Default value is 5.
+     * @defaultValue 5
      */
     step?: number | undefined;
 }
 
+/**
+ * Defines valid slots in Accordion slots.
+ */
 export interface ScrollPanelSlots {
     /**
      * Custom content slot.
@@ -16,9 +31,21 @@ export interface ScrollPanelSlots {
     default: () => VNode[];
 }
 
-export declare type ScrollPanelEmits = {};
+export interface ScrollPanelEmits {}
 
-declare class ScrollPanel extends ClassComponent<ScrollPanelProps, ScrollPanelSlots, ScrollPanelEmits> {}
+/**
+ * **PrimeVue - ScrollPanel**
+ *
+ * _ScrollPanel is a cross browser, lightweight and themable alternative to native browser scrollbar.._
+ *
+ * [Live Demo](https://www.primevue.org/scrollpanel/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo.svg)
+ *
+ * @group Component
+ *
+ */
+export declare class ScrollPanel extends ClassComponent<ScrollPanelProps, ScrollPanelSlots, ScrollPanelEmits> {}
 
 declare module '@vue/runtime-core' {
     interface GlobalComponents {
