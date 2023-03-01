f8d8a968acbf18d16e57a57478964588d15502b6
mertsincan <sincan.mert@gmail.com>
2023-03-01 11:23:31 +0000

    TriStateCheckbox d.ts updated

--- components/tristatecheckbox/TriStateCheckbox.d.ts
+++ components/tristatecheckbox/TriStateCheckbox.d.ts
@@ -1,13 +1,27 @@
+/**
+ *
+ * TriStateCheckbox is used to select either 'true', 'false' or 'null' as the value.
+ *
+ * [Live Demo](https://www.primevue.org/tristatecheckbox/)
+ *
+ * @module tristatecheckbox
+ *
+ */
 import { InputHTMLAttributes } from 'vue';
 import { ClassComponent, GlobalComponentConstructor, Nullable } from '../ts-helpers';
 
+/**
+ * Defines valid properties in TriStateCheckbox component.
+ */
 export interface TriStateCheckboxProps {
     /**
      * Value of the component.
+     * @defaultValue null
      */
     modelValue?: Nullable<boolean>;
     /**
      * When present, it specifies that the component should be disabled.
+     * @defaultValue false
      */
     disabled?: boolean | undefined;
     /**
@@ -32,16 +46,34 @@ export interface TriStateCheckboxProps {
     'aria-label'?: string | undefined;
 }
 
+/**
+ * Defines valid slots in TriStateCheckbox component.
+ */
 export interface TriStateCheckboxSlots {}
 
-export declare type TriStateCheckboxEmits = {
+/**
+ * Defines valid emits in TriStateCheckbox component.
+ */
+export interface TriStateCheckboxEmits {
     /**
      * Emitted when the value changes.
      * @param {boolean|null|undefined} value - New value.
      */
-    'update:modelValue': (value: Nullable<boolean>) => void;
-};
+    'update:modelValue'(value: Nullable<boolean>): void;
+}
 
+/**
+ * **PrimeVue - TriStateCheckbox**
+ *
+ * _TriStateCheckbox is used to select either 'true', 'false' or 'null' as the value._
+ *
+ * [Live Demo](https://www.primevue.org/tristatecheckbox/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo.svg)
+ *
+ * @group Component
+ *
+ */
 declare class TriStateCheckbox extends ClassComponent<TriStateCheckboxProps, TriStateCheckboxSlots, TriStateCheckboxEmits> {}
 
 declare module '@vue/runtime-core' {
@@ -50,13 +82,4 @@ declare module '@vue/runtime-core' {
     }
 }
 
-/**
- *
- * TriStateCheckbox is used to select either 'true', 'false' or 'null' as the value.
- *
- * Demos:
- *
- * - [TriStateCheckbox](https://www.primefaces.org/primevue/tristatecheckbox)
- *
- */
 export default TriStateCheckbox;
