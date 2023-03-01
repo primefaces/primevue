692dc38c13f5df106a8c9e05e281bbc565148545
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-01 17:39:42 +0300

    ColumnGroup .d.ts updated

--- components/columngroup/ColumnGroup.d.ts
+++ components/columngroup/ColumnGroup.d.ts
@@ -1,8 +1,15 @@
 /**
+ * Columns can be grouped at header and footer sections by defining a ColumnGroup component.
+ * It is a helper component for DataTable.
+ *
+ * [Live Demo](https://www.primevue.org/datatable/)
  * @module columngroup
  */
 import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
+/**
+ * Defines valid properties in ColumnGroup component.
+ */
 export interface ColumnGroupProps {
     /**
      * Type of column group
@@ -10,10 +17,27 @@ export interface ColumnGroupProps {
     type?: 'header' | 'footer' | undefined;
 }
 
+/**
+ * Defines valid slots in ColumnGroup component.
+ */
 export interface ColumnGroupSlots {}
 
+/**
+ * Defines valid emits in ColumnGroup component.
+ */
 export interface ColumnGroupEmits {}
 
+/**
+ * **PrimeVue - ColumnGroup**
+ *
+ * _It is a helper component for DataTable._
+ *
+ * [Live Demo](https://www.primevue.org/datatable/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo-100.png)
+ *
+ * @group Component
+ */
 declare class ColumnGroup extends ClassComponent<ColumnGroupProps, ColumnGroupSlots, ColumnGroupEmits> {}
 
 declare module '@vue/runtime-core' {
