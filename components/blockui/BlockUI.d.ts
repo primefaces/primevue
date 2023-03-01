df5a35a27795fc003baecd5b3b58fe86208f1848
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-01 16:00:49 +0300

    BlockUI .d.ts. updated

--- components/blockui/BlockUI.d.ts
+++ components/blockui/BlockUI.d.ts
@@ -1,6 +1,18 @@
+/**
+ *
+ * BlockUI represents people using icons, labels and images.
+ *
+ * [Live Demo](https://www.primevue.org/blockui)
+ *
+ * @module blockui
+ *
+ */
 import { VNode } from 'vue';
 import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
+/**
+ * Defines valid properties in BlockUI component
+ */
 export interface BlockUIProps {
     /**
      * Controls the blocked state.
@@ -12,34 +24,51 @@ export interface BlockUIProps {
     fullScreen?: boolean | undefined;
     /**
      * Base zIndex value to use in layering.
-     * Default value is 0.
+     * @defaultValue 0
      */
     baseZIndex?: number | undefined;
     /**
      * Whether to automatically manage layering.
-     * Default value is true.
+     * @defaultValue true
      */
     autoZIndex?: boolean | undefined;
 }
 
+/**
+ * Defines valid slots in BlockUI component
+ */
 export interface BlockUISlots {
     /**
      * Custom content's slot.
      */
-    default: () => VNode[];
+    default(): VNode[];
 }
 
-export declare type BlockUIEmits = {
+/**
+ * Defines valid emits in BlockUI component
+ */
+export interface BlockUIEmits {
     /**
      * Fired when the element gets blocked.
      */
-    block: () => void;
+    block(): void;
     /**
      * Fired when the element gets unblocked.
      */
-    unblock: () => void;
-};
+    unblock(): void;
+}
 
+/**
+ * **PrimeVue - BlockUI**
+ *
+ * _BlockUI represents people using icons, labels and images._
+ *
+ * [Live Demo](https://www.primevue.org/blockui/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo-100.png)
+ *
+ * @group Component
+ */
 declare class BlockUI extends ClassComponent<BlockUIProps, BlockUISlots, BlockUIEmits> {}
 
 declare module '@vue/runtime-core' {
