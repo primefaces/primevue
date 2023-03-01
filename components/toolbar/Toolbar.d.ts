da15632f357e70601e11c92cfd9ed9483d96f7a1
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-03-01 11:09:50 +0300

    Toolbar d.ts updated

--- components/toolbar/Toolbar.d.ts
+++ components/toolbar/Toolbar.d.ts
@@ -1,6 +1,18 @@
+/**
+ *
+ * Toolbar is a grouping component for buttons and other content.
+ *
+ * [Live Demo](https://www.primevue.org/toolbar/)
+ *
+ * @module toolbar
+ *
+ */
 import { VNode } from 'vue';
 import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
+/**
+ * Defines valid properties in Toolbar component.
+ */
 export interface ToolbarProps {
     /**
      * Defines a string value that labels an interactive element.
@@ -8,26 +20,43 @@ export interface ToolbarProps {
     'aria-labelledby'?: string | undefined;
 }
 
+/**
+ * Defines valid slots in Toolbar slots.
+ */
 export interface ToolbarSlots {
     /**
      * Custom start template.
      */
-    start: () => VNode[];
+    start(): VNode[];
 
     /**
      * Custom center template.
      */
-    center: () => VNode[];
+    center(): VNode[];
 
     /**
      * Custom end template.
      */
-    end: () => VNode[];
+    end(): VNode[];
 }
+/**
+ * Defines valid emits in Toolbar component.
+ */
+export interface ToolbarEmits {}
 
-export declare type ToolbarEmits = {};
-
-declare class Toolbar extends ClassComponent<ToolbarProps, ToolbarSlots, ToolbarEmits> {}
+/**
+ * **PrimeVue - Toolbar**
+ *
+ * _Toolbar is a grouping component for buttons and other content._
+ *
+ * [Live Demo](https://www.primevue.org/toolbar/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo.svg)
+ *
+ * @group Component
+ *
+ */
+export declare class Toolbar extends ClassComponent<ToolbarProps, ToolbarSlots, ToolbarEmits> {}
 
 declare module '@vue/runtime-core' {
     interface GlobalComponents {
