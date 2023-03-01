095f600cc834fd38316a1cc4cb56f015da849ebb
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-01 16:12:09 +0300

    Chip .d.ts updated

--- components/chip/Chip.d.ts
+++ components/chip/Chip.d.ts
@@ -1,6 +1,18 @@
+/**
+ *
+ * Chip represents people using icons, labels and images.
+ *
+ * [Live Demo](https://www.primevue.org/chip)
+ *
+ * @module chip
+ *
+ */
 import { VNode } from 'vue';
 import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
+/**
+ * Defines valid properties in Chip component.
+ */
 export interface ChipProps {
     /**
      * Defines the text to display.
@@ -16,30 +28,48 @@ export interface ChipProps {
     image?: string;
     /**
      * Whether to display a remove icon.
+     * @defaultValue false
      */
     removable?: boolean;
     /**
      * Icon of the remove element.
-     * Default value is 'pi pi-times-circle'.
+     * @defaultValue pi pi-times-circle
      */
     removeIcon?: string;
 }
 
+/**
+ * Defines valid slots in Chip component.
+ */
 export interface ChipSlots {
     /**
      * Content can easily be customized with the default slot instead of using the built-in modes.
      */
-    default: () => VNode[];
+    default(): VNode[];
 }
 
-export declare type ChipEmits = {
+/**
+ * Defines valid properties in Chip component.
+ */
+export interface ChipEmits {
     /**
      * Callback to invoke when a chip is removed.
      * @param {Event} event - Browser event.
      */
-    remove: (event: Event) => void;
-};
+    remove(event: Event): void;
+}
 
+/**
+ * **PrimeVue - Chip**
+ *
+ * _Chip represents people using icons, labels and images._
+ *
+ * [Live Demo](https://www.primevue.org/chip/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo-100.png)
+ *
+ * @group Component
+ */
 declare class Chip extends ClassComponent<ChipProps, ChipSlots, ChipEmits> {}
 
 declare module '@vue/runtime-core' {
@@ -48,13 +78,4 @@ declare module '@vue/runtime-core' {
     }
 }
 
-/**
- *
- * Chip represents entities using icons, labels and images.
- *
- * Demos:
- *
- * - [Chip](https://www.primefaces.org/primevue/chip)
- *
- */
 export default Chip;
