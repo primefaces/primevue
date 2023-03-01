7418526f6950672d36dad2c3600f5bb4404f12f7
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-01 17:13:01 +0300

    Skeleton .d.ts updated

--- components/skeleton/Skeleton.d.ts
+++ components/skeleton/Skeleton.d.ts
@@ -1,28 +1,35 @@
+/**
+ *
+ * Skeleton is a placeholder to display instead of the actual content.
+ *
+ * [Live Demo](https://www.primevue.org/skeleton/)
+ *
+ * @module skeleton
+ *
+ */
 import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
-type SkeletonShapeType = 'rectangle' | 'circle' | undefined;
-
-type SkeletonAnimationType = 'wave' | 'none' | undefined;
-
+/**
+ * Defines valid properties in Skeleton component.
+ */
 export interface SkeletonProps {
     /**
      * Shape of the element.
-     * @see SkeletonShapeType
-     * Default value is 'rectangle'.
+     * @defaultValue rectangle
      */
-    shape?: SkeletonShapeType;
+    shape?: 'rectangle' | 'circle' | undefined;
     /**
      * Size of the Circle or Square.
      */
     size?: string | undefined;
     /**
      * Width of the element.
-     * Default value is '100%'.
+     * @defaultValue 100%
      */
     width?: string | undefined;
     /**
      * Height of the element.
-     * Default value is '1rem'.
+     * @defaultValue 1rem
      */
     height?: string | undefined;
     /**
@@ -31,16 +38,32 @@ export interface SkeletonProps {
     borderRadius?: string | undefined;
     /**
      * Type of the animation.
-     * @see SkeletonAnimationType
-     * Default value is 'wave'.
+     * @defaultValue wave
      */
-    animation?: SkeletonAnimationType;
+    animation?: 'wave' | 'none' | undefined;
 }
 
+/**
+ * Defines valid slots in Skeleton component.
+ */
 export interface SkeletonSlots {}
 
-export declare type SkeletonEmits = {};
+/**
+ * Defines valid emits in Skeleton component.
+ */
+export interface SkeletonEmits {}
 
+/**
+ * **PrimeVue - Skeleton**
+ *
+ * _Skeleton is a placeholder to display instead of the actual content._
+ *
+ * [Live Demo](https://www.primevue.org/skeleton/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo-100.png)
+ *
+ * @group Component
+ */
 declare class Skeleton extends ClassComponent<SkeletonProps, SkeletonSlots, SkeletonEmits> {}
 
 declare module '@vue/runtime-core' {
@@ -49,13 +72,4 @@ declare module '@vue/runtime-core' {
     }
 }
 
-/**
- *
- * Skeleton is a placeholder to display instead of the actual content.
- *
- * Demos:
- *
- * - [Skeleton](https://www.primefaces.org/primevue/skeleton)
- *
- */
 export default Skeleton;
