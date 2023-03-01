a8970d5a1fa5b55670d3f9a5c6f1f5781baeba58
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-01 17:13:50 +0300

    ProgresSpinner description fixes

--- pages/progressspinner/index.vue
+++ pages/progressspinner/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <DocComponent title="Vue ProgressBar Component" header="ProgressBar" description="ProgressBar is a process status indicator." :componentDocs="docs" :apiDocs="['ProgressBar']" />
+    <DocComponent title="Vue ProgressSpinner Component" header="ProgressSpinner" description="ProgressBar is a process status indicator." :componentDocs="docs" :apiDocs="['ProgressSpinner']" />
 </template>
 
 <script>
