2a3e15de17294897a15685559d099c1cd647a630
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-01 16:12:34 +0300

    ScrollTop .d.ts updated

--- components/scrolltop/ScrollTop.d.ts
+++ components/scrolltop/ScrollTop.d.ts
@@ -1,35 +1,61 @@
+/**
+ *
+ * ScrollTop gets displayed after a certain scroll position and used to navigates to the top of the page quickly.
+ *
+ * [Live Demo](https://www.primevue.org/scrolltop/)
+ *
+ * @module scrolltop
+ *
+ */
 import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
-type ScrollTopTargetType = 'window' | 'parent' | undefined;
-
+/**
+ * Defines valid properties in ScrollTop component.
+ */
 export interface ScrollTopProps {
     /**
      * Target of the ScrollTop.
-     * @see ScrollTopTargetType
-     * Default value is 'window'.
+     * @defaultValue window
      */
-    target?: ScrollTopTargetType;
+    target?: 'window' | 'parent' | undefined;
     /**
      * Defines the threshold value of the vertical scroll position of the target to toggle the visibility.
-     * Default value is 400.
+     * @defaultValue 400
      */
     threshold?: number | undefined;
     /**
      * Icon to display.
-     * Default value is 'pi pi-chevron-up'.
+     * @defaultValue pi pi-chevron-up
      */
     icon?: string | undefined;
     /**
      * Defines the scrolling behaviour, 'smooth' adds an animation and 'auto' scrolls with a jump.
-     * Default value is 'smooth'.
+     * @defaultValue smooth
      */
     behavior?: string | undefined;
 }
 
+/**
+ * Defines valid slots in ScrollTop component.
+ */
 export interface ScrollTopSlots {}
 
-export declare type ScrollTopEmits = {};
+/**
+ * Defines valid emits in ScrollTop component.
+ */
+export interface ScrollTopEmits {}
 
+/**
+ * **PrimeVue - ScrollTop**
+ *
+ * _ScrollTop gets displayed after a certain scroll position and used to navigates to the top of the page quickly._
+ *
+ * [Live Demo](https://www.primevue.org/scrolltop/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo-100.png)
+ *
+ * @group Component
+ */
 declare class ScrollTop extends ClassComponent<ScrollTopProps, ScrollTopSlots, ScrollTopEmits> {}
 
 declare module '@vue/runtime-core' {
@@ -38,13 +64,4 @@ declare module '@vue/runtime-core' {
     }
 }
 
-/**
- *
- * ScrollTop gets displayed after a certain scroll position and used to navigates to the top of the page quickly.
- *
- * Demos:
- *
- * - [ScrollTop](https://www.primefaces.org/primevue/scrolltop)
- *
- */
 export default ScrollTop;
