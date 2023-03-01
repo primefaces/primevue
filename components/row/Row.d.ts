d92ca851081f59ee2a922d88747424b07b2aa8f0
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-01 17:39:50 +0300

    Row .d.ts updated

--- components/row/Row.d.ts
+++ components/row/Row.d.ts
@@ -1,14 +1,38 @@
 /**
+ * Row component is a helper component used to create grouping structures in DataTable.
+ *
+ * [Live Demo](https://www.primereact.org/datatable/)
+ *
  * @module row
  */
 import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
 
+/**
+ * Defines valid properties in Row component.
+ */
 export interface RowProps {}
 
+/**
+ * Defines valid slots in Row component.
+ */
 export interface RowSlots {}
 
+/**
+ * Defines valid emits in Row component.
+ */
 export interface RowEmits {}
 
+/**
+ * **PrimeVue - Row**
+ *
+ * _Row component is a helper component used to create grouping structures in DataTable._
+ *
+ * [Live Demo](https://www.primevue.org/datatable/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo-100.png)
+ *
+ * @group Component
+ */
 declare class Row extends ClassComponent<RowProps, RowSlots, RowEmits> {}
 
 declare module '@vue/runtime-core' {
@@ -17,13 +41,4 @@ declare module '@vue/runtime-core' {
     }
 }
 
-/**
- *
- * Row is a helper component to create column group.
- *
- * Demos:
- *
- * - [DataTable](https://www.primefaces.org/primevue/datatable/colgroup)
- *
- */
 export default Row;
