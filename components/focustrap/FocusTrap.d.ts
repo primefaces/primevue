82883bcfc1cebc5ad0168f2bb861a149cd6478b1
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-02 14:35:08 +0300

    FocusTrap .d.ts updated

--- components/focustrap/FocusTrap.d.ts
+++ components/focustrap/FocusTrap.d.ts
@@ -1,5 +1,42 @@
-import { ObjectDirective } from 'vue';
+/**
+ *
+ * Focus Trap keeps focus within a certain DOM element while tabbing.
+ *
+ * - [Live Demo](https://primevue.org/focustrap)
+ */
+import { DirectiveBinding, ObjectDirective } from 'vue';
 
-declare const Ripple: ObjectDirective;
+/**
+ * Defines options of FocusTrap.
+ */
+export interface FocusTrapOptions {
+    /**
+     * When present, it specifies that the directive should be disabled.
+     * @defaultValue false
+     */
+    disabled?: boolean | undefined;
+}
 
-export default Ripple;
+/**
+ * Binding of FocusTrap directive.
+ */
+export interface FocusTrapDirectiveBinding extends Omit<DirectiveBinding, 'modifiers' | 'value'> {
+    /**
+     * Value of the FocusTrap.
+     */
+    value?: FocusTrapOptions | undefined;
+}
+
+/**
+ * **PrimeVue - FocusTrap**
+ *
+ * _FocusTrap directive provides advisory information for a component._
+ *
+ * [Live Demo](https://www.primevue.org/focustrap/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo.svg)
+ *
+ */
+declare const FocusTrap: ObjectDirective;
+
+export default FocusTrap;
