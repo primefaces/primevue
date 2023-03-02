d1f7cd4dca926fddb0025e5044d34ef7bd984a7e
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-02 14:11:01 +0300

    Ripple .d.ts updated

--- components/ripple/Ripple.d.ts
+++ components/ripple/Ripple.d.ts
@@ -1,5 +1,26 @@
-import { ObjectDirective } from 'vue';
+/**
+ *
+ * Ripple directive adds ripple effect to the host element.
+ *
+ * - [Live Demo](https://primevue.org/ripple)
+ */
+import { DirectiveBinding, ObjectDirective } from 'vue';
 
+/**
+ * Binding of Ripple directive.
+ */
+export interface RippleDirectiveBinding extends Omit<DirectiveBinding, 'modifiers' | 'value'> {}
+
+/**
+ * **PrimeVue - Ripple**
+ *
+ * _Ripple directive adds ripple effect to the host element._
+ *
+ * [Live Demo](https://www.primevue.org/ripple/)
+ * --- ---
+ * ![PrimeVue](https://primefaces.org/cdn/primevue/images/logo.svg)
+ *
+ */
 declare const Ripple: ObjectDirective;
 
 export default Ripple;
