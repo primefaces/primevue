3ddbf017f6e7e5e3ea723aa1143b5c784d99e8b2
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-03-08 15:26:10 +0300

    Update Menubar header

--- pages/menubar/index.vue
+++ pages/menubar/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <DocComponent title="Vue Navbar Component" header="Navbar" description="Menubar also known as Navbar, is a horizontal menu component." :componentDocs="docs" :apiDocs="['Menubar', 'MenuItem']" />
+    <DocComponent title="Vue Navbar Component" header="Menubar" description="Menubar also known as Navbar, is a horizontal menu component." :componentDocs="docs" :apiDocs="['Menubar', 'MenuItem']" />
 </template>
 
 <script>
