d9e769cddd0c1e15ab40150b84c9f91e4a65b454
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-04-10 17:00:54 +0300

    Fieldset & ScrollPanel pt tab added

---
+++ doc/fieldset/pt/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="doc-main">
+        <div class="doc-intro">
+            <h1>Fieldset Pass Through</h1>
+        </div>
+        <DocSections :docs="docs" />
+    </div>
+    <DocSectionNav :docs="docs" />
+</template>
+
+<script>
+import DocApiTable from '@/components/doc/DocApiTable.vue';
+import { getPTOption } from '@/components/doc/helpers/PTHelper.js';
+import PtDoc from './PTDoc.vue';
+import PTImage from './PTImage.vue';
+
+export default {
+    data() {
+        return {
+            docs: [
+                {
+                    id: 'pt.image',
+                    label: 'WireFrame',
+                    component: PTImage
+                },
+                {
+                    id: 'pt.doc',
+                    label: 'Fieldset Elements',
+                    component: DocApiTable,
+                    data: getPTOption('Fieldset')
+                },
+                {
+                    id: 'pt.demo',
+                    label: 'Example',
+                    component: PtDoc
+                }
+            ]
+        };
+    }
+};
+</script>
