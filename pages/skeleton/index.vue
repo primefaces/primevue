2141a3af616ca6028789accfd16cadcb088ec986
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-04-27 14:56:31 +0300

    Skeleton pt demo added

--- pages/skeleton/index.vue
+++ pages/skeleton/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <DocComponent title="Vue Skeleton Component" header="Skeleton" description="Skeleton is a placeholder to display instead of the actual content." :componentDocs="docs" :apiDocs="['Skeleton']" />
+    <DocComponent title="Vue Skeleton Component" header="Skeleton" description="Skeleton is a placeholder to display instead of the actual content." :componentDocs="docs" :apiDocs="['Skeleton']" :ptTabComponent="ptComponent" />
 </template>
 
 <script>
@@ -10,6 +10,7 @@ import ImportDoc from '@/doc/skeleton/ImportDoc.vue';
 import ListDoc from '@/doc/skeleton/ListDoc.vue';
 import ShapesDoc from '@/doc/skeleton/ShapesDoc.vue';
 import StyleDoc from '@/doc/skeleton/StyleDoc.vue';
+import PTComponent from '@/doc/skeleton/pt/index.vue';
 
 export default {
     data() {
@@ -50,7 +51,8 @@ export default {
                     label: 'Accessibility',
                     component: Accessibility
                 }
-            ]
+            ],
+            ptComponent: PTComponent
         };
     }
 };
