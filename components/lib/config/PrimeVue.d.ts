b29ad8db8b9e1025a985f7edec281b1ff425eb21
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-05-03 17:40:21 +0300

    Update PrimeVue.d.ts

--- components/lib/config/PrimeVue.d.ts
+++ components/lib/config/PrimeVue.d.ts
@@ -1,6 +1,49 @@
 import { Plugin } from 'vue';
+import { AccordionPassThroughOptions } from '../accordion';
+import { AccordionTabPassThroughOptions } from '../accordiontab';
+import { AvatarPassThroughOptions } from '../avatar';
+import { BadgePassThroughOptions } from '../badge';
+import { BlockUIPassThroughOptions } from '../blockui';
+import { BreadcrumbPassThroughOptions } from '../breadcrumb';
+import { ButtonPassThroughOptions } from '../button';
 import { CardPassThroughOptions } from '../card';
+import { ChartPassThroughOptions } from '../chart';
+import { ChipPassThroughOptions } from '../chip';
+import { ConfirmDialogPassThroughOptions } from '../confirmdialog';
+import { ConfirmPopupPassThroughOptions } from '../confirmpopup';
+import { ContextMenuPassThroughOptions } from '../contextmenu';
+import { DeferredContentPassThroughOptions } from '../deferredcontent';
+import { DialogPassThroughOptions } from '../dialog';
+import { DividerPassThroughOptions } from '../divider';
+import { DockPassThroughOptions } from '../dock';
+import { FieldsetPassThroughOptions } from '../fieldset';
+import { FileUploadPassThroughOptions } from '../fileupload';
+import { InlineMessagePassThroughOptions } from '../inlinemessage';
+import { InplacePassThroughOptions } from '../inplace';
+import { MegaMenuPassThroughOptions } from '../megamenu';
+import { MenuPassThroughOptions } from '../menu';
+import { MenubarPassThroughOptions } from '../menubar';
+import { MessagePassThroughOptions } from '../message';
+import { OverlayPanelPassThroughOptions } from '../overlaypanel';
 import { PanelPassThroughOptions } from '../panel';
+import { PanelMenuPassThroughOptions } from '../panelmenu';
+import { ProgressBarPassThroughOptions } from '../progressbar';
+import { ProgressSpinnerPassThroughOptions } from '../progressspinner';
+import { ScrollPanelPassThroughOptions } from '../scrollpanel';
+import { ScrollTopPassThroughOptions } from '../scrolltop';
+import { SidebarPassThroughOptions } from '../sidebar';
+import { SkeletonPassThroughOptions } from '../skeleton';
+import { SpeedDialPassThroughOptions } from '../speeddial';
+import { SplitButtonPassThroughOptions } from '../splitbutton';
+import { SplitterPassThroughOptions } from '../splitter';
+import { TabMenuPassThroughOptions } from '../tabmenu';
+import { TabPanelPassThroughOptions } from '../tabpanel';
+import { TabViewPassThroughOptions } from '../tabview';
+import { TagPassThroughOptions } from '../tag';
+import { TerminalPassThroughOptions } from '../terminal';
+import { TieredMenuPassThroughOptions } from '../tieredmenu';
+import { ToastPassThroughOptions } from '../toast';
+import { ToolbarPassThroughOptions } from '../toolbar';
 
 interface PrimeVueConfiguration {
     ripple?: boolean;
@@ -19,8 +62,53 @@ interface PrimeVueZIndexOptions {
 }
 
 interface PrimeVuePTOptions {
-    panel?: PanelPassThroughOptions;
+    accordion?: AccordionPassThroughOptions;
+    accordiontab?: AccordionTabPassThroughOptions;
+    avatar?: AvatarPassThroughOptions;
+    badge?: BadgePassThroughOptions;
+    blockui?: BlockUIPassThroughOptions;
+    breadcrumb?: BreadcrumbPassThroughOptions;
+    button?: ButtonPassThroughOptions;
     card?: CardPassThroughOptions;
+    chart?: ChartPassThroughOptions;
+    chip?: ChipPassThroughOptions;
+    confirmdialog?: ConfirmDialogPassThroughOptions;
+    confirmpopup?: ConfirmPopupPassThroughOptions;
+    contextmenu?: ContextMenuPassThroughOptions;
+    deferredcontent?: DeferredContentPassThroughOptions;
+    divider?: DividerPassThroughOptions;
+    dialog?: DialogPassThroughOptions;
+    dock?: DockPassThroughOptions;
+    dynamicdialog?: DialogPassThroughOptions;
+    fieldset?: FieldsetPassThroughOptions;
+    fileupload?: FileUploadPassThroughOptions;
+    inlinemessage?: InlineMessagePassThroughOptions;
+    inplace?: InplacePassThroughOptions;
+    megamenu?: MegaMenuPassThroughOptions;
+    menu?: MenuPassThroughOptions;
+    menubar?: MenubarPassThroughOptions;
+    message?: MessagePassThroughOptions;
+    overlaypanel?: OverlayPanelPassThroughOptions;
+    panel?: PanelPassThroughOptions;
+    panelmenu?: PanelMenuPassThroughOptions;
+    progressbar?: ProgressBarPassThroughOptions;
+    progressspinner?: ProgressSpinnerPassThroughOptions;
+    scrollpanel?: ScrollPanelPassThroughOptions;
+    scrolltop?: ScrollTopPassThroughOptions;
+    sidebar?: SidebarPassThroughOptions;
+    skeleton?: SkeletonPassThroughOptions;
+    speeddial?: SpeedDialPassThroughOptions;
+    splitbutton?: SplitButtonPassThroughOptions;
+    splitter?: SplitterPassThroughOptions;
+    steps?: PanelMenuPassThroughOptions;
+    tabmenu?: TabMenuPassThroughOptions;
+    tabpanel?: TabPanelPassThroughOptions;
+    tabview?: TabViewPassThroughOptions;
+    tag?: TagPassThroughOptions;
+    terminal?: TerminalPassThroughOptions;
+    tieredmenu?: TieredMenuPassThroughOptions;
+    toast?: ToastPassThroughOptions;
+    toolbar?: ToolbarPassThroughOptions;
 }
 
 interface PrimeVueLocaleAriaOptions {
