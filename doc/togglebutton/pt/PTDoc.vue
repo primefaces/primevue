bf9d7c0380fee5bebe6e0716dd2b44687c434b3b
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-05-08 16:01:44 +0300

    ToggleButton pt demo updated

--- doc/togglebutton/pt/PTDoc.vue
+++ doc/togglebutton/pt/PTDoc.vue
@@ -1,7 +1,15 @@
 <template>
     <DocSectionText v-bind="$attrs"> </DocSectionText>
     <div class="card flex justify-content-center">
-        <ToggleButton v-model="checked" class="w-8rem" />
+        <ToggleButton
+            v-model="checked"
+            class="w-8rem"
+            :pt="{
+                root: () => ({
+                    class: checked ? 'bg-green-500 border-white' : undefined
+                })
+            }"
+        />
     </div>
     <DocSectionCode :code="code" />
 </template>
@@ -13,11 +21,27 @@ export default {
             checked: false,
             code: {
                 basic: `
-<ToggleButton v-model="checked" />`,
+<ToggleButton
+    v-model="checked"
+    class="w-8rem"
+    :pt="{
+        root: () => ({
+            class: checked ? 'bg-green-500 border-white' : undefined
+        })
+    }"
+/>`,
                 options: `
 <template>
     <div class="card flex justify-content-center">
-        <ToggleButton v-model="checked" class="w-8rem" />
+        <ToggleButton
+            v-model="checked"
+            class="w-8rem"
+            :pt="{
+                root: () => ({
+                    class: checked ? 'bg-green-500 border-white' : undefined
+                })
+            }"
+        />
     </div>
 </template>
 
@@ -33,7 +57,15 @@ export default {
                 composition: `
 <template>
     <div class="card flex justify-content-center">
-        <ToggleButton v-model="checked" class="w-8rem" />
+        <ToggleButton
+            v-model="checked"
+            class="w-8rem"
+            :pt="{
+                root: () => ({
+                    class: checked ? 'bg-green-500 border-white' : undefined
+                })
+            }"
+        />
     </div>
 </template>
 
