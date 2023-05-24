fee8498a7edf9b74f173cf9f784e78bff1baf7fd
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-05-24 17:56:02 +0300

    Refactor #3965 - For BlockUI

---
+++ components/lib/blockui/BaseBlockUI.vue
@@ -0,0 +1,66 @@
+<script>
+import BaseComponent from 'primevue/basecomponent';
+import { useStyle } from 'primevue/usestyle';
+
+const styles = `
+.p-blockui-container {
+    position: relative;
+}
+
+.p-blockui {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.p-blockui.p-component-overlay {
+    position: absolute;
+}
+
+.p-blockui-document.p-component-overlay {
+    position: fixed;
+}
+`;
+
+const classes = {
+    root: 'p-blockui-container'
+};
+
+const { load: loadStyle } = useStyle(styles, { id: 'primevue_divider_style', manual: true });
+
+export default {
+    name: 'BaseAvatar',
+    extends: BaseComponent,
+    props: {
+        blocked: {
+            type: Boolean,
+            default: false
+        },
+        fullScreen: {
+            type: Boolean,
+            default: false
+        },
+        baseZIndex: {
+            type: Number,
+            default: 0
+        },
+        autoZIndex: {
+            type: Boolean,
+            default: true
+        }
+    },
+    css: {
+        classes
+    },
+    watch: {
+        isUnstyled: {
+            immediate: true,
+            handler(newValue) {
+                !newValue && loadStyle();
+            }
+        }
+    }
+};
+</script>
