1d615bb05d21a419aae10a97cce78cebfbd4d683
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-05-24 12:23:34 +0300

    Refactor #3965 - Update Card-divider data-pc-* structure

--- components/lib/card/Card.vue
+++ components/lib/card/Card.vue
@@ -1,19 +1,19 @@
 <template>
-    <div :class="cx('root')" v-bind="ptm('root')" data-pc-name="card" data-pc-section="root">
-        <div v-if="$slots.header" :class="cx('header')" v-bind="ptm('header')" data-pc-section="header">
+    <div :class="cx('root')" v-bind="ptm('root')">
+        <div v-if="$slots.header" :class="cx('header')">
             <slot name="header"></slot>
         </div>
         <div :class="cx('body')" v-bind="ptm('body')" data-pc-section="body">
-            <div v-if="$slots.title" :class="cx('title')" v-bind="ptm('title')" data-pc-section="title">
+            <div v-if="$slots.title" :class="cx('title')">
                 <slot name="title"></slot>
             </div>
-            <div v-if="$slots.subtitle" :class="cx('subtitle')" v-bind="ptm('subtitle')" data-pc-section="subtitle">
+            <div v-if="$slots.subtitle" :class="cx('subtitle')" v-bind="ptm('subtitle')">
                 <slot name="subtitle"></slot>
             </div>
-            <div :class="cx('content')" v-bind="ptm('content')" data-pc-section="content">
+            <div :class="cx('content')" v-bind="ptm('content')">
                 <slot name="content"></slot>
             </div>
-            <div v-if="$slots.footer" :class="cx('footer')" v-bind="ptm('footer')" data-pc-section="footer">
+            <div v-if="$slots.footer" :class="cx('footer')">
                 <slot name="footer"></slot>
             </div>
         </div>
