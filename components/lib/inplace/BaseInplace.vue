033c7304f37f16f1f130dd91ec9d8c116a1cf557
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-05-24 12:52:20 +0300

    Refactor #3965 - For Inplace

---
+++ components/lib/inplace/BaseInplace.vue
@@ -0,0 +1,75 @@
+<script>
+import BaseComponent from 'primevue/basecomponent';
+import { useStyle } from 'primevue/usestyle';
+
+const styles = `
+.p-inplace .p-inplace-display {
+    display: inline;
+    cursor: pointer;
+}
+
+.p-inplace .p-inplace-content {
+    display: inline;
+}
+
+.p-fluid .p-inplace.p-inplace-closable .p-inplace-content {
+    display: flex;
+}
+
+.p-fluid .p-inplace.p-inplace-closable .p-inplace-content > .p-inputtext {
+    flex: 1 1 auto;
+    width: 1%;
+}
+`;
+
+const classes = {
+    root: ({ props }) => ['p-inplace p-component', { 'p-inplace-closable': props.closable }],
+    display: ({ props }) => ['p-inplace-display', { 'p-disabled': props.disabled }],
+    content: 'p-inplace-content',
+    closeButton: ({ props }) => props.closeIcon
+};
+
+const { load: loadStyle } = useStyle(styles, { id: 'primevue_divider_style', manual: true });
+
+export default {
+    name: 'BaseAvatar',
+    extends: BaseComponent,
+    props: {
+        closable: {
+            type: Boolean,
+            default: false
+        },
+        active: {
+            type: Boolean,
+            default: false
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        closeIcon: {
+            type: String,
+            default: undefined
+        },
+        displayProps: {
+            type: null,
+            default: null
+        },
+        closeButtonProps: {
+            type: null,
+            default: null
+        }
+    },
+    css: {
+        classes
+    },
+    watch: {
+        isUnstyled: {
+            immediate: true,
+            handler(newValue) {
+                !newValue && loadStyle();
+            }
+        }
+    }
+};
+</script>
