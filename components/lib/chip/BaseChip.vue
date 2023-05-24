ae43587f19fae4d80b442c0d79ca51dd8a8ce7cc
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-05-24 10:39:02 +0300

    Refactor #3965 - For Chip

---
+++ components/lib/chip/BaseChip.vue
@@ -0,0 +1,80 @@
+<script>
+import BaseComponent from 'primevue/basecomponent';
+import { useStyle } from 'primevue/usestyle';
+
+const styles = `
+.p-chip {
+    display: inline-flex;
+    align-items: center;
+}
+
+.p-chip-text {
+    line-height: 1.5;
+}
+
+.p-chip-icon.pi {
+    line-height: 1.5;
+}
+
+.p-chip-remove-icon {
+    line-height: 1.5;
+    cursor: pointer;
+}
+
+.p-chip img {
+    border-radius: 50%;
+}
+`;
+
+const classes = {
+    root: ({ props }) => [
+        'p-chip p-component',
+        {
+            'p-chip-image': props.image != null
+        }
+    ],
+    icon: ({ props }) => ['p-chip-icon', props.icon],
+    label: 'p-chip-text',
+    removeIcon: ({ props }) => ['p-chip-remove-icon', props.removeIcon]
+};
+
+const { load: loadStyle } = useStyle(styles, { id: 'primevue_divider_style', manual: true });
+
+export default {
+    name: 'BaseAvatar',
+    extends: BaseComponent,
+    props: {
+        label: {
+            type: String,
+            default: null
+        },
+        icon: {
+            type: String,
+            default: null
+        },
+        image: {
+            type: String,
+            default: null
+        },
+        removable: {
+            type: Boolean,
+            default: false
+        },
+        removeIcon: {
+            type: String,
+            default: undefined
+        }
+    },
+    css: {
+        classes
+    },
+    watch: {
+        isUnstyled: {
+            immediate: true,
+            handler(newValue) {
+                !newValue && loadStyle();
+            }
+        }
+    }
+};
+</script>
