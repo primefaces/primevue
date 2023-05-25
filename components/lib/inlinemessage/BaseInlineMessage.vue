0d3816522a08bc79354ec0451fdecbfe1e6616f5
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-05-25 17:15:19 +0300

    Refactor #3965 - For InlineMessage

---
+++ components/lib/inlinemessage/BaseInlineMessage.vue
@@ -0,0 +1,56 @@
+<script>
+import BaseComponent from 'primevue/basecomponent';
+import { useStyle } from 'primevue/usestyle';
+
+const styles = `
+.p-inline-message {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    vertical-align: top;
+}
+
+.p-inline-message-icon-only .p-inline-message-text {
+    visibility: hidden;
+    width: 0;
+}
+
+.p-fluid .p-inline-message {
+    display: flex;
+}
+`;
+
+const classes = {
+    root: ({ props, instance }) => ['p-inline-message p-component p-inline-message-' + props.severity, { 'p-inline-message-icon-only': !instance.$slots.default }],
+    icon: ({ props }) => ['p-inline-message-icon', props.icon],
+    text: 'p-inline-message-text'
+};
+
+const { load: loadStyle } = useStyle(styles, { id: 'primevue_inlinemessage_style', manual: true });
+
+export default {
+    name: 'BaseInlineMessage',
+    extends: BaseComponent,
+    props: {
+        severity: {
+            type: String,
+            default: 'error'
+        },
+        icon: {
+            type: String,
+            default: undefined
+        }
+    },
+    css: {
+        classes
+    },
+    watch: {
+        isUnstyled: {
+            immediate: true,
+            handler(newValue) {
+                !newValue && loadStyle();
+            }
+        }
+    }
+};
+</script>
