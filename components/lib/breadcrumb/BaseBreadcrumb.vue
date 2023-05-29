b1d229f2c841c0de03532b3bdb5b4e20394b8e83
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-05-29 10:05:29 +0300

    Refactor #3965 - For Breadcrumb

---
+++ components/lib/breadcrumb/BaseBreadcrumb.vue
@@ -0,0 +1,87 @@
+<script>
+import BaseComponent from 'primevue/basecomponent';
+import { useStyle } from 'primevue/usestyle';
+
+const styles = `
+.p-breadcrumb {
+    overflow-x: auto;
+}
+
+.p-breadcrumb .p-breadcrumb-list {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+}
+
+.p-breadcrumb .p-menuitem-text {
+    line-height: 1;
+}
+
+.p-breadcrumb .p-menuitem-link {
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+}
+
+.p-breadcrumb .p-menuitem-separator {
+    display: flex;
+    align-items: center;
+}
+
+.p-breadcrumb::-webkit-scrollbar {
+    display: none;
+}
+`;
+
+const classes = {
+    root: 'p-breadcrumb p-component',
+    menu: 'p-breadcrumb-list',
+    home: 'p-breadcrumb-home',
+    separator: 'p-menuitem-separator',
+    menuitem: ({ context }) => ['p-menuitem', { 'p-disabled': context.disabled() }],
+    action: ({ context, routerProps }) => [
+        'p-menuitem-link',
+        {
+            'router-link-active': routerProps && routerProps.isActive,
+            'router-link-active-exact': context.exact && routerProps && routerProps.isExactActive
+        }
+    ],
+    icon: 'p-menuitem-icon',
+    label: 'p-menuitem-text'
+};
+
+const { load: loadStyle, unload: unloadStyle } = useStyle(styles, { id: 'primevue_breadcrumb_style', manual: true });
+
+export default {
+    name: 'BaseBreadcrumb',
+    extends: BaseComponent,
+    props: {
+        model: {
+            type: Array,
+            default: null
+        },
+        home: {
+            type: null,
+            default: null
+        },
+        exact: {
+            type: Boolean,
+            default: true
+        }
+    },
+    css: {
+        classes
+    },
+    watch: {
+        isUnstyled: {
+            immediate: true,
+            handler(newValue) {
+                !newValue && loadStyle();
+            }
+        }
+    }
+};
+</script>
