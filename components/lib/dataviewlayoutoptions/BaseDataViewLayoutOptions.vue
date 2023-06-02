80478bbfe19ae4a4a5cf185403ee9a588aad5053
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-06-02 19:04:55 +0300

    Refactor #3965 - For DataViewLayoutOptions

---
+++ components/lib/dataviewlayoutoptions/BaseDataViewLayoutOptions.vue
@@ -0,0 +1,35 @@
+<script>
+import BaseComponent from 'primevue/basecomponent';
+
+const classes = {
+    root: 'p-dataview-layout-options p-selectbutton p-buttonset',
+    listButton: ({ props }) => [
+        'p-button p-button-icon-only',
+        {
+            'p-highlight': props.modelValue === 'list'
+        }
+    ],
+    gridButton: ({ props }) => [
+        'p-button p-button-icon-only',
+        {
+            'p-highlight': props.modelValue === 'grid'
+        }
+    ]
+};
+
+export default {
+    name: 'BaseDataViewLayoutOptions',
+    extends: BaseComponent,
+    props: {
+        modelValue: String
+    },
+    css: {
+        classes
+    },
+    provide() {
+        return {
+            $parentInstance: this
+        };
+    }
+};
+</script>
