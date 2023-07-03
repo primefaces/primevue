60d0049eb16cb4cd413ccf27670e6227c3781fdf
mertsincan <sincan.mert@gmail.com>
2023-07-03 23:32:41 +0100

    Create UseStyle.d.ts

---
+++ components/lib/usestyle/UseStyle.d.ts
@@ -0,0 +1,19 @@
+export interface StyleOptions {
+    document?: HTMLElement;
+    immediate?: boolean;
+    manual?: boolean;
+    name?: string;
+    id?: string;
+    media?: string;
+}
+
+export declare function useStyle(
+    css: string,
+    options?: StyleOptions
+): {
+    id: string;
+    css: any;
+    unload: () => void;
+    load: () => void;
+    isLoaded: boolean;
+};
