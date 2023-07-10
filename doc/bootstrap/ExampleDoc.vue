a74912ad2863bec5d8f3ab69d46b44408c2f9b6a
mertsincan <sincan.mert@gmail.com>
2023-07-10 17:20:47 +0100

    Refactor

--- doc/bootstrap/ExampleDoc.vue
+++ doc/bootstrap/ExampleDoc.vue
@@ -3,7 +3,7 @@
         <p>As an example, <a href="https://getbootstrap.com/docs/4.0/components/modal/">Modal</a> and <a href="https://getbootstrap.com/docs/4.0/components/button/">Button</a> styles of Bootstrap are used to style PrimeVue Button and Dialog.</p>
     </DocSectionText>
     <div class="flex justify-content-center">
-        <iframe class="w-full h-full" style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius: 2px; min-height: 800px" src="https://codesandbox.io/p/sandbox/primevue-bootstrap-dialog-button-pt-example-4p3t7d" allowfullscreen></iframe>
+        <iframe class="w-full h-full" style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius: 2px; min-height: 800px" src="https://codesandbox.io/p/sandbox/unstyled-primevue-dialog-with-bootstrap-t7y52t?embed=1" allowfullscreen></iframe>
     </div>
 </template>
 
--- doc/tailwind/ExampleDoc.vue
+++ doc/tailwind/ExampleDoc.vue
@@ -3,7 +3,7 @@
         <p>As an example, Tailwind is used to style the <i>Button</i> and <i>Dialog</i> components of PrimeVue.</p>
     </DocSectionText>
     <div class="flex justify-content-center">
-        <iframe class="w-full h-full" style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius: 2px; min-height: 800px" src="https://codesandbox.io/p/sandbox/primevue-tailwind-dialog-button-pt-example-jl2xzx?embed=1" allowfullscreen></iframe>
+        <iframe class="w-full h-full" style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius: 2px; min-height: 800px" src="https://codesandbox.io/p/sandbox/unstyled-primevue-dialog-with-tailwind-css-2jp96n?embed=1" allowfullscreen></iframe>
     </div>
 </template>
 
