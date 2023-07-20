932e332a0bd985932104d3f5cdae9e2a3bd228f3
Franz Amersian <114055827+versacodes@users.noreply.github.com>
2023-07-20 17:23:42 +0800

    Added css 'import' line in icons doc's import section

--- doc/icons/ImportDoc.vue
+++ doc/icons/ImportDoc.vue
@@ -1,17 +1,21 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p>CSS file of the icon library needs to be imported in <i>styles.scss</i> of your application.</p>
+        <p>CSS file of the icon library needs to be imported in <i>styles.css</i> or <i>styles.scss</i> of your application.</p>
     </DocSectionText>
-    <DocSectionCode :code="code" hideToggleCode importCode hideCodeSandbox hideStackBlitz />
+    <DocSectionCode :code="code_css" hideToggleCode importCode hideCodeSandbox hideStackBlitz />
+    <DocSectionCode :code="code_scss" hideToggleCode importCode hideCodeSandbox hideStackBlitz />
 </template>
 
 <script>
 export default {
     data() {
         return {
-            code: {
-                basic: `import 'primeicons/primeicons.css';`
-            }
+            code_css: {
+                basic: `// For .scss files\nimport 'primeicons/primeicons.css';`
+            },
+            code_scss: {
+                basic: `// For .css files\n@import 'primeicons/primeicons.css';`
+                        }
         };
     }
 };
