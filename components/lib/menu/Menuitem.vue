fd326c4d8d019cd1638544bde8d20ffd1c3a7230
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-07-25 12:11:23 +0300

    Refactor #4190 - For Menu

--- components/lib/menu/Menuitem.vue
+++ components/lib/menu/Menuitem.vue
@@ -47,7 +47,8 @@ export default {
         templates: null,
         exact: null,
         id: null,
-        focusedOptionId: null
+        focusedOptionId: null,
+        index: null
     },
     methods: {
         getItemProp(processedItem, name) {
@@ -56,6 +57,8 @@ export default {
         getPTOptions(key) {
             return this.ptm(key, {
                 context: {
+                    item: this.item,
+                    index: this.index,
                     focused: this.isItemFocused()
                 }
             });
