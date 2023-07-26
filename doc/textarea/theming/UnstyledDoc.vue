54252e463ffc8cc140b6915f28bb650d88da88ca
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-07-26 20:30:11 +0300

    Textarea, ToggleButton, TreeSelect, TriStateCheckbox unstyled demo updates

---
+++ doc/textarea/theming/UnstyledDoc.vue
@@ -0,0 +1,30 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Theming is implemented with the pass through properties in unstyled mode. Example below demonstrates the built-in Tailwind theme.</p>
+    </DocSectionText>
+    <DocSectionCode :code="code" embedded />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: '',
+            code: {
+                composition: `
+<template>
+    <div class="card flex justify-center">
+        <Textarea v-model="value" rows="5" cols="30" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const value = ref('');
+<\/script>`
+            }
+        };
+    }
+};
+</script>
