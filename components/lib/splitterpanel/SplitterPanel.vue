38bd2712e9cefaf9a3fdb8fe9c0b126d5156e1bc
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-07-26 10:52:37 +0300

    Add pt options to SplitterPanel

--- components/lib/splitterpanel/SplitterPanel.vue
+++ components/lib/splitterpanel/SplitterPanel.vue
@@ -1,5 +1,5 @@
 <template>
-    <div ref="container" :class="cx('root')" v-bind="ptm('root')" data-pc-name="splitterpanel">
+    <div ref="container" :class="cx('root')" v-bind="ptm('root', getPTOptions)" data-pc-name="splitterpanel">
         <slot></slot>
     </div>
 </template>
