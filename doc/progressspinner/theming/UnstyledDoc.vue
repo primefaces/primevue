af6ad719a4d2734db126825323fbed625fe3014b
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-08-03 17:45:24 +0300

    add progresspinner demo

---
+++ doc/progressspinner/theming/UnstyledDoc.vue
@@ -0,0 +1,26 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Theming is implemented with the pass through properties in unstyled mode. Example below demonstrates the built-in Tailwind theme.</p>
+    </DocSectionText>
+    <DocSectionCode :code="code" embedded />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                composition: `
+<template>
+    <div class="card flex justify-content-center">
+        <ProgressSpinner />
+    </div>
+</template>
+
+<script setup>
+<\/script>`
+            }
+        };
+    }
+};
+</script>
