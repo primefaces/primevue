2e9e12ee6e7754ed60966d43ddc5f7d4635570af
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-08-09 16:21:03 +0300

    Fixed #4216 - Dropdown: clearicon slot prop onClick has invalid type

--- components/lib/dropdown/Dropdown.d.ts
+++ components/lib/dropdown/Dropdown.d.ts
@@ -566,7 +566,7 @@ export interface DropdownSlots {
         /**
          * Clear icon click function.
          */
-        onClick: void;
+        onClick: (event: MouseEvent) => void;
     }): VNode[];
     /**
      * Custom dropdown icon template.
