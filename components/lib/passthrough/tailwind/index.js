b5c5f9fa1c86b135a34d08b59e27ba1097d9b390
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-08-22 17:11:31 +0300

    Tailwind Tag severity fixes

--- components/lib/passthrough/tailwind/index.js
+++ components/lib/passthrough/tailwind/index.js
@@ -1954,11 +1954,9 @@ export default {
                 'inline-flex items-center justify-center',
                 'bg-blue-500 text-white text-xs font-semibold px-2 py-1 ',
                 {
-                    'bg-gray-500 ': props.severity == 'secondary',
                     'bg-green-500 ': props.severity == 'success',
                     'bg-blue-500 ': props.severity == 'info',
                     'bg-orange-500 ': props.severity == 'warning',
-                    'bg-purple-500 ': props.severity == 'help',
                     'bg-red-500 ': props.severity == 'danger'
                 },
                 {
