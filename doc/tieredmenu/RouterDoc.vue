81b2102c5657becd4bef0eb5a7f1b6eb5c0452ea
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-08-30 17:38:19 +0300

    Steps docs for router

--- doc/tieredmenu/RouterDoc.vue
+++ doc/tieredmenu/RouterDoc.vue
@@ -2,7 +2,7 @@
     <DocSectionText v-bind="$attrs">
         <p>
             Since v3.33.0 the vue-router dependency of menu components is deprecated and templating should be used to define router links instead. This approach provides flexibility to be able to use any kind of router link component such as
-            <i>NuxtLink</i> or <i>router-link</i>.
+            <i>NuxtLink</i> or <i>router-link</i>. Here is an example with vue-router.
         </p>
     </DocSectionText>
     <div class="card flex justify-content-center">
