82442f8d13b066d01c9ff2b83be70e1519e87d18
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-09-06 14:50:21 +0300

    Fixed #4399 - Breadcrumb: duplicate key warning

--- components/lib/breadcrumb/Breadcrumb.vue
+++ components/lib/breadcrumb/Breadcrumb.vue
@@ -2,7 +2,7 @@
     <nav :class="cx('root')" v-bind="ptm('root')" data-pc-name="breadcrumb">
         <ol :class="cx('menu')" v-bind="ptm('menu')">
             <BreadcrumbItem v-if="home" :item="home" :class="cx('home')" :templates="$slots" :exact="exact" :pt="pt" :unstyled="unstyled" v-bind="ptm('home')" />
-            <template v-for="(item, i) of model" :key="item.label">
+            <template v-for="(item, i) of model" :key="item.label + '_' + i">
                 <li v-if="home || i !== 0" :class="cx('separator')" v-bind="ptm('separator')">
                     <slot name="separator">
                         <ChevronRightIcon aria-hidden="true" v-bind="ptm('separatorIcon')" />
