adede19fea46d666b481cdcec5c6964a2a75ba9c
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-09-21 10:23:01 +0300

    Remove Vue-CLI example from doc

--- doc/installation/ExamplesDoc.vue
+++ doc/installation/ExamplesDoc.vue
@@ -11,10 +11,6 @@
             <img src="https://primefaces.org/cdn/primevue/images/logos/nuxt.svg" alt="Nuxt.JS" class="w-8rem h-8rem mb-3" />
             <span class="font-bold text-xl text-900">Nuxt</span>
         </a>
-        <a href="https://github.com/primefaces/primevue-quickstart" class="flex flex-column align-items-center">
-            <img src="https://primefaces.org/cdn/primevue/images/logos/vue.svg" alt="Vue CLI" class="w-8rem h-8rem mb-3" />
-            <span class="font-bold text-xl text-900">Vue-CLI</span>
-        </a>
         <a href="https://github.com/primefaces/primevue-examples/tree/main/astro-quickstart" class="text-800 flex flex-column align-items-center">
             <img src="https://primefaces.org/cdn/primevue/images/logos/astro.svg" alt="Astro" class="w-8rem h-8rem mb-3" />
             <span class="font-bold text-xl text-900">Astro</span>
