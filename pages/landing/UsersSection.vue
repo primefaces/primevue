be61b277424378edf7d70419a2c39e6124e17ed0
Cagatay Civici <cagatay.civici@gmail.com>
2023-10-10 12:41:56 +0300

    Update downloads

--- pages/landing/UsersSection.vue
+++ pages/landing/UsersSection.vue
@@ -2,7 +2,7 @@
     <section class="landing-users py-8 px-3 lg:px-8">
         <div class="section-header">Who Uses</div>
         <p class="section-detail">
-            PrimeTek libraries have reached over <span class="font-semibold animated-text relative white-space-nowrap"> <span>110 Million Downloads </span> </span> on npm! Join the PrimeLand community and experience the difference yourself.
+            PrimeTek libraries have reached over <span class="font-semibold animated-text relative white-space-nowrap"> <span>140 Million Downloads </span> </span> on npm! Join the PrimeLand community and experience the difference yourself.
         </p>
         <div class="flex justify-content-center align-items-center mt-4">
             <span class="ml-2"> </span>
