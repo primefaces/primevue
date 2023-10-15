5bed4f752befc94a9290b86c16e0734b2eaa57bd
Cagatay Civici <cagatay.civici@gmail.com>
2023-10-15 17:01:21 +0300

    Cleanup plugin

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -1,14 +1,7 @@
 import PrimeVue from '@/components/lib/config/PrimeVue';
 import ToastService from '@/components/lib/toastservice/ToastService';
 
-import DocApiSection from '@/components/doc/DocApiSection';
-import DocComponent from '@/components/doc/DocComponent';
-import DocSectionCode from '@/components/doc/DocSectionCode';
-import DocSectionNav from '@/components/doc/DocSectionNav';
-import DocSections from '@/components/doc/DocSections';
-import DocSectionText from '@/components/doc/DocSectionText';
 import CodeHighlight from '@/components/layout/CodeHighlight';
-import DevelopmentSection from '@/components/layout/DevelopmentSection';
 import Animate from '@/components/lib/animate/Animate';
 
 export default defineNuxtPlugin((nuxtApp) => {
@@ -17,13 +10,4 @@ export default defineNuxtPlugin((nuxtApp) => {
 
     nuxtApp.vueApp.directive('animate', Animate);
     nuxtApp.vueApp.directive('code', CodeHighlight);
-
-    nuxtApp.vueApp.component('DocSections', DocSections);
-    nuxtApp.vueApp.component('DocSectionText', DocSectionText);
-    nuxtApp.vueApp.component('DocSectionCode', DocSectionCode);
-    nuxtApp.vueApp.component('DocSectionNav', DocSectionNav);
-    nuxtApp.vueApp.component('DocApiSection', DocApiSection);
-    nuxtApp.vueApp.component('DocComponent', DocComponent);
-
-    nuxtApp.vueApp.component('DevelopmentSection', DevelopmentSection);
 });
