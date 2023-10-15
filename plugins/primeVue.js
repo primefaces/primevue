6798b17cf48bab1703733afca17f8e0a0cd0dfcc
mertsincan <sincan.mert@gmail.com>
2023-10-15 20:03:12 +0100

    Update nuxt-primevue version

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -1,12 +1,10 @@
 import PrimeVue from '@/components/lib/config/PrimeVue';
-import ToastService from '@/components/lib/toastservice/ToastService';
 
-import CodeHighlight from '@/directives/CodeHighlight';
 import Animate from '@/components/lib/animate/Animate';
+import CodeHighlight from '@/directives/CodeHighlight';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: true });
-    nuxtApp.vueApp.use(ToastService);
 
     nuxtApp.vueApp.directive('animate', Animate);
     nuxtApp.vueApp.directive('code', CodeHighlight);
