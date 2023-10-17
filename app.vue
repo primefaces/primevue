032a6a8355906000f918c9a88b34a2230d6c741d
Cagatay Civici <cagatay.civici@gmail.com>
2023-10-18 00:59:30 +0300

    Prevent unnecessary switch

--- app.vue
+++ app.vue
@@ -22,11 +22,13 @@ export default {
     },
     mounted() {
         this.themeChangeListener = (event) => {
-            this.$primevue.changeTheme(this.$appState.theme, event.theme, 'theme-link', () => {
-                this.$appState.theme = event.theme;
-                this.$appState.darkTheme = event.dark;
-                EventBus.emit('theme-change-complete', { theme: event.theme, dark: event.dark });
-            });
+            if (event.theme !== this.$appState.theme) {
+                this.$primevue.changeTheme(this.$appState.theme, event.theme, 'theme-link', () => {
+                    this.$appState.theme = event.theme;
+                    this.$appState.darkTheme = event.dark;
+                    EventBus.emit('theme-change-complete', { theme: event.theme, dark: event.dark });
+                });
+            }
         };
 
         EventBus.on('theme-change', this.themeChangeListener);
