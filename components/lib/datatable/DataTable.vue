4934e0e1468f4b3e6698ea8fa537c820b5f1d0b7
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2023-10-27 17:52:34 +0300

    Fixed #4498 - Datatable: Cannot set properties of null (setting 'tabIndex')

--- components/lib/datatable/DataTable.vue
+++ components/lib/datatable/DataTable.vue
@@ -787,6 +787,8 @@ export default {
             this.rowTouched = false;
 
             if (focusedItem) {
+                if (e.originalEvent.target.getAttribute('data-pc-section') === 'rowtogglericon' || e.originalEvent.target.parentElement.getAttribute('data-pc-section') === 'rowtogglericon') return;
+
                 const targetRow = e.originalEvent.target.closest('tr[data-p-selectable-row="true"]');
 
                 focusedItem.tabIndex = '-1';
