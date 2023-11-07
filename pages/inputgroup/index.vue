5192b34ebc3d71a0e23ea01f0ab13d77195d54c9
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2023-11-07 09:16:02 +0300

    InputGroup passthrough section added

--- pages/inputgroup/index.vue
+++ pages/inputgroup/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <DocComponent title="Vue InputGroup Component" header="InputGroup" description="Text, icon, buttons and other content can be grouped next to an input." :componentDocs="docs" />
+    <DocComponent
+        title="Vue InputGroup Component"
+        header="InputGroup"
+        description="Text, icon, buttons and other content can be grouped next to an input."
+        :componentDocs="docs"
+        :apiDocs="['InputGroup', 'InputGroupAddon']"
+        :ptTabComponent="ptComponent"
+    />
 </template>
 
 <script>
@@ -7,6 +14,8 @@ import BasicDoc from '@/doc/inputgroup/BasicDoc.vue';
 import ButtonDoc from '@/doc/inputgroup/ButtonDoc.vue';
 import CheckboxDoc from '@/doc/inputgroup/CheckboxDoc.vue';
 import MultipleDoc from '@/doc/inputgroup/MultipleDoc.vue';
+import PTComponent from '@/doc/inputgroup/pt/index.vue';
+import AccessibilityDoc from '@/doc/inputgroup/AccessibilityDoc.vue';
 
 export default {
     data() {
@@ -31,8 +40,14 @@ export default {
                     id: 'checkbox',
                     label: 'Check & Radio',
                     component: CheckboxDoc
+                },
+                {
+                    id: 'accessibility',
+                    label: 'Accessibility',
+                    component: AccessibilityDoc
                 }
-            ]
+            ],
+            ptComponent: PTComponent
         };
     }
 };
