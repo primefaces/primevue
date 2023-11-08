ad9b437003e0d3ae26047d2cfd1a6a6ecc9037d7
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2023-11-08 15:47:48 +0300

    Refactor #4739 - For Breadcrumb

--- components/lib/breadcrumb/Breadcrumb.d.ts
+++ components/lib/breadcrumb/Breadcrumb.d.ts
@@ -134,6 +134,7 @@ export interface BreadcrumbProps {
     home?: MenuItem | undefined;
     /**
      * Whether to apply 'router-link-active-exact' class if route exactly matches the item path.
+     * @deprecated since v3.40.0.
      * @defaultValue true
      */
     exact?: boolean | undefined;
