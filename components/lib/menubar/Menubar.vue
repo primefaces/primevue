75d6867507920e9c8ce89b4503dbdff1bdb236a5
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2023-11-20 11:00:34 +0300

    Refactor #4820

--- components/lib/menubar/Menubar.vue
+++ components/lib/menubar/Menubar.vue
@@ -97,6 +97,7 @@ export default {
 
         query.addEventListener('change', () => {
             this.queryMatches = query.matches;
+            this.mobileActive = false;
         });
     },
     beforeUnmount() {
