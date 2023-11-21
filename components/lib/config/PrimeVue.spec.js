4092e434650285c42bed1ea3be51b4be953b49ae
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2023-11-21 17:34:41 +0300

    Fixed matchMedia tests

--- components/lib/config/PrimeVue.spec.js
+++ components/lib/config/PrimeVue.spec.js
@@ -5,6 +5,17 @@ config.global.mocks['$primevue'] = {
     config: defaultOptions
 };
 
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn()
+    }))
+});
+
 describe('PrimeVue.vue', () => {
     it('should exist', async () => {});
 });
