46c5857512787f1f8576cbca34ad754911e4848f
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2023-11-27 14:06:02 +0300

    Update AccessibilityDoc.vue

--- doc/inlinemessage/AccessibilityDoc.vue
+++ doc/inlinemessage/AccessibilityDoc.vue
@@ -5,5 +5,7 @@
             InlineMessage component uses <i>alert</i> role that implicitly defines <i>aria-live</i> as "assertive" and <i>aria-atomic</i> as "true". Since any attribute is passed to the root element, attributes like <i>aria-labelledby</i> and
             <i>aria-label</i> can optionally be used as well.
         </p>
+        <h3>Keyboard Support</h3>
+        <p>Component does not include any interactive elements.</p>
     </DocSectionText>
 </template>
