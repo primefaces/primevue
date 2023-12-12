ae3edacda57ee74927c9a35393552f553c282f0f
Onur Usluca <51825926+onurusluca@users.noreply.github.com>
2023-12-12 16:57:24 +0900

    Update PluginDoc.vue - fix Nuxt link

--- doc/installation/PluginDoc.vue
+++ doc/installation/PluginDoc.vue
@@ -1,7 +1,7 @@
 <template>
     <DocSectionText v-bind="$attrs">
         <p>
-            PrimeVue plugin is required to be installed with the <i>use</i> function to set up the default <NuxtLink to="/theming">configuration</NuxtLink>. If you are using <NuxtLink to="/theming">Nuxt</NuxtLink>, this step is not required as our
+            PrimeVue plugin is required to be installed with the <i>use</i> function to set up the default <NuxtLink to="/theming">configuration</NuxtLink>. If you are using <NuxtLink to="https://nuxt.com/">Nuxt</NuxtLink>, this step is not required as our
             nuxt module handles it.
         </p>
     </DocSectionText>
