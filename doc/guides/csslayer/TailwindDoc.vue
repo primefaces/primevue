8a04072cbbfbd1db5c487aa582c8e2bff04a0b95
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2023-12-26 12:00:08 +0300

    Code section updates

--- doc/guides/csslayer/TailwindDoc.vue
+++ doc/guides/csslayer/TailwindDoc.vue
@@ -4,7 +4,7 @@
             Tailwind CSS includes a reset utility in base called <a href="https://tailwindcss.com/docs/preflight" target="_blank" rel="noopener noreferrer">preflight</a>. If you are using this feature, wrap the base and utilities in separate layers
             and make sure primevue layer comes after the base.
         </p>
-        <DocSectionCode :code="code" />
+        <DocSectionCode :code="code" hideToggleCode hideCodeSandbox hideStackBlitz />
     </DocSectionText>
 </template>
 
@@ -18,12 +18,12 @@ export default {
 @layer tailwind-base, primevue, tailwind-utilities;
 
 @layer tailwind-base {
-  @tailwind base;
+    @tailwind base;
 }
 
 @layer tailwind-utilities {
-  @tailwind components;
-  @tailwind utilities;
+    @tailwind components;
+    @tailwind utilities;
 }
 `
             }
