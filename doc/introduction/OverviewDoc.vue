b4f2c00dce18a60de868426f94f6c77883634e3b
Cagatay Civici <cagatay.civici@gmail.com>
2023-12-30 22:38:03 +0300

    Adjust spacing

--- doc/introduction/OverviewDoc.vue
+++ doc/introduction/OverviewDoc.vue
@@ -4,7 +4,7 @@
             PrimeVue is a complete UI suite for Vue.js consisting of a rich set of UI components, icons, blocks, and application templates. The project's primary goal is to boost developer productivity by offering reusable solutions that are easy to
             tune and customize as an in-house library.
         </p>
-        <p>
+        <p class="mb-0">
             The project has been created by <a href="https://www.primetek.com.tr" class="text-primary hover:underline font-medium">PrimeTek</a> a world-renowned vendor of popular UI Component suites, including
             <a href="https://primefaces.org" class="text-primary hover:underline font-medium">PrimeFaces</a>, <a href="https://primeng.org" class="text-primary hover:underline font-medium">PrimeNG</a>, and
             <a href="https://primereact.org" class="text-primary hover:underline font-medium">PrimeReact.</a> All the members in <NuxtLink to="/team">our team</NuxtLink> are full time employees of PrimeTek who share the same passion and vision for
