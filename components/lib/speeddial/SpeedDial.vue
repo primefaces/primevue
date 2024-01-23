6862264318dc3fcec7e7adeadc64c0aaa9ef97b3
mertsincan <sincan.mert@gmail.com>
2024-01-23 17:47:37 +0000

    Fixed #5135 - The hideIcon property is not working on SpeedDial

--- components/lib/speeddial/SpeedDial.vue
+++ components/lib/speeddial/SpeedDial.vue
@@ -17,7 +17,7 @@
             >
                 <template #icon>
                     <slot name="icon" :visible="d_visible">
-                        <component v-if="d_visible && !!hideIcon" :is="hideIcon ? 'span' : 'PlusIcon'" :class="cx('buttonIcon')" v-bind="ptm('button')['icon']" data-pc-section="icon" />
+                        <component v-if="d_visible && !!hideIcon" :is="hideIcon ? 'span' : 'PlusIcon'" :class="[hideIcon, cx('buttonIcon')]" v-bind="ptm('button')['icon']" data-pc-section="icon" />
                         <component v-else :is="showIcon ? 'span' : 'PlusIcon'" :class="d_visible && !!hideIcon ? hideIcon : showIcon" v-bind="ptm('button')['icon']" data-pc-section="icon" />
                     </slot>
                 </template>
