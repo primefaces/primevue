f8a31676189cd33b35f4f6278227ea192416f51e
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-01-30 11:03:01 +0300

    Fixed #5175 - InputIcon component added

---
+++ components/lib/inputicon/InputIcon.vue
@@ -0,0 +1,12 @@
+<template>
+    <slot />
+</template>
+
+<script>
+import BaseInputIcon from './BaseInputIcon.vue';
+
+export default {
+    name: 'InputIcon',
+    extends: BaseInputIcon
+};
+</script>
