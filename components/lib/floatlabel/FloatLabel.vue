355bbebe7e1cb8e1e42eaf76e06b10501d08f196
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-01-30 11:01:40 +0300

    Refactor #5175 - FloatLabel

---
+++ components/lib/floatlabel/FloatLabel.vue
@@ -0,0 +1,14 @@
+<template>
+    <span :class="cx('root')" v-bind="ptm('root')" data-pc-name="floatlabel">
+        <slot />
+    </span>
+</template>
+
+<script>
+import BaseFloatLabel from './BaseFloatLabel.vue';
+
+export default {
+    name: 'FloatLabel',
+    extends: BaseFloatLabel
+};
+</script>
