9c162ae9e67d2f7df825613db58773b49b1100f7
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-02-01 10:44:05 +0300

    Create BaseFloatLabel.vue

---
+++ components/lib/floatlabel/BaseFloatLabel.vue
@@ -0,0 +1,16 @@
+<script>
+import BaseComponent from 'primevue/basecomponent';
+import FloatLabelStyle from 'primevue/floatlabel/style';
+
+export default {
+    name: 'BaseFloatLabel',
+    extends: BaseComponent,
+    props: {},
+    style: FloatLabelStyle,
+    provide() {
+        return {
+            $parentInstance: this
+        };
+    }
+};
+</script>
