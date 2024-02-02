c797f944a6c3b6dacaaf84354b5bc125fd947aaa
mertsincan <sincan.mert@gmail.com>
2024-02-02 15:47:25 +0000

    Refactor #4953 - For Galleria

--- components/lib/galleria/GalleriaContent.vue
+++ components/lib/galleria/GalleriaContent.vue
@@ -76,13 +76,16 @@ export default {
     emits: ['activeitem-change', 'mask-hide'],
     data() {
         return {
-            id: this.$attrs.id || UniqueComponentId(),
+            id: this.$attrs.id,
             activeIndex: this.$attrs.activeIndex,
             numVisible: this.$attrs.numVisible,
             slideShowActive: false
         };
     },
     watch: {
+        '$attrs.id': function (newValue) {
+            this.id = newValue || UniqueComponentId();
+        },
         '$attrs.value': function (newVal) {
             if (newVal && newVal.length < this.numVisible) {
                 this.numVisible = newVal.length;
@@ -97,6 +100,9 @@ export default {
         '$attrs.autoPlay': function (newVal) {
             newVal ? this.startSlideShow() : this.stopSlideShow();
         }
+    },
+    mounted() {
+        this.id = this.id || UniqueComponentId();
     },
     updated() {
         this.$emit('activeitem-change', this.activeIndex);
