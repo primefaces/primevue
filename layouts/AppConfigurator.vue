63d7798446299b5bfed9d2bfd464128748cbf50b
Cagatay Civici <cagatay.civici@gmail.com>
2024-02-14 00:42:42 +0300

    Fixed default surface

--- layouts/AppConfigurator.vue
+++ layouts/AppConfigurator.vue
@@ -42,7 +42,7 @@ export default {
     data() {
         return {
             selectedPrimaryColor: 'emerald',
-            selectedSurfaceColor: 'slate',
+            selectedSurfaceColor: null,
             primaryColors: [
                 { name: 'emerald', palette: { 50: '#ecfdf5', 100: '#d1fae5', 200: '#a7f3d0', 300: '#6ee7b7', 400: '#34d399', 500: '#10b981', 600: '#059669', 700: '#047857', 800: '#065f46', 900: '#064e3b', 950: '#022c22' } },
                 { name: 'green', palette: { 50: '#f0fdf4', 100: '#dcfce7', 200: '#bbf7d0', 300: '#86efac', 400: '#4ade80', 500: '#22c55e', 600: '#16a34a', 700: '#15803d', 800: '#166534', 900: '#14532d', 950: '#052e16' } },
