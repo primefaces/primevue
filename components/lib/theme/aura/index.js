da87ce386489200da262d1e41a8d53e2c8bbf89a
Cagatay Civici <cagatay.civici@gmail.com>
2024-02-13 09:30:15 +0300

    Migrated Dialog

--- components/lib/theme/aura/index.js
+++ components/lib/theme/aura/index.js
@@ -4,6 +4,7 @@ import badge from 'primevue/theme/aura/badge';
 import blockui from 'primevue/theme/aura/blockui';
 import card from 'primevue/theme/aura/card';
 import chip from 'primevue/theme/aura/chip';
+import dialog from 'primevue/theme/aura/dialog';
 import divider from 'primevue/theme/aura/divider';
 import fieldset from 'primevue/theme/aura/fieldset';
 import global from 'primevue/theme/aura/global';
@@ -26,7 +27,8 @@ export default {
         rounded: {
             sm: '4px',
             base: '6px',
-            lg: '12px'
+            lg: '8px',
+            xl: '12px'
         },
         emerald: { 50: '#ecfdf5', 100: '#d1fae5', 200: '#a7f3d0', 300: '#6ee7b7', 400: '#34d399', 500: '#10b981', 600: '#059669', 700: '#047857', 800: '#065f46', 900: '#064e3b', 950: '#022c22' },
         green: { 50: '#f0fdf4', 100: '#dcfce7', 200: '#bbf7d0', 300: '#86efac', 400: '#4ade80', 500: '#22c55e', 600: '#16a34a', 700: '#15803d', 800: '#166534', 900: '#14532d', 950: '#052e16' },
@@ -136,6 +138,7 @@ export default {
         blockui,
         card,
         chip,
+        dialog,
         divider,
         fieldset,
         metergroup,
