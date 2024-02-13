62e6aaeb28629a4c02e4bd9b3776710a4e21474a
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-02-13 09:22:45 +0300

    Fixed #5266 - New Stepper component

---
+++ components/lib/stepper/BaseStepper.vue
@@ -0,0 +1,29 @@
+<script>
+import BaseComponent from 'primevue/basecomponent';
+import StepperStyle from 'primevue/stepper/style';
+
+export default {
+    name: 'BaseStepper',
+    extends: BaseComponent,
+    props: {
+        activeStep: {
+            type: Number,
+            default: 0
+        },
+        orientation: {
+            type: String,
+            default: 'horizontal'
+        },
+        linear: {
+            type: Boolean,
+            default: false
+        }
+    },
+    style: StepperStyle,
+    provide() {
+        return {
+            $parentInstance: this
+        };
+    }
+};
+</script>
