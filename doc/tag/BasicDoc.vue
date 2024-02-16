1bb4f67536aeba385cda21133e669706589462c6
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-02-16 09:53:32 +0300

    Update BasicDoc.vue

--- doc/tag/BasicDoc.vue
+++ doc/tag/BasicDoc.vue
@@ -2,12 +2,8 @@
     <DocSectionText v-bind="$attrs">
         <p>Label of the tag is defined with the <i>value</i> property.</p>
     </DocSectionText>
-    <div class="card">
-        <Stepper>
-            <StepperPanel header="Header 1">Content 1</StepperPanel>
-            <StepperPanel header="Header 2">Content 2</StepperPanel>
-            <StepperPanel header="Header 3">Content 3</StepperPanel>
-        </Stepper>
+    <div class="card flex justify-content-center">
+        <Tag value="New"></Tag>
     </div>
     <DocSectionCode :code="code" />
 </template>
@@ -22,7 +18,7 @@ export default {
 `,
                 options: `
 <template>
-    <div class="flex justify-content-center">
+    <div class="card flex justify-content-center">
         <Tag value="New"></Tag>
     </div>
 </template>
@@ -33,7 +29,7 @@ export default {
 `,
                 composition: `
 <template>
-    <div class="flex justify-content-center">
+    <div class="card flex justify-content-center">
         <Tag value="New"></Tag>
     </div>
 </template>
