3bbd74287f8eda7eda8c6f079d08d81a53bf292e
Cagatay Civici <cagatay.civici@gmail.com>
2024-02-21 10:59:25 +0300

    Fixed mapping

--- components/lib/themes/primeone/presets/aura/calendar/index.js
+++ components/lib/themes/primeone/presets/aura/calendar/index.js
@@ -1,137 +1,135 @@
 export default {
-    variables: {
-        colorScheme: {
-            light: {
-                root: {
-                    background: '{surface.0}',
-                    borderColor: '{surface.200}',
-                    color: '{surface.700}'
-                },
-                header: {
-                    background: '{surface.0}',
-                    borderColor: '{surface.200}',
-                    color: '{surface.700}'
-                },
-                navigator: {
-                    backgroundHover: '{surface.100}',
-                    color: '{surface.500}',
-                    colorHover: '{surface.600}'
-                },
-                monthPicker: {
-                    backgroundHover: '{surface.100}',
-                    textColor: '{surface.700}',
-                    textColorHover: '{surface.800}'
-                },
-                yearPicker: {
-                    backgroundHover: '{surface.100}',
-                    textColor: '{surface.700}',
-                    textColorHover: '{surface.800}'
-                },
-                group: {
-                    borderColor: '{surface.200}'
-                },
-                date: {
-                    backgroundHover: '{surface.100}',
-                    backgroundSelected: '{highlight.background}',
-                    textColor: '{surface.700}',
-                    textColorHover: '{surface.800}',
-                    textColorSelected: '{highlight.text.color}'
-                },
-                today: {
-                    background: '{surface.200}',
-                    textColor: '{surface.900}'
-                },
-                month: {
-                    backgroundHover: '{surface.100}',
-                    backgroundSelected: '{highlight.background}',
-                    textColor: '{surface.700}',
-                    textColorHover: '{surface.800}',
-                    textColorSelected: '{highlight.text.color}'
-                },
-                year: {
-                    backgroundHover: '{surface.100}',
-                    backgroundSelected: '{highlight.background}',
-                    textColor: '{surface.700}',
-                    textColorHover: '{surface.800}',
-                    textColorSelected: '{highlight.text.color}'
-                },
-                buttonbar: {
-                    borderColor: '{surface.200}'
-                },
-                timepicker: {
-                    borderColor: '{surface.200}'
-                },
-                timepickerSpin: {
-                    backgroundHover: '{surface.100}',
-                    color: '{surface.500}',
-                    colorHover: '{surface.600}'
-                }
+    colorScheme: {
+        light: {
+            root: {
+                background: '{surface.0}',
+                borderColor: '{surface.200}',
+                color: '{surface.700}'
             },
-            dark: {
-                root: {
-                    background: '{surface.900}',
-                    borderColor: '{surface.700}',
-                    color: '{surface.0}'
-                },
-                header: {
-                    background: '{surface.900}',
-                    borderColor: '{surface.700}',
-                    color: '{surface.0}'
-                },
-                navigator: {
-                    backgroundHover: '{surface.800}',
-                    color: '{surface.400}',
-                    colorHover: '{surface.300}'
-                },
-                monthPicker: {
-                    backgroundHover: '{surface.800}',
-                    textColor: '{surface.0}',
-                    textColorHover: '{surface.0}'
-                },
-                yearPicker: {
-                    backgroundHover: '{surface.800}',
-                    textColor: '{surface.0}',
-                    textColorHover: '{surface.0}'
-                },
-                group: {
-                    borderColor: '{surface.700}'
-                },
-                date: {
-                    backgroundHover: '{surface.800}',
-                    backgroundSelected: '{highlight.background}',
-                    textColor: '{surface.0}',
-                    textColorHover: '{surface.0}',
-                    textColorSelected: '{highlight.text.color}'
-                },
-                today: {
-                    background: '{surface.700}',
-                    textColor: '{surface.0}'
-                },
-                month: {
-                    backgroundHover: '{surface.800}',
-                    backgroundSelected: '{highlight.background}',
-                    textColor: '{surface.0}',
-                    textColorHover: '{surface.0}',
-                    textColorSelected: '{highlight.text.color}'
-                },
-                year: {
-                    backgroundHover: '{surface.800}',
-                    backgroundSelected: '{highlight.background}',
-                    textColor: '{surface.0}',
-                    textColorHover: '{surface.0}',
-                    textColorSelected: '{highlight.text.color}'
-                },
-                buttonbar: {
-                    borderColor: '{surface.700}'
-                },
-                timepicker: {
-                    borderColor: '{surface.700}'
-                },
-                timepickerSpin: {
-                    backgroundHover: '{surface.800}',
-                    color: '{surface.400}',
-                    colorHover: '{surface.300}'
-                }
+            header: {
+                background: '{surface.0}',
+                borderColor: '{surface.200}',
+                color: '{surface.700}'
+            },
+            navigator: {
+                backgroundHover: '{surface.100}',
+                color: '{surface.500}',
+                colorHover: '{surface.600}'
+            },
+            monthPicker: {
+                backgroundHover: '{surface.100}',
+                textColor: '{surface.700}',
+                textColorHover: '{surface.800}'
+            },
+            yearPicker: {
+                backgroundHover: '{surface.100}',
+                textColor: '{surface.700}',
+                textColorHover: '{surface.800}'
+            },
+            group: {
+                borderColor: '{surface.200}'
+            },
+            date: {
+                backgroundHover: '{surface.100}',
+                backgroundSelected: '{highlight.background}',
+                textColor: '{surface.700}',
+                textColorHover: '{surface.800}',
+                textColorSelected: '{highlight.text.color}'
+            },
+            today: {
+                background: '{surface.200}',
+                textColor: '{surface.900}'
+            },
+            month: {
+                backgroundHover: '{surface.100}',
+                backgroundSelected: '{highlight.background}',
+                textColor: '{surface.700}',
+                textColorHover: '{surface.800}',
+                textColorSelected: '{highlight.text.color}'
+            },
+            year: {
+                backgroundHover: '{surface.100}',
+                backgroundSelected: '{highlight.background}',
+                textColor: '{surface.700}',
+                textColorHover: '{surface.800}',
+                textColorSelected: '{highlight.text.color}'
+            },
+            buttonbar: {
+                borderColor: '{surface.200}'
+            },
+            timepicker: {
+                borderColor: '{surface.200}'
+            },
+            timepickerSpin: {
+                backgroundHover: '{surface.100}',
+                color: '{surface.500}',
+                colorHover: '{surface.600}'
+            }
+        },
+        dark: {
+            root: {
+                background: '{surface.900}',
+                borderColor: '{surface.700}',
+                color: '{surface.0}'
+            },
+            header: {
+                background: '{surface.900}',
+                borderColor: '{surface.700}',
+                color: '{surface.0}'
+            },
+            navigator: {
+                backgroundHover: '{surface.800}',
+                color: '{surface.400}',
+                colorHover: '{surface.300}'
+            },
+            monthPicker: {
+                backgroundHover: '{surface.800}',
+                textColor: '{surface.0}',
+                textColorHover: '{surface.0}'
+            },
+            yearPicker: {
+                backgroundHover: '{surface.800}',
+                textColor: '{surface.0}',
+                textColorHover: '{surface.0}'
+            },
+            group: {
+                borderColor: '{surface.700}'
+            },
+            date: {
+                backgroundHover: '{surface.800}',
+                backgroundSelected: '{highlight.background}',
+                textColor: '{surface.0}',
+                textColorHover: '{surface.0}',
+                textColorSelected: '{highlight.text.color}'
+            },
+            today: {
+                background: '{surface.700}',
+                textColor: '{surface.0}'
+            },
+            month: {
+                backgroundHover: '{surface.800}',
+                backgroundSelected: '{highlight.background}',
+                textColor: '{surface.0}',
+                textColorHover: '{surface.0}',
+                textColorSelected: '{highlight.text.color}'
+            },
+            year: {
+                backgroundHover: '{surface.800}',
+                backgroundSelected: '{highlight.background}',
+                textColor: '{surface.0}',
+                textColorHover: '{surface.0}',
+                textColorSelected: '{highlight.text.color}'
+            },
+            buttonbar: {
+                borderColor: '{surface.700}'
+            },
+            timepicker: {
+                borderColor: '{surface.700}'
+            },
+            timepickerSpin: {
+                backgroundHover: '{surface.800}',
+                color: '{surface.400}',
+                colorHover: '{surface.300}'
             }
         }
     }
