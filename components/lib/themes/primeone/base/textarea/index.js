d36dabf2575ad9682c695b576a1d20b44c34fac5
Cagatay Civici <cagatay.civici@gmail.com>
2024-03-25 21:59:56 +0300

    Fixed textarea

--- components/lib/themes/primeone/base/textarea/index.js
+++ components/lib/themes/primeone/base/textarea/index.js
@@ -4,46 +4,46 @@ export default {
     font-family: inherit;
     font-feature-settings: inherit;
     font-size: 1rem;
-    color: ${dt('textara.color')};
-    background: ${dt('textara.background')};
+    color: ${dt('textarea.color')};
+    background: ${dt('textarea.background')};
     padding: 0.5rem 0.75rem;
-    border: 1px solid ${dt('textara.border.color')};
+    border: 1px solid ${dt('textarea.border.color')};
     transition: background-color ${dt('transition.duration')}, color ${dt('transition.duration')}, border-color ${dt('transition.duration')}, outline-color ${dt('transition.duration')};
     appearance: none;
     border-radius: ${dt('rounded.base')};
     outline-color: transparent;
-    box-shadow: ${dt('textara.box.shadow')};
+    box-shadow: ${dt('textarea.box.shadow')};
 }
 
 .p-inputtextarea:enabled:hover {
-    border-color: ${dt('textara.hover.border.color')};
+    border-color: ${dt('textarea.hover.border.color')};
 }
 
 .p-inputtextarea:enabled:focus {
-    border-color: ${dt('textara.focus.border.color')};
+    border-color: ${dt('textarea.focus.border.color')};
     outline: 0 none;
 }
 
 .p-inputtextarea.p-invalid {
-    border-color: ${dt('textara.invalid.border.color')};
+    border-color: ${dt('textarea.invalid.border.color')};
 }
 
 .p-inputtextarea.p-variant-filled {
-    background-color: ${dt('textara.filled.background')};
+    background-color: ${dt('textarea.filled.background')};
 }
 
 .p-inputtextarea.p-variant-filled:enabled:focus {
-    background-color: ${dt('textara.filled.focus.background')};
+    background-color: ${dt('textarea.filled.focus.background')};
 }
 
 .p-inputtextarea:disabled {
     opacity: 1;
-    background: ${dt('textara.disabled.background')};
-    color: ${dt('textara.disabled.color')};
+    background: ${dt('textarea.disabled.background')};
+    color: ${dt('textarea.disabled.color')};
 }
 
 .p-inputtextarea::placeholder {
-    color: ${dt('textara.placeholder.color')};
+    color: ${dt('textarea.placeholder.color')};
 }
 
 .p-fluid .p-inputtextarea {
