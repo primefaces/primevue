3541f7af2570433727e45eb5f8d4deb2c6b599ac
Cagatay Civici <cagatay.civici@gmail.com>
2024-03-31 02:16:33 +0300

    Set default as noir

--- layouts/AppConfigurator.vue
+++ layouts/AppConfigurator.vue
@@ -44,7 +44,7 @@ import { updatePreset, updateSurface } from 'primevue/themes';
 export default {
     data() {
         return {
-            selectedPrimaryColor: 'emerald',
+            selectedPrimaryColor: 'noir',
             selectedSurfaceColor: null,
             primaryColors: [
                 { name: 'noir', palette: { 50: '#fafafa', 100: '#f4f4f5', 200: '#e4e4e7', 300: '#d4d4d8', 400: '#a1a1aa', 500: '#71717a', 600: '#52525b', 700: '#3f3f46', 800: '#27272a', 900: '#18181b', 950: '#09090b' } },
