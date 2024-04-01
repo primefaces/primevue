835e6ca2343c57d6bf941b7fa825b8f94f9bcbd7
mertsincan <sincan.mert@gmail.com>
2024-04-01 11:12:58 +0100

    Theming API: Rename `updatePrimary` and `updateSurface` with `updatePrimaryPalette` and `updateSurfacePalette`

--- components/lib/themes/actions/index.js
+++ components/lib/themes/actions/index.js
@@ -1,6 +1,6 @@
 export { default as definePreset } from './definePreset';
 export { default as updatePreset } from './updatePreset';
-export { default as updatePrimary } from './updatePrimary';
-export { default as updateSurface } from './updateSurface';
+export { default as updatePrimaryPalette } from './updatePrimaryPalette';
+export { default as updateSurfacePalette } from './updateSurfacePalette';
 export { default as usePreset } from './usePreset';
 export { default as useTheme } from './useTheme';
