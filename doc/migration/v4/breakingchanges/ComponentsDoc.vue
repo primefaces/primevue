9a3fdf3c394eaea29e4fbceb660d9f56655a1337
Cagatay Civici <cagatay.civici@gmail.com>
2024-04-01 10:43:18 +0300

    Initiate migration guide

---
+++ doc/migration/v4/breakingchanges/ComponentsDoc.vue
@@ -0,0 +1,13 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>The team has put in a great effort to migrate the entire UI suite with minor breaking changes. Two components have been removed by design.</p>
+    </DocSectionText>
+    <ul class="flex flex-column gap-2">
+        <li><i>DataViewLayoutOptions</i> is removed, use SelectButton instead as in the demo.</li>
+        <li><i>TriStateCheckbox</i> is removed, use Checkbox with the new <i>indeterminate</i> state as a replacement.</li>
+        <li>
+            Enhancements to internal structure of some components, see the tickets <a href="https://tailwind.primevue.org" target="_blank" rel="noopener noreferrer" class="doc-link">#5426</a> and
+            <a href="https://tailwind.primevue.org" target="_blank" rel="noopener noreferrer" class="doc-link">#5437</a> for more information.
+        </li>
+    </ul>
+</template>
---
+++ pages/migration/v4/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <Head>
+        <Title>Migration - PrimeVue</Title>
+        <Meta name="description" content="Migration Guide to PrimeVue v4." />
+    </Head>
+    <div class="doc">
+        <div class="doc-main">
+            <div class="doc-intro">
+                <h1>Introduction</h1>
+                <p>Migration guide to PrimeVue v4.</p>
+            </div>
+            <DocSections :docs="docs" />
+        </div>
+        <DocSectionNav :docs="docs" />
+    </div>
+</template>
+
+<script>
+import NewFeaturesDoc from '@/doc/migration/v4/NewFeaturesDoc.vue';
+import OverviewDoc from '@/doc/migration/v4/OverviewDoc.vue';
+import ComponentsDoc from '@/doc/migration/v4/breakingchanges/ComponentsDoc.vue';
+import StyledModeDoc from '@/doc/migration/v4/breakingchanges/StyledModeDoc.vue';
+
+export default {
+    data() {
+        return {
+            docs: [
+                {
+                    id: 'overview',
+                    label: 'Overview',
+                    component: OverviewDoc
+                },
+                {
+                    id: 'newfeatures',
+                    label: 'New Features',
+                    component: NewFeaturesDoc
+                },
+                {
+                    id: 'breakingchanges',
+                    label: 'Breaking Changes',
+                    children: [
+                        {
+                            id: 'styledmode',
+                            label: 'Styled Mode',
+                            component: StyledModeDoc
+                        },
+                        {
+                            id: 'components',
+                            label: 'Components',
+                            component: ComponentsDoc
+                        }
+                    ]
+                }
+            ]
+        };
+    }
+};
+</script>
