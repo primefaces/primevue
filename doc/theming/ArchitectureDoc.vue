e9a77efa59dd1799160a5b0f12fd63799a668429
Cagatay Civici <cagatay.civici@gmail.com>
2024-04-02 13:48:54 +0300

    Add links to v4 beta

--- doc/theming/ArchitectureDoc.vue
+++ doc/theming/ArchitectureDoc.vue
@@ -1,6 +1,8 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p class="notification"><strong>Note</strong>: By the end of Q1 2024, PrimeVue v4 will be introduced with a modern theming architecture based on design tokens and css variables.</p>
+        <p class="notification">
+            <strong>Note</strong>: PrimeVue v4 has a brand new styled mode implementation, visit the <a href="https://v4.primevue.org/theming/styled" target="_blank" rel="noopener noreferrer">v4 documentation</a> for more information.
+        </p>
         <p>
             PrimeVue is a design agnostic library so unlike other UI libraries it does not enforce a certain styling such as material or bootstrap. In order to achieve this, styling has been separated into two parts, core and theme. The core resides
             inside PrimeVue to implement the structure of the components such as positioning whereas theme brings the colors and spacing.
