1bdaf7357653fca91a7316fbcdaae1abea9ad7bd
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-04-25 09:54:35 +0300

    Fixed #5170 - InputNumber can't enter 0.0x using minFractionDigits/mode="currency"

--- components/lib/inputnumber/InputNumber.vue
+++ components/lib/inputnumber/InputNumber.vue
@@ -856,10 +856,7 @@ export default {
                     this.$refs.input.$el.setSelectionRange(selectionEnd, selectionEnd);
                 } else if (newLength === currentLength) {
                     if (operation === 'insert' || operation === 'delete-back-single') {
-                        const re = /[.,]/g;
-                        const newSelectionEnd = selectionEnd + Number(re.test(value) || re.test(insertedValueStr));
-
-                        this.$refs.input.$el.setSelectionRange(newSelectionEnd, newSelectionEnd);
+                        this.$refs.input.$el.setSelectionRange(selectionEnd + 1, selectionEnd + 1);
                     } else if (operation === 'delete-single') {
                         this.$refs.input.$el.setSelectionRange(selectionEnd - 1, selectionEnd - 1);
                     } else if (operation === 'delete-range' || operation === 'spin') {
