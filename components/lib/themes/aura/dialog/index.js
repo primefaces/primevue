4e16e5098ebb40537f8089e87698e38edf230d58
Cagatay Civici <cagatay.civici@gmail.com>
2024-05-04 16:08:23 +0300

    Refactored overlay component tokens

--- components/lib/themes/aura/dialog/index.js
+++ components/lib/themes/aura/dialog/index.js
@@ -1,18 +1,7 @@
 export default {
-    colorScheme: {
-        light: {
-            root: {
-                background: '{surface.0}',
-                borderColor: '{surface.200}',
-                color: '{surface.700}'
-            }
-        },
-        dark: {
-            root: {
-                background: '{surface.900}',
-                borderColor: '{surface.700}',
-                color: '{surface.0}'
-            }
-        }
+    root: {
+        background: '{content.background}',
+        borderColor: '{content.border.color}',
+        color: '{content.color}'
     }
 };
