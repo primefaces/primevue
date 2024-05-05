c0e1093898003a1115c657823ae863582ab03567
Cagatay Civici <cagatay.civici@gmail.com>
2024-05-05 19:15:16 +0300

    Updated inputgroup dt

--- components/lib/themes/aura/inputgroup/index.js
+++ components/lib/themes/aura/inputgroup/index.js
@@ -2,6 +2,6 @@ export default {
     addon: {
         background: '{form.field.background}',
         borderColor: '{form.field.border.color}',
-        color: '{form.field.placeholder.color}'
+        color: '{form.field.icon.color}'
     }
 };
