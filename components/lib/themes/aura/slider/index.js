87b6b38f8c1412c4d47b5a8ed8b5db5680b8fce0
Cagatay Civici <cagatay.civici@gmail.com>
2024-05-05 18:58:31 +0300

    Refactored Slider tokens

--- components/lib/themes/aura/slider/index.js
+++ components/lib/themes/aura/slider/index.js
@@ -1,29 +1,23 @@
 export default {
+    track: {
+        background: '{content.border.color}'
+    },
+    range: {
+        background: '{primary.color}'
+    },
+    handle: {
+        background: '{content.border.color}',
+        hoverBackground: '{content.border.color}'
+    },
     colorScheme: {
         light: {
-            track: {
-                background: '{surface.200}'
-            },
             handle: {
-                background: '{surface.200}',
-                hoverBackground: '{surface.200}',
                 contentBackground: '{surface.0}'
-            },
-            range: {
-                background: '{primary.color}'
             }
         },
         dark: {
-            track: {
-                background: '{surface.700}'
-            },
             handle: {
-                background: '{surface.700}',
-                hoverBackground: '{surface.700}',
                 contentBackground: '{surface.950}'
-            },
-            range: {
-                background: '{primary.color}'
             }
         }
     }
