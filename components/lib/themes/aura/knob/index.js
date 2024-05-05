924777045139368be251fe2b50ae71c4dcc9e4cc
Cagatay Civici <cagatay.civici@gmail.com>
2024-05-05 19:21:59 +0300

    Update knob tokens

--- components/lib/themes/aura/knob/index.js
+++ components/lib/themes/aura/knob/index.js
@@ -1,26 +1,11 @@
 export default {
-    colorScheme: {
-        light: {
-            value: {
-                background: '{primary.color}'
-            },
-            range: {
-                background: '{surface.200}'
-            },
-            text: {
-                color: '{surface.500}'
-            }
-        },
-        dark: {
-            value: {
-                background: '{primary.color}'
-            },
-            range: {
-                background: '{surface.700}'
-            },
-            text: {
-                color: '{surface.400}'
-            }
-        }
+    value: {
+        background: '{primary.color}'
+    },
+    range: {
+        background: '{content.border.color}'
+    },
+    text: {
+        color: '{text.muted.color}'
     }
 };
