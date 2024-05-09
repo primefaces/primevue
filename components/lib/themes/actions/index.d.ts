210a4ad2890d5c0150eb55155221c1a27715b205
Mert Sincan <sincan.mert@gmail.com>
2024-05-09 11:32:51 +0100

    Added d.ts file to theme core (1/2)

---
+++ components/lib/themes/actions/index.d.ts
@@ -0,0 +1,7 @@
+/* @todo - remove any types */
+export function definePreset(...arg: any[]): any;
+export function updatePreset(...arg: any[]): any;
+export function updatePrimaryPalette(palette?: any): any;
+export function updateSurfacePalette(palette?: any): string;
+export function usePreset(...arg: any[]): any;
+export function useTheme(theme: any): any;
