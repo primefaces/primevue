3cd90df52711f64e60dc6d575ff93822b1c451fa
Mert Sincan <sincan.mert@gmail.com>
2024-05-21 13:38:25 +0100

    Added index.json

--- components/lib/styleclass/StyleClass.d.ts
+++ components/lib/styleclass/StyleClass.d.ts
@@ -1,6 +1,6 @@
 /**
  *
- * StyleClass manages css classes declaratively to during enter/leave animations or just to toggle classes on an element.
+ * StyleClass manages CSS classes declaratively to during enter/leave animations or just to toggle classes on an element.
  *
  * [Live Demo](https://primevue.org/styleclass)
  *
