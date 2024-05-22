1d46aec1646966e0fa3272419140934e202fb7b2
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-05-22 10:31:06 +0300

    Update VirtualScrollerStyle.d.ts

--- components/lib/virtualscroller/style/VirtualScrollerStyle.d.ts
+++ components/lib/virtualscroller/style/VirtualScrollerStyle.d.ts
@@ -9,6 +9,12 @@
  */
 import { BaseStyle } from '../../base/style';
 
-export enum VirtualScrollerClasses {}
+export enum VirtualScrollerClasses {
+    root = 'p-virtualscroller',
+    content = 'p-virtualscroller-content',
+    spacer = 'p-virtualscroller-spacer',
+    loader = 'p-virtualscroller-loader',
+    loadingIcon = 'p-virtualscroller-loading-icon'
+}
 
 export interface VirtualScrollerStyle extends BaseStyle {}
