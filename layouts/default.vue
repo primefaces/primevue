e96f2070e1c3e67429758acbae8931a3e6154e81
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-05-24 10:25:00 +0300

    Error page dark mode change fixes

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,6 +19,7 @@
 
 <script>
 import DomHandler from '@/components/lib/utils/DomHandler';
+import EventBus from '@/layouts/AppEventBus';
 import AppFooter from './AppFooter.vue';
 import AppMenu from './AppMenu.vue';
 import AppNews from './AppNews.vue';
@@ -44,7 +45,32 @@ export default {
             }
         }
     },
+    mounted() {
+        EventBus.on('dark-mode-toggle', this.darkModeToggleListener);
+    },
+    beforeUnmount() {
+        EventBus.off('dark-mode-toggle', this.darkModeToggleListener);
+    },
     methods: {
+        darkModeToggleListener(event) {
+            if (!document.startViewTransition) {
+                this.toggleDarkMode(event);
+
+                return;
+            }
+
+            document.startViewTransition(() => this.toggleDarkMode(event));
+        },
+        toggleDarkMode(event) {
+            const isDark = event.dark;
+
+            if (isDark) document.documentElement.classList.add('p-dark');
+            else document.documentElement.classList.remove('p-dark');
+
+            this.$appState.darkTheme = isDark;
+
+            EventBus.emit('dark-mode-toggle-complete');
+        },
         onMenuButtonClick() {
             if (this.sidebarActive) {
                 this.sidebarActive = false;
