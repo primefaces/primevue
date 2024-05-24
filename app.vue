afdbfc9aaf325eb1ef11eeac37b45c7dfc2bfce1
Cagatay Civici <cagatay.civici@gmail.com>
2024-05-24 11:17:58 +0300

    Revert "Error page dark mode change fixes"
    This reverts commit e96f2070e1c3e67429758acbae8931a3e6154e81.

--- app.vue
+++ app.vue
@@ -3,3 +3,39 @@
         <NuxtPage />
     </NuxtLayout>
 </template>
+
+<script>
+import EventBus from '@/layouts/AppEventBus';
+
+export default {
+    mounted() {
+        EventBus.on('dark-mode-toggle', this.darkModeToggleListener);
+    },
+    beforeUnmount() {
+        EventBus.off('dark-mode-toggle', this.darkModeToggleListener);
+    },
+    methods: {
+        darkModeToggleListener(event) {
+            if (!document.startViewTransition) {
+                this.toggleDarkMode(event);
+
+                return;
+            }
+
+            document.startViewTransition(() => this.toggleDarkMode(event));
+        },
+        toggleDarkMode(event) {
+            const isDark = event.dark;
+
+            if (isDark) document.documentElement.classList.add('p-dark');
+            else document.documentElement.classList.remove('p-dark');
+
+            this.$appState.darkTheme = isDark;
+
+            EventBus.emit('dark-mode-toggle-complete');
+        }
+    }
+};
+</script>
+
+<style lang="scss"></style>
