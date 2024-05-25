eb4523a9348c50ad75035bf8f704fce02ad4d4d7
Cagatay Civici <cagatay.civici@gmail.com>
2024-05-25 14:30:46 +0300

    Fixed for zero numeric value

--- components/lib/tag/Tag.vue
+++ components/lib/tag/Tag.vue
@@ -2,7 +2,7 @@
     <span :class="cx('root')" v-bind="ptmi('root')">
         <component v-if="$slots.icon" :is="$slots.icon" :class="cx('icon')" v-bind="ptm('icon')" />
         <span v-else-if="icon" :class="[cx('icon'), icon]" v-bind="ptm('icon')"></span>
-        <slot v-if="value || $slots.default">
+        <slot v-if="value != null || $slots.default">
             <span :class="cx('label')" v-bind="ptm('label')">{{ value }}</span>
         </slot>
     </span>
