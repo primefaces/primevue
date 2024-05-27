2feb01c7140052fd73d44fe6c6a93642e58e75c2
Cagatay Civici <cagatay.civici@gmail.com>
2024-05-27 10:12:51 +0300

    Update error page

--- error.vue
+++ error.vue
@@ -1,16 +1,15 @@
 <template>
-    <div>
-        <NuxtLayout name="default">
-            <div class="flex card flex-col items-center gap-8 sm:p-20">
-                <div class="flex flex-col sm:flex-row items-center justify-center gap-4 text-primary">
-                    <span class="font-bold" style="font-size: 144px"> 4 </span>
-                    <div class="flex items-center justify-center bg-primary text-primary-contrast rounded-full w-32 h-32">
-                        <i class="pi pi-prime" style="font-size: 4.5rem"></i>
-                    </div>
-                    <span class="font-bold" style="font-size: 144px"> 4 </span>
+    <div class="flex min-h-screen items-center justify-center">
+        <div class="flex card flex-col items-center gap-8 sm:p-20">
+            <div class="flex flex-col sm:flex-row items-center justify-center gap-4 text-primary">
+                <span class="font-bold text-9xl"> 4 </span>
+                <div class="flex items-center justify-center bg-primary text-primary-contrast rounded-full w-28 h-28">
+                    <i class="pi pi-prime text-6xl"></i>
                 </div>
-                <div class="font-bold text-center text-6xl border-t border-surface-200 dark:border-surface-700 pt-8">PAGE NOT FOUND</div>
+                <span class="font-bold text-9xl"> 4 </span>
             </div>
-        </NuxtLayout>
+            <div class="font-bold text-center text-4xl border-t border-surface pt-8">Page Not Found.</div>
+            <NuxtLink to="/"><Button label="GO TO HOMEPAGE" /></NuxtLink>
+        </div>
     </div>
 </template>
