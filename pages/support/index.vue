2c1047fe1b75548cf2c2d85a8f740c9ce0aec126
Cagatay Civici <cagatay.civici@gmail.com>
2024-06-06 21:50:49 +0300

    Update doc

--- pages/support/index.vue
+++ pages/support/index.vue
@@ -6,14 +6,13 @@
                 <div class="text-2xl font-semibold mb-4 relative">Community Support</div>
                 <p class="m-0 leading-normal relative text-lg">
                     <a href="https://github.com/orgs/primefaces/discussions" class="doc-link" target="_blank" rel="noopener noreferrer">Forum</a> and
-                    <a href="https://discord.gg/gzKFYnpmCY" class="doc-link" target="_blank" rel="noopener noreferrer">Discord</a> are where the community users gather to seek support, post topics and discuss the technology. GitHub issue tracker is
-                    the channel where community users can create tickets however PrimeTek does not guarantee a response time although they are monitored and maintained by our staff. If you need to secure a response, you may consider PRO support
-                    instead.
+                    <a href="https://discord.gg/gzKFYnpmCY" class="doc-link" target="_blank" rel="noopener noreferrer">Discord</a> are where the community users gather to seek support, post topics and discuss the technology. GitHub issue is the
+                    channel for the community users to create tickets however PrimeTek does not guarantee a response time although they are monitored and maintained by our staff. If you need to secure a response, you may consider PRO support instead.
                 </p>
             </div>
             <div class="card !m-0 flex-1 !bg-primary !text-primary-contrast font-medium">
                 <div class="text-2xl font-semibold mb-4">Professional Support</div>
-                <p class="leading-normal text-lg">
+                <p class="m-0 leading-normal text-lg">
                     With PRO support, it's easy to support, tune, and add features to PrimeVue as an in-house library. With the exclusive services of a PRO account, you no longer need to post questions in the community forum and the community issue
                     tracker at GitHub. Service is delivered via a private issue tracker based on a one-business-day response time.
                 </p>
@@ -22,8 +21,8 @@
 
         <div class="grid grid-cols-3 gap-8 mb-8">
             <div class="card !m-0 col-span-full lg:col-span-1">
-                <div class="flex justify-between">
-                    <div class="text-2xl font-semibold mb-4">Service Features</div>
+                <div class="flex items-center justify-between mb-4">
+                    <div class="text-2xl font-semibold">Service Features</div>
                     <span class="font-bold text-lg">200$/h</span>
                 </div>
                 <div class="gap-8 px-4">
@@ -50,7 +49,7 @@
                         </li>
                         <li class="flex items-center mb-4">
                             <i class="pi pi-check-circle mr-4 text-green-500"></i>
-                            <span class="leading-normal">Minimum 30 Hours for initiation</span>
+                            <span class="leading-normal">Minimum 25 Hours for initiation</span>
                         </li>
                         <li class="flex items-center mb-4">
                             <i class="pi pi-check-circle mr-4 text-green-500"></i>
@@ -70,7 +69,12 @@
                         </li>
                     </ul>
                 </div>
-                <a href="mailto:primetek.com.tr" class="block mt-4 w-full bg-primary rounded-border py-3 px-4 hover:bg-primary-emphasis text-center transition-all duration-300 text-primary-contrast font-semibold text-lg leading-none"> Buy Now </a>
+                <a
+                    href="mailto:primetek.com.tr?subject=PrimeVue%20PRO%20Support"
+                    class="block mt-4 w-full bg-primary rounded-border py-3 px-4 hover:bg-primary-emphasis text-center transition-all duration-300 text-primary-contrast font-semibold text-lg leading-none"
+                >
+                    Buy Now
+                </a>
             </div>
             <div class="card !m-0 col-span-full lg:col-span-2">
                 <div class="text-2xl font-semibold mb-4">How It Works</div>
@@ -85,7 +89,7 @@
                     </li>
                     <li>
                         <div class="font-semibold mb-1">3. Request Support</div>
-                        <span class="leading-normal">Create a ticket using the exclusive private issue tracker with a description of the issue.</span>
+                        <span class="leading-normal">Create a ticket with a description of the issue and receive a reply within 1 business day.</span>
                     </li>
                     <li>
                         <div class="font-semibold mb-1">4. Estimate and Approval</div>
@@ -98,7 +102,7 @@
                     <li>
                         <div class="font-semibold mb-1">6. Delivery</div>
                         <span class="leading-normal"
-                            >If the issue requires an updated in the library, it gets published to npm by as part of the public PrimeVue package. A patch update on an older version can also be requested if you are not using the latest version.
+                            >If the issue requires an update in the library, it gets published to npm by as part of the public PrimeVue package. A patch update on an older version can also be requested if you are not using the latest version.
                         </span>
                     </li>
                 </ul>
@@ -133,8 +137,8 @@
                 </div>
             </div>
 
-            <div class="border-t border-surface pt-4 flex flex-col items-center">
-                <p class="m-0 leading-normal mb-8 text-lg">
+            <div class="border-t border-surface pt-8 flex flex-col items-center">
+                <p class="leading-normal mb-8 text-lg">
                     We are proud to collaborate with <a href="https://www.epicmax.co/contacts" target="_blank" rel="noopener noreferrer" class="doc-link">EpicMax</a> that share our commitment to excellence and open source values.
                 </p>
 
@@ -162,7 +166,7 @@
                 <div class="flex-1 flex flex-col gap-8">
                     <div>
                         <div class="leading-normal mb-2 font-semibold">How many issue tracker accounts do we get?</div>
-                        <p class="!m-0 leading-normal">We provide one account per organization.</p>
+                        <p class="!m-0 leading-normal">We provide at most 5 accounts per organization.</p>
                     </div>
                     <div>
                         <div class="leading-normal mb-2 font-semibold">What is the duration of the service?</div>
@@ -172,15 +176,23 @@
                         <div class="leading-normal mb-2 font-semibold">What happens if we extend after 1 year and we have unused hours?</div>
                         <p class="!m-0 leading-normal">Unused hours expire and are not transferred to the new term.</p>
                     </div>
-                </div>
-                <div class="flex-1 flex flex-col gap-8">
-                    <div>
-                        <div class="leading-normal mb-2 font-semibold">Is there a minimum hour requirement?</div>
-                        <p class="!m-0 leading-normal">At least 30 hours are required to initiate the service.</p>
-                    </div>
                     <div>
                         <div class="leading-normal mb-2 font-semibold">What are hours used for?</div>
                         <p class="!m-0 leading-normal">Hours are utilized when creating tickets, filing defects, requesting enhancements, POC implementations of a use case and questions.</p>
+                    </div>
+                </div>
+                <div class="flex-1 flex flex-col gap-8">
+                    <div>
+                        <div class="leading-normal mb-2 font-semibold">Are the changes delivered with a custom build?</div>
+                        <p class="!m-0 leading-normal">No, changes become part of the PrimeVue core and pushed to the public npm package on next update.</p>
+                    </div>
+                    <div>
+                        <div class="leading-normal mb-2 font-semibold">Who provides the support service?</div>
+                        <p class="!m-0 leading-normal">Support service is provided by the PrimeVue team at PrimeTek.</p>
+                    </div>
+                    <div>
+                        <div class="leading-normal mb-2 font-semibold">Is there a minimum hour requirement?</div>
+                        <p class="!m-0 leading-normal">At least 25 hours are required to initiate the service.</p>
                     </div>
                     <div>
                         <div class="leading-normal mb-2 font-semibold">What happens if the issue takes longer or shorter than the approved estimate?</div>
@@ -189,12 +201,16 @@
                 </div>
                 <div class="flex-1 flex flex-col gap-8">
                     <div>
-                        <div class="leading-normal mb-2 font-semibold">Who provides the service?</div>
-                        <p class="!m-0 leading-normal">Support service is provided by the PrimeVue team at PrimeTek.</p>
-                    </div>
-                    <div>
                         <div class="leading-normal mb-2 font-semibold">Can we request new features and enhancements?</div>
                         <p class="!m-0 leading-normal">Yes, initially the request needs to be approved by PrimeTek based on project roadmap fit. As a result, not all requests may be accepted.</p>
+                    </div>
+                    <div>
+                        <div class="leading-normal mb-2 font-semibold">What are the payment terms?</div>
+                        <p class="!m-0 leading-normal">Payment in advance is required to initiate the service.</p>
+                    </div>
+                    <div>
+                        <div class="leading-normal mb-2 font-semibold">What payment methods are available?</div>
+                        <p class="!m-0 leading-normal">Credit card and bank wire transfers are the available options.</p>
                     </div>
                     <div>
                         <div class="leading-normal mb-2 font-semibold">We are a reseller, can we purchase this support for our client?</div>
