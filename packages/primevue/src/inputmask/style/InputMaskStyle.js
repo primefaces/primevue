00d37a2c6f1cd22622ab5b6f984d5937689e57a6
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-06-26 12:46:45 +0300

    Fixed #5956 - InputMask: FloatLabel defects

--- packages/primevue/src/inputmask/style/InputMaskStyle.js
+++ packages/primevue/src/inputmask/style/InputMaskStyle.js
@@ -1,7 +1,12 @@
 import BaseStyle from '@primevue/core/base/style';
 
 const classes = {
-    root: 'p-inputmask'
+    root: ({ instance }) => [
+        'p-inputmask  p-inputwrapper',
+        {
+            'p-inputwrapper-filled': instance.filled
+        }
+    ]
 };
 
 export default BaseStyle.extend({
