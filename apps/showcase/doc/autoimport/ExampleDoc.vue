a1097de2f8769746dd27c71b3be5b753a66259c4
Cagatay Civici <cagatay.civici@gmail.com>
2024-07-02 15:35:25 +0300

    Update auto import docs

--- apps/showcase/doc/autoimport/ExampleDoc.vue
+++ apps/showcase/doc/autoimport/ExampleDoc.vue
@@ -1,8 +1,5 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p>
-            A complete example using a PrimeVue and unplugin is available at <a href="https://github.com/primefaces/primevue-examples/tree/main/auto-import" rel="noopener noreferrer">primevue-examples</a> repository. You can also view this sample
-            live at <a href="https://stackblitz.com/edit/vitejs-vite-cbjzs7?file=vite.config.ts" target="_blank" rel="noopener noreferrer">Stackblitz</a>.
-        </p>
+        <p>A complete example using PrimeVue with auto imports is available at the <NuxtLink to="/playground">playground</NuxtLink>.</p>
     </DocSectionText>
 </template>
