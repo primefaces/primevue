569ae35f98894d4f394a7a56716324c8bbb915a8
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-07-03 14:57:30 +0300

    Stackblitz version updates

--- apps/showcase/components/doc/codeeditor/templates.js
+++ apps/showcase/components/doc/codeeditor/templates.js
@@ -2,7 +2,7 @@ import pkg from '../../../package.json';
 import { services } from './services';
 
 const PrimeVue = {
-    version: '^4.0.0-rc.2',
+    version: '^4.0.0',
     description:
         'PrimeVue is an open source UI library for Vue featuring a rich set of 80+ components, a theme designer, various theme alternatives such as Material, Bootstrap, Tailwind, premium templates and professional support. In addition, it integrates with PrimeBlock, which has 400+ ready to use UI blocks to build spectacular applications in no time.'
 };
@@ -13,8 +13,8 @@ const core_dependencies = {
     vue: '^3.2.45',
     '@vitejs/plugin-vue': '^4.0.0',
     vite: '^4.0.0',
-    '@primevue/themes': PrimeVue.version || 'latest',
-    primevue: PrimeVue.version || 'latest',
+    '@primevue/themes': pkg.version || PrimeVue.version || 'latest',
+    primevue: pkg.version || PrimeVue.version || 'latest',
     primeicons: app_dependencies['primeicons'] || 'latest',
     tailwindcss: app_dependencies['tailwindcss'] || 'latest',
     autoprefixer: app_dependencies['autoprefixer'] || 'latest',
