cbc6186936520fd695e141412553dca9af42115a
Cagatay Civici <cagatay.civici@gmail.com>
2024-07-24 13:26:47 +0300

    Fixed link

--- apps/showcase/pages/templates/freya/index.vue
+++ apps/showcase/pages/templates/freya/index.vue
@@ -193,7 +193,7 @@ export default {
                                     title: 'Support',
                                     description: `PrimeTek offers assistance with account management and licensing issues, with the expectation that users have the necessary technical knowledge to use our products, as we do not offer technical support or consulting. Users
                                         can seek assistance in our community via our public <a href="https://discord.com/invite/gzKFYnpmCY">Discord</a> and
-                                        <a href="https://github.com/orgs/primefaces/discussions/categories/figma-ui-kit" class="doc-link">Forum</a>.`,
+                                        <a href="https://github.com/orgs/primefaces/discussions/categories/primevue-templates" class="doc-link">Forum</a>.`,
                                     src: 'https://primefaces.org/cdn/primevue/images/templates/apollo/apollo-features2-lifetime.png'
                                 },
                                 {
