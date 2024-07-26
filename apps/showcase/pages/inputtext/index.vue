30055d534700417de687292e79b3c27f05f477ba
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-07-26 14:20:45 +0300

    Fluid demo added

--- apps/showcase/pages/inputtext/index.vue
+++ apps/showcase/pages/inputtext/index.vue
@@ -8,6 +8,7 @@ import BasicDoc from '@/doc/inputtext/BasicDoc.vue';
 import DisabledDoc from '@/doc/inputtext/DisabledDoc.vue';
 import FilledDoc from '@/doc/inputtext/FilledDoc.vue';
 import FloatLabelDoc from '@/doc/inputtext/FloatLabelDoc.vue';
+import FluidDoc from '@/doc/inputtext/FluidDoc.vue';
 import HelpTextDoc from '@/doc/inputtext/HelpTextDoc.vue';
 import ImportDoc from '@/doc/inputtext/ImportDoc.vue';
 import InvalidDoc from '@/doc/inputtext/InvalidDoc.vue';
@@ -48,6 +49,11 @@ export default {
                     id: 'filled',
                     label: 'Filled',
                     component: FilledDoc
+                },
+                {
+                    id: 'fluid',
+                    label: 'Fluid',
+                    component: FluidDoc
                 },
                 {
                     id: 'invalid',
