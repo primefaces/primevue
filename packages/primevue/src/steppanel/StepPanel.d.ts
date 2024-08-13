e04ff53326a4aff9ed6dc883a31e0b1e23d65e5e
Damien Churchill <damien@chapeltor.uk>
2024-08-13 09:01:48 +0100

    fix(Stepper): include panel value in activateCallback signature
    This removes the need to add a wrapper function that changes the type
    of activateCallback to any before calling it.

--- packages/primevue/src/steppanel/StepPanel.d.ts
+++ packages/primevue/src/steppanel/StepPanel.d.ts
@@ -117,7 +117,7 @@ export interface StepPanelSlots {
         /**
          * Click function.
          */
-        activateCallback: () => void;
+        activateCallback: (value?: string | number) => void;
     }): VNode[];
 }
 
