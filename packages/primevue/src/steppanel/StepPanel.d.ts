08cc64a95d14152824671120e89156b83587ce83
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-08-20 22:56:21 +0300

    Refactor

--- packages/primevue/src/steppanel/StepPanel.d.ts
+++ packages/primevue/src/steppanel/StepPanel.d.ts
@@ -117,7 +117,7 @@ export interface StepPanelSlots {
         /**
          * Click function.
          */
-        activateCallback: (value?: string | number) => void;
+        activateCallback: (value: string | number) => void;
     }): VNode[];
 }
 
