6ebfc4c16643f790f06c4b22d9f87af7ea2af27b
LeaderbotX400 <eric@singerfamily.ca>
2024-08-30 14:52:25 -0600

    refactor: Update UseToast to TypeScript

--- packages/primevue/src/usetoast/UseToast.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ToastServiceMethods } from 'primevue/toastservice';
-
-export declare function useToast(): ToastServiceMethods;
--- packages/primevue/src/usetoast/UseToast.ts
+++ packages/primevue/src/usetoast/UseToast.ts
@@ -1,9 +1,10 @@
+import { ToastServiceMethods } from 'primevue/primevue';
 import { inject } from 'vue';
 
 export const PrimeVueToastSymbol = Symbol();
 
 export function useToast() {
-    const PrimeVueToast = inject(PrimeVueToastSymbol);
+    const PrimeVueToast = inject<ToastServiceMethods>(PrimeVueToastSymbol);
 
     if (!PrimeVueToast) {
         throw new Error('No PrimeVue Toast provided!');
