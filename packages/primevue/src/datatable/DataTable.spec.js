a3a45347300fef72101bab9a293e2199f88de86b
KumJungMin <rmawjdals@naver.com>
2024-09-04 18:15:37 +0900

    test: update test target

--- packages/primevue/src/datatable/DataTable.spec.js
+++ packages/primevue/src/datatable/DataTable.spec.js
@@ -472,13 +472,13 @@ describe('DataTable.vue', () => {
         await wrapper.setProps({ selection: null, selectionMode: 'multiple' });
 
         await wrapper.vm.onRowClick({
-            originalEvent: { shiftKey: true, target: wrapper.findAll('tr.p-datatable-selectable-row')[0].element },
+            originalEvent: { shiftKey: true, currentTarget: wrapper.findAll('tr.p-datatable-selectable-row')[0].element },
             data: smallData[0],
             index: 0
         });
 
         await wrapper.vm.onRowClick({
-            originalEvent: { shiftKey: true, target: wrapper.findAll('tr.p-datatable-selectable-row')[1].element },
+            originalEvent: { shiftKey: true, currentTarget: wrapper.findAll('tr.p-datatable-selectable-row')[1].element },
             data: smallData[1],
             index: 1
         });
