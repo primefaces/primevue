e72583d658e56ef8c585f515b005f2318f99d50d
mergehez <argesozdogan@gmail.com>
2024-09-06 23:57:23 +0200

    fixes #6353 - InputOtp: IntegerOnly mode accepts "space"

--- packages/primevue/src/inputotp/InputOtp.vue
+++ packages/primevue/src/inputotp/InputOtp.vue
@@ -166,7 +166,7 @@ export default {
                     break;
 
                 default:
-                    if ((this.integerOnly && !(Number(event.key) >= 0 && Number(event.key) <= 9)) || (this.tokens.join('').length >= this.length && event.code !== 'Delete')) {
+                    if ((this.integerOnly && !(event.code !== 'Space' && Number(event.key) >= 0 && Number(event.key) <= 9)) || (this.tokens.join('').length >= this.length && event.code !== 'Delete')) {
                         event.preventDefault();
                     }
 
