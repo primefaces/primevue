279145a149b58fef72f735fa05a5adfd7460bb7c
mergehez <argesozdogan@gmail.com>
2024-09-06 22:13:02 +0200

    [Doc] created new component DocSectionCardWithValue Also used it in InputOtp documentation

---
+++ apps/showcase/components/doc/DocSectionCardWithValue.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="card flex flex-col items-center relative">
+        <slot></slot>
+
+        <div v-if="showValue" class="mt-3" :class="valueClass">
+            <b>Value:</b> <span>{{ value }}</span>
+        </div>
+
+        <div class="absolute top-2 right-2 flex items-center gap-1 opacity-20 hover:opacity-100 transition-opacity duration-300">
+            <Button
+                v-tooltip.bottom="{ value: showValue ? 'Hide Value' : 'Show Value', class: 'doc-section-code-tooltip' }"
+                @click="showValue = !showValue"
+                class="size-8"
+                severity="secondary"
+            >
+                <i v-if="showValue" class="pi pi-eye-slash text-lg"></i>
+                <i v-else class="pi text-lg pi-eye"></i>
+            </Button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    props: {
+        value: {
+            type: null,
+            default: null
+        },
+        valueClass: {
+            type: String,
+            default: 'mt-3'
+        }
+    },
+    data() {
+        return {
+            showValue: false
+        };
+    }
+};
+</script>
--- apps/showcase/doc/inputotp/BasicDoc.vue
+++ apps/showcase/doc/inputotp/BasicDoc.vue
@@ -2,17 +2,19 @@
     <DocSectionText v-bind="$attrs">
         <p>Two-way value binding is defined using <i>v-model</i>. The number of characters is defined with the <i>length</i> property, which is set to 4 by default.</p>
     </DocSectionText>
-    <div class="card flex justify-center">
+    <DocSectionCardWithValue :value="value" value-class="w-28">
         <InputOtp v-model="value" />
-    </div>
+    </DocSectionCardWithValue>
     <DocSectionCode :code="code" />
 </template>
 
 <script>
+
 export default {
     data() {
         return {
             value: null,
+            showValue: false,
             code: {
                 basic: `
 <InputOtp v-model="value" />
