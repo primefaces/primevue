acfba694f97519b4dcae20252482e28896855c83
Cagatay Civici <cagatay.civici@gmail.com>
2024-09-09 21:27:03 +0300

    Add video tutorial

---
+++ apps/showcase/doc/vite/VideoDoc.vue
@@ -0,0 +1,8 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Watch the short video tutorial from Çağatay Çivici to setup PrimeVue in styled mode with <a alt="Create Vue App" href="https://github.com/vuejs/create-vue">Create-Vue</a>.</p>
+    </DocSectionText>
+    <div class="video-container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/xW8EFqalm4I" frameborder="0" allowfullscreen></iframe>
+    </div>
+</template>
