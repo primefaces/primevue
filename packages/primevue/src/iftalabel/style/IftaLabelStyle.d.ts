53985c1855e8898bf8bc2905c6d70e2b8bed2310
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-17 13:02:13 +0300

    New IftaLabel component first implementation

---
+++ packages/primevue/src/iftalabel/style/IftaLabelStyle.d.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * FloatLabel appears on top of the input field when focused.
+ *
+ * [Live Demo](https://www.primevue.org/floatlabel/)
+ *
+ * @module iftalabelstyle
+ *
+ */
+import type { BaseStyle } from '@primevue/core/base/style';
+
+export enum IftaLabelClasses {
+    /**
+     * Class name of the root element
+     */
+    root = 'p-iftalabel'
+}
+
+export interface IftaLabelStyle extends BaseStyle {}
---
+++ packages/primevue/src/iftalabel/style/IftaLabelStyle.js
@@ -0,0 +1,14 @@
+import BaseStyle from '@primevue/core/base/style';
+
+const theme = ({ dt }) => `
+`;
+
+const classes = {
+    root: 'p-iftalabel'
+};
+
+export default BaseStyle.extend({
+    name: 'iftalabel',
+    theme,
+    classes
+});
