5b358df01612a10fb6f826b7623ab9ce06fd6a64
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-17 15:20:05 +0300

    PTViewer added to Menu components II

---
+++ apps/showcase/doc/megamenu/pt/PTViewer.vue
@@ -0,0 +1,83 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="ptViewerDoc">
+        <MegaMenu :model="items" />
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    label: 'Furniture',
+                    icon: 'pi pi-box',
+                    items: [
+                        [
+                            {
+                                label: 'Living Room',
+                                items: [{ label: 'Accessories' }, { label: 'Armchair' }, { label: 'Coffee Table' }, { label: 'Couch' }, { label: 'TV Stand' }]
+                            }
+                        ],
+                        [
+                            {
+                                label: 'Kitchen',
+                                items: [{ label: 'Bar stool' }, { label: 'Chair' }, { label: 'Table' }]
+                            },
+                            {
+                                label: 'Bathroom',
+                                items: [{ label: 'Accessories' }]
+                            }
+                        ]
+                    ]
+                },
+                {
+                    label: 'Electronics',
+                    icon: 'pi pi-mobile',
+                    items: [
+                        [
+                            {
+                                label: 'Computer',
+                                items: [{ label: 'Monitor' }, { label: 'Mouse' }, { label: 'Notebook' }, { label: 'Keyboard' }, { label: 'Printer' }, { label: 'Storage' }]
+                            }
+                        ],
+                        [
+                            {
+                                label: 'Home Theater',
+                                items: [{ label: 'Projector' }, { label: 'Speakers' }, { label: 'TVs' }]
+                            }
+                        ]
+                    ]
+                },
+                {
+                    label: 'Sports',
+                    icon: 'pi pi-clock',
+                    items: [
+                        [
+                            {
+                                label: 'Football',
+                                items: [{ label: 'Kits' }, { label: 'Shoes' }, { label: 'Shorts' }, { label: 'Training' }]
+                            }
+                        ],
+                        [
+                            {
+                                label: 'Running',
+                                items: [{ label: 'Accessories' }, { label: 'Shoes' }, { label: 'T-Shirts' }, { label: 'Shorts' }]
+                            }
+                        ]
+                    ]
+                }
+            ],
+            ptViewerDoc: [
+                {
+                    data: getPTOptions('MegaMenu'),
+                    key: 'MegaMenu'
+                }
+            ]
+        };
+    }
+};
+</script>
---
+++ apps/showcase/doc/menubar/pt/PTViewer.vue
@@ -0,0 +1,69 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="ptViewerDoc">
+        <Menubar :model="items" />
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    label: 'Home',
+                    icon: 'pi pi-home'
+                },
+                {
+                    label: 'Features',
+                    icon: 'pi pi-star'
+                },
+                {
+                    label: 'Projects',
+                    icon: 'pi pi-search',
+                    items: [
+                        {
+                            label: 'Components',
+                            icon: 'pi pi-bolt'
+                        },
+                        {
+                            label: 'Blocks',
+                            icon: 'pi pi-server'
+                        },
+                        {
+                            label: 'UI Kit',
+                            icon: 'pi pi-pencil'
+                        },
+                        {
+                            label: 'Templates',
+                            icon: 'pi pi-palette',
+                            items: [
+                                {
+                                    label: 'Apollo',
+                                    icon: 'pi pi-palette'
+                                },
+                                {
+                                    label: 'Ultima',
+                                    icon: 'pi pi-palette'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    label: 'Contact',
+                    icon: 'pi pi-envelope'
+                }
+            ],
+            ptViewerDoc: [
+                {
+                    data: getPTOptions('Menubar'),
+                    key: 'Menubar'
+                }
+            ]
+        };
+    }
+};
+</script>
