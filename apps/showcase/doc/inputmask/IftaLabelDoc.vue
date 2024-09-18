a5bfe0526b9e7d3f1100aa8593c1eb4819254f82
Cagatay Civici <cagatay.civici@gmail.com>
2024-09-18 13:49:16 +0300

    New label demos for inputmask

---
+++ apps/showcase/doc/inputmask/IftaLabelDoc.vue
@@ -0,0 +1,66 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>IftaLabel is used to create infield top aligned labels. Visit <PrimeVueNuxtLink to="/iftalabel">IftaLabel</PrimeVueNuxtLink> documentation for more information.</p>
+    </DocSectionText>
+    <div class="card flex justify-center">
+        <IftaLabel>
+            <InputMask id="ssn" v-model="value" mask="999-99-9999" autocomplete="off" />
+            <label for="ssn">SSN</label>
+        </IftaLabel>
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: null,
+            code: {
+                basic: `
+<IftaLabel>
+    <InputMask id="ssn" v-model="value" mask="999-99-9999" />
+    <label for="ssn">SSN</label>
+</IftaLabel>
+`,
+                options: `
+<template>
+    <div class="card flex justify-center">
+        <IftaLabel>
+            <InputMask id="ssn" v-model="value" mask="999-99-9999" />
+            <label for="ssn">SSN</label>
+        </IftaLabel>
+    </div>
+</template>
+
+<script setup>
+export default {
+    data() {
+        return {
+            value: null
+        }
+    }
+}
+<\/script>
+`,
+                composition: `
+<template>
+    <div class="card flex justify-center">
+        <IftaLabel>
+            <InputMask id="ssn" v-model="value" mask="999-99-9999" />
+            <label for="ssn">SSN</label>
+        </IftaLabel>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const value = ref(null);
+<\/script>
+`
+            }
+        };
+    }
+};
+</script>
