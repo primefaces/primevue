ca5b1b46b99e377d641b555e5a009b186e9e5c10
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-18 12:58:59 +0300

    PTViewer added to Button components

---
+++ apps/showcase/doc/button/pt/PTViewer.vue
@@ -0,0 +1,23 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="docs">
+        <Button label="Profile" icon="pi pi-user" severity="secondary" badge="2" badgeSeverity="contrast" />
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            docs: [
+                {
+                    data: getPTOptions('Button'),
+                    key: 'Button'
+                }
+            ]
+        };
+    }
+};
+</script>
