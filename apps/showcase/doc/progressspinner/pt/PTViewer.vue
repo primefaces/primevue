79928bbea3875191f9dca041efa7920c76aa6435
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-18 16:06:57 +0300

    PTViewer added to Misc components II

---
+++ apps/showcase/doc/progressspinner/pt/PTViewer.vue
@@ -0,0 +1,23 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="ptViewerDoc">
+        <ProgressSpinner />
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            ptViewerDoc: [
+                {
+                    data: getPTOptions('ProgressSpinner'),
+                    key: 'ProgressSpinner'
+                }
+            ]
+        };
+    }
+};
+</script>
---
+++ apps/showcase/doc/skeleton/pt/PTViewer.vue
@@ -0,0 +1,29 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="ptViewerDoc">
+        <div class="w-full">
+            <Skeleton class="mb-2"></Skeleton>
+            <Skeleton width="10rem" class="mb-2"></Skeleton>
+            <Skeleton width="5rem" class="mb-2"></Skeleton>
+            <Skeleton height="2rem" class="mb-2"></Skeleton>
+            <Skeleton width="10rem" height="4rem"></Skeleton>
+        </div>
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            ptViewerDoc: [
+                {
+                    data: getPTOptions('Skeleton'),
+                    key: 'Skeleton'
+                }
+            ]
+        };
+    }
+};
+</script>
