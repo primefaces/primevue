7fff0e60d53fd048b7e7ae24a310ac9ff84041f5
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-18 16:05:52 +0300

    PTViewer added to Misc components I

---
+++ apps/showcase/doc/avatar/pt/PTViewer.vue
@@ -0,0 +1,33 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="ptViewerDoc">
+        <div class="flex flex-wrap gap-8">
+            <AvatarGroup>
+                <Avatar label="P" size="xlarge" shape="circle" />
+                <Avatar icon="pi pi-user" size="xlarge" shape="circle" />
+                <Avatar image="https://www.gravatar.com/avatar/05dfd4b41340d09cae045235eb0893c3?d=mp" class="flex items-center justify-center" size="xlarge" shape="circle" />
+            </AvatarGroup>
+        </div>
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            ptViewerDoc: [
+                {
+                    data: getPTOptions('Avatar'),
+                    key: 'Avatar'
+                },
+                {
+                    data: getPTOptions('AvatarGroup'),
+                    key: 'AvatarGroup'
+                }
+            ]
+        };
+    }
+};
+</script>
---
+++ apps/showcase/doc/chip/pt/PTViewer.vue
@@ -0,0 +1,26 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="ptViewerDoc">
+        <div class="flex flex-wrap gap-8">
+            <Chip label="Microsoft" icon="pi pi-microsoft" removable />
+            <Chip label="Xuxue Feng" image="https://primefaces.org/cdn/primevue/images/avatar/xuxuefeng.png" removable />
+        </div>
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            ptViewerDoc: [
+                {
+                    data: getPTOptions('Chip'),
+                    key: 'Chip'
+                }
+            ]
+        };
+    }
+};
+</script>
