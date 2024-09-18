d34ad313a352bf210b0a2b13e043bff0a9a0a6b4
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-18 12:53:36 +0300

    PTViewer added to Message components

---
+++ apps/showcase/doc/message/pt/PTViewer.vue
@@ -0,0 +1,23 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="docs">
+        <Message closable severity="info" icon="pi pi-send">Info Message</Message>
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            docs: [
+                {
+                    data: getPTOptions('Message'),
+                    key: 'Message'
+                }
+            ]
+        };
+    }
+};
+</script>
