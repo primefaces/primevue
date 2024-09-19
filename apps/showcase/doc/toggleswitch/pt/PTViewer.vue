db266ec00b085b9f7601ff94ac6d40b97db3d8cf
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-19 10:02:49 +0300

    PTViewer added to Form components IV

---
+++ apps/showcase/doc/toggleswitch/pt/PTViewer.vue
@@ -0,0 +1,24 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="docs">
+        <ToggleSwitch v-model="checked" />
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            checked: false,
+            docs: [
+                {
+                    data: getPTOptions('ToggleSwitch'),
+                    key: 'ToggleSwitch'
+                }
+            ]
+        };
+    }
+};
+</script>
