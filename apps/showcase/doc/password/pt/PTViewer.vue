f75f3fdf8376fbefb23272e5e22d5ff884bbe35f
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-19 10:01:54 +0300

    PTViewer added to Form components III

---
+++ apps/showcase/doc/password/pt/PTViewer.vue
@@ -0,0 +1,24 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="docs">
+        <Password v-model="value" />
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            value: null,
+            docs: [
+                {
+                    data: getPTOptions('Password'),
+                    key: 'Password'
+                }
+            ]
+        };
+    }
+};
+</script>
