ed054ffb1d98cad0cf2f1db2539999f5e4790072
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-19 09:55:07 +0300

    PTViewer added to Form components I

---
+++ apps/showcase/doc/colorpicker/pt/PTViewer.vue
@@ -0,0 +1,24 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="docs">
+        <ColorPicker v-model="color" inline />
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            color: null,
+            docs: [
+                {
+                    data: getPTOptions('ColorPicker'),
+                    key: 'ColorPicker'
+                }
+            ]
+        };
+    }
+};
+</script>
---
+++ apps/showcase/doc/floatlabel/pt/PTViewer.vue
@@ -0,0 +1,27 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocPTViewer :docs="docs">
+        <FloatLabel>
+            <InputText id="username" v-model="value" autocomplete="off" />
+            <label for="username">Username</label>
+        </FloatLabel>
+    </DocPTViewer>
+</template>
+
+<script>
+import { getPTOptions } from '@/components/doc/helpers';
+
+export default {
+    data() {
+        return {
+            value: null,
+            docs: [
+                {
+                    data: getPTOptions('FloatLabel'),
+                    key: 'FloatLabel'
+                }
+            ]
+        };
+    }
+};
+</script>
