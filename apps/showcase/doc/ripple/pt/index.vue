ad63b5994ef4e935ab5fbca4f799a39e4e80a0f6
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-09-20 14:20:26 +0300

    Ripple remove wireframe

--- apps/showcase/doc/ripple/pt/index.vue
+++ apps/showcase/doc/ripple/pt/index.vue
@@ -11,17 +11,11 @@
 <script>
 import DocApiTable from '@/components/doc/DocApiTable.vue';
 import { getPTOptions } from '@/components/doc/helpers';
-import PTImage from './PTImage.vue';
 
 export default {
     data() {
         return {
             docs: [
-                {
-                    id: 'pt.image',
-                    label: 'Wireframe',
-                    component: PTImage
-                },
                 {
                     id: 'pt.doc.ripple',
                     label: 'Ripple PT Options',
