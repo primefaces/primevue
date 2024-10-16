4e7f8edd060a93317fb20c015b7fbf762d954fa5
Cagatay Civici <cagatay.civici@gmail.com>
2024-10-16 09:57:31 +0300

    Remove unnecessary css

--- packages/primevue/src/autocomplete/style/AutoCompleteStyle.js
+++ packages/primevue/src/autocomplete/style/AutoCompleteStyle.js
@@ -74,7 +74,6 @@ const theme = ({ dt }) => `
 
 .p-autocomplete-overlay {
     position: absolute;
-    overflow: auto;
     top: 0;
     left: 0;
     background: ${dt('autocomplete.overlay.background')};
@@ -90,7 +89,6 @@ const theme = ({ dt }) => `
 
 .p-autocomplete-list {
     margin: 0;
-    padding: 0;
     list-style-type: none;
     display: flex;
     flex-direction: column;
