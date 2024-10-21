138d4cd302c36c4328e4e4818cb6f7d9789aff7b
Mert Sincan <sincan.mert@gmail.com>
2024-10-21 15:02:08 +0100

    Update form package

--- packages/form/src/form/BaseForm.vue
+++ packages/form/src/form/BaseForm.vue
@@ -1,6 +1,6 @@
 <script>
 import BaseComponent from '@primevue/core/basecomponent';
-import FormStyle from '@primevue/form/form/style';
+import FormStyle from '@primevue/form/style';
 
 export default {
     name: 'BaseForm',
--- packages/form/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as Form } from '@primevue/form/form';
-export * from '@primevue/form/resolvers';
-export * from '@primevue/form/useform';
