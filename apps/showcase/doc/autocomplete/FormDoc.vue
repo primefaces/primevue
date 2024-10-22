b9c477ca5ff467ff5039af454add1a7cd0c9b7e5
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-10-22 14:16:05 +0300

    AutoComplete form demo

--- apps/showcase/doc/autocomplete/FormDoc.vue
+++ apps/showcase/doc/autocomplete/FormDoc.vue
@@ -1,15 +1,17 @@
 <template>
-    <DocSectionText v-bind="$attrs"> </DocSectionText>
+    <DocSectionText v-bind="$attrs">
+        <p>AutoComplete can be used with the <NuxtLink to="/forms">PrimeVue Forms</NuxtLink> library.</p>
+    </DocSectionText>
     <div class="card flex justify-center">
         <Form v-slot="$form" :resolver="resolver" :initialValues="initialValues" @submit="onFormSubmit" class="flex justify-center flex-col gap-4">
             <div class="flex flex-col gap-2">
                 <AutoComplete name="country" optionLabel="name" :suggestions="filteredCountries" @complete="search" />
                 <Message v-if="$form.country?.invalid" severity="error">{{ $form.country.errors[0]?.message }}</Message>
             </div>
-            <Button type="submit" severity="secondary" class="self-center p-2">Submit</Button>
+            <Button type="submit" severity="secondary" label="Submit" />
         </Form>
     </div>
-    <DocSectionCode :code="code" />
+    <DocSectionCode :code="code" :dependencies="{ zod: '3.23.8' }" />
 </template>
 
 <script>
@@ -25,12 +27,13 @@ export default {
             },
             countries: null,
             filteredCountries: null,
-            resolver: null,
-            schema: z.object({
-                country: z.object({
-                    name: z.string().min(1, 'Country cannot be empty.')
+            resolver: zodResolver(
+                z.object({
+                    country: z.object({
+                        name: z.string().min(1, 'Country cannot be empty.')
+                    })
                 })
-            }),
+            ),
             code: {
                 basic: `
 <Form v-slot="$form" :resolver="resolver" :initialValues="initialValues" @submit="onFormSubmit" class="flex justify-center flex-col gap-4">
@@ -38,21 +41,64 @@ export default {
         <AutoComplete name="country" optionLabel="name" :suggestions="filteredCountries" @complete="search" />
         <Message v-if="$form.country?.invalid" severity="error">{{ $form.country.errors[0]?.message }}</Message>
     </div>
-    <Button type="submit" severity="secondary" class="self-center p-2">Submit</Button>
+    <Button type="submit" severity="secondary" label="Submit" />
 </Form>
 `,
                 options: `
 <template>
     <div class="card flex justify-center">
-        <InputText type="text" v-model="value" />
+        <Form v-slot="$form" :resolver="resolver" :initialValues="initialValues" @submit="onFormSubmit" class="flex justify-center flex-col gap-4">
+            <div class="flex flex-col gap-2">
+                <AutoComplete name="country" optionLabel="name" :suggestions="filteredCountries" @complete="search" />
+                <Message v-if="$form.country?.invalid" severity="error">{{ $form.country.errors[0]?.message }}</Message>
+            </div>
+            <Button type="submit" severity="secondary" label="Submit" />
+        </Form>
+        <Toast />
     </div>
 </template>
 
 <script>
+import { CountryService } from '@/service/CountryService';
+import { zodResolver } from '@primevue/form/resolvers';
+import { z } from 'zod';
+
 export default {
     data() {
         return {
-            value: null
+            initialValues: {
+                country: { name: '' }
+            },
+            countries: null,
+            filteredCountries: null,
+            resolver: zodResolver(
+                z.object({
+                    country: z.object({
+                        name: z.string().min(1, 'Country cannot be empty.')
+                    })
+                })
+            )
+        }
+    },
+    mounted() {
+        CountryService.getCountries().then((data) => (this.countries = data));
+    },
+    methods: {
+        search(event) {
+            setTimeout(() => {
+                if (!event.query.trim().length) {
+                    this.filteredCountries = [...this.countries];
+                } else {
+                    this.filteredCountries = this.countries.filter((country) => {
+                        return country.name.toLowerCase().startsWith(event.query.toLowerCase());
+                    });
+                }
+            }, 250);
+        },
+        onFormSubmit({ valid }) {
+            if (valid) {
+                this.$toast.add({ severity: 'success', summary: 'Form is submitted.', life: 3000 });
+            }
         }
     }
 }
@@ -62,14 +108,61 @@ export default {
                 composition: `
 <template>
     <div class="card flex justify-center">
-        <InputText type="text" v-model="value" />
+        <Form v-slot="$form" :resolver="resolver" :initialValues="initialValues" @submit="onFormSubmit" class="flex justify-center flex-col gap-4">
+            <div class="flex flex-col gap-2">
+                <AutoComplete name="country" optionLabel="name" :suggestions="filteredCountries" @complete="search" />
+                <Message v-if="$form.country?.invalid" severity="error">{{ $form.country.errors[0]?.message }}</Message>
+            </div>
+            <Button type="submit" severity="secondary" label="Submit" />
+        </Form>
+        <Toast />
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from "vue";
+import { zodResolver } from '@primevue/form/resolvers';
+import { useToast } from "primevue/usetoast";
+import { z } from 'zod';
+import { CountryService } from "@/service/CountryService";
 
-const value = ref(null);
+onMounted(() => {
+    CountryService.getCountries().then((data) => (countries.value = data));
+});
+
+const initialValues = ref({
+    country: { name: '' }
+});
+const resolver = ref(
+zodResolver(
+    z.object({
+        country: z.object({
+            name: z.string().min(1, 'Country cannot be empty.')
+        })
+    })
+));
+const countries = ref();
+const selectedCountry = ref();
+const filteredCountries = ref();
+const toast = useToast();
+
+const search = (event) => {
+    setTimeout(() => {
+        if (!event.query.trim().length) {
+            filteredCountries.value = [...countries.value];
+        } else {
+            filteredCountries.value = countries.value.filter((country) => {
+                return country.name.toLowerCase().startsWith(event.query.toLowerCase());
+            });
+        }
+    }, 250);
+};
+
+const onFormSubmit = ({ valid }) => {
+    if (valid) {
+        toast.add({ severity: 'success', summary: 'Form is submitted.', life: 3000 });
+    }
+};
 <\/script>
         `
             }
@@ -77,7 +170,6 @@ const value = ref(null);
     },
     mounted() {
         CountryService.getCountries().then((data) => (this.countries = data));
-        this.resolver = zodResolver(this.schema);
     },
     methods: {
         search(event) {
