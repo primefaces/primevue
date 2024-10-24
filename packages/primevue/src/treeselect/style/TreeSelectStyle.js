e439a1795df0dbeb67c9d97fd473a58e930caf03
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-10-24 17:29:50 +0300

    Refactor #6636 - For TreeSelect

--- packages/primevue/src/treeselect/style/TreeSelectStyle.js
+++ packages/primevue/src/treeselect/style/TreeSelectStyle.js
@@ -56,8 +56,8 @@ const theme = ({ dt }) => `
     background: transparent;
     color: ${dt('treeselect.dropdown.color')};
     width: ${dt('treeselect.dropdown.width')};
-    border-top-right-radius: ${dt('border.radius.md')};
-    border-bottom-right-radius: ${dt('border.radius.md')};
+    border-start-end-radius: ${dt('border.radius.md')};
+    border-end-end-radius: ${dt('border.radius.md')};
 }
 
 .p-treeselect-label-container {
@@ -68,7 +68,7 @@ const theme = ({ dt }) => `
 
 .p-treeselect-label {
     display: flex;
-    align-items-center;
+    align-items: center;
     gap: calc(${dt('treeselect.padding.y')} / 2);
     white-space: nowrap;
     cursor: pointer;
@@ -129,8 +129,8 @@ const theme = ({ dt }) => `
 }
 
 .p-treeselect-label .p-chip {
-    padding-top: calc(${dt('treeselect.padding.y')} / 2);
-    padding-bottom: calc(${dt('treeselect.padding.y')} / 2);
+    padding-block-start: calc(${dt('treeselect.padding.y')} / 2);
+    padding-block-end: calc(${dt('treeselect.padding.y')} / 2);
     border-radius: ${dt('treeselect.chip.border.radius')};
 }
 
