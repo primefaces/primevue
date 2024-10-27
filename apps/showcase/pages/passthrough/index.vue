bee6a7f0f1c93b95bb333ba6f6c13bb27bf107e3
Cagatay Civici <cagatay.civici@gmail.com>
2024-10-27 12:46:57 +0300

    Update pass-through docs

--- apps/showcase/pages/passthrough/index.vue
+++ apps/showcase/pages/passthrough/index.vue
@@ -1,13 +1,13 @@
 <template>
     <Head>
         <Title>PrimeVue - Pass Through</Title>
-        <Meta name="description" content="The Pass Through props is an API to access the internal DOM Structure of the components." />
+        <Meta name="description" content="The Pass Through attributes is an API to access the internal DOM Structure of the components." />
     </Head>
     <div class="doc">
         <div class="doc-main">
             <div class="doc-intro">
                 <h1>Pass Through</h1>
-                <p>The Pass Through props is an API to access the internal DOM Structure of the components.</p>
+                <p>The Pass Through attributes is an API to access the internal DOM Structure of the components.</p>
             </div>
             <DocSections :docs="docs" />
         </div>
@@ -20,6 +20,7 @@ import BasicDoc from '@/doc/passthrough/BasicDoc.vue';
 import CustomCSSDoc from '@/doc/passthrough/CustomCSSDoc.vue';
 import DeclarativeDoc from '@/doc/passthrough/DeclarativeDoc.vue';
 import GlobalDoc from '@/doc/passthrough/GlobalDoc.vue';
+import IntroductionDoc from '@/doc/passthrough/IntroductionDoc.vue';
 import LifecycleDoc from '@/doc/passthrough/LifecycleDoc.vue';
 import PCPrefixDoc from '@/doc/passthrough/PCPrefixDoc.vue';
 import UsePassThroughDoc from '@/doc/passthrough/UsePassThroughDoc.vue';
@@ -28,6 +29,11 @@ export default {
     data() {
         return {
             docs: [
+                {
+                    id: 'introduction',
+                    label: 'Introduction',
+                    component: IntroductionDoc
+                },
                 {
                     id: 'basic',
                     label: 'Basic',
