5aeef7137d941a44a689618e768ead938ee00a33
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-10-30 12:11:38 +0300

    Update TemplateDoc.vue

--- apps/showcase/doc/paginator/TemplateDoc.vue
+++ apps/showcase/doc/paginator/TemplateDoc.vue
@@ -33,7 +33,7 @@ export default {
             first: 0,
             code: {
                 basic: `
-<Paginator v-model:first="first" :rows="1" :totalRecords="1" template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink" />
+<Paginator v-model:first="first" :rows="1" :totalRecords="12" template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink" />
 
 <div class="p-4 text-center">
     <img :src="\`https://primefaces.org/cdn/primevue/images/nature/nature\${first + 1}.jpg\`" :alt="first" class="rounded w-full sm:w-[30rem]" />
