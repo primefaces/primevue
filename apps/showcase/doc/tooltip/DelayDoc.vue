725c926b033ee219fdde0402fa3b0897c22a2d99
Evan Sutherland <esutherland@pollyex.com>
2024-10-31 12:41:14 -0500

    fixed typo in delay doc

--- apps/showcase/doc/tooltip/DelayDoc.vue
+++ apps/showcase/doc/tooltip/DelayDoc.vue
@@ -1,6 +1,6 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p>Dlays to the enter and leave events are defined with <i>showDelay</i> and <i>hideDelay</i> options respectively.</p>
+        <p>Delays to the enter and leave events are defined with <i>showDelay</i> and <i>hideDelay</i> options respectively.</p>
     </DocSectionText>
     <div class="card flex flex-wrap justify-center">
         <Button v-tooltip="{ value: 'Confirm to proceed', showDelay: 1000, hideDelay: 300 }" label="Save" />
