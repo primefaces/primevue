f30aed88b37a3d0f3a10e2da24bcf268fa249633
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-11-11 09:45:54 +0300

    Update ptviewer docs

--- apps/showcase/doc/select/pt/PTViewer.vue
+++ apps/showcase/doc/select/pt/PTViewer.vue
@@ -1,6 +1,6 @@
 <template>
     <DocPTViewer :docs="docs">
-        <Select v-model="selectedCity" filter :options="groupedCities" optionLabel="label" optionGroupLabel="label" optionGroupChildren="items" placeholder="Select a City" class="w-full md:w-56">
+        <Select v-model="selectedCity" filter :options="groupedCities" optionLabel="label" optionGroupLabel="label" optionGroupChildren="items" showClear placeholder="Select a City" class="w-full md:w-56">
             <template #optiongroup="slotProps">
                 <div class="flex items-center">
                     <img :alt="slotProps.option.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
