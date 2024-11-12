333bebd513d432251e131a4404f55cc2629deef7
Cagatay Civici <cagatay.civici@gmail.com>
2024-11-12 17:51:58 +0300

    Add to home page

--- apps/showcase/pages/index.vue
+++ apps/showcase/pages/index.vue
@@ -2,6 +2,7 @@
     <div :class="containerClass">
         <AppNews />
         <AppTopBar :showMenuButton="false" />
+        <AppDesigner />
         <HeroSection />
         <FeaturesSection />
         <UsersSection />
