8a9833bbb97b74ae9b54bcab067b820913ece2f3
Cagatay Civici <cagatay.civici@gmail.com>
2024-11-13 15:30:17 +0300

    Text update

--- apps/showcase/components/layout/AppDesigner.vue
+++ apps/showcase/components/layout/AppDesigner.vue
@@ -86,10 +86,10 @@
                     </Accordion>
                 </TabPanel>
                 <TabPanel value="3">
-                    <span class="leading-6">Component tokens are not supported by the Visual Editor at the moment.</span>
+                    <span class="leading-6 text-muted-color">Component tokens are not supported by the Visual Editor at the moment and will be available with a future update. </span>
                 </TabPanel>
                 <TabPanel value="4">
-                    <span class="leading-6">Extend the theming system with your own design tokens e.g. <span class="font-medium">accent.color</span>. Do not use curly braces in the name field.</span>
+                    <span class="leading-6 text-muted-color">Extend the theming system with your own design tokens e.g. <span class="font-medium">accent.color</span>. Do not use curly braces in the name field.</span>
                     <ul class="flex flex-col gap-4 list-none p-0 mx-0 my-4">
                         <li v-for="(token, index) of customTokens" :key="index" class="first:border-t border-b border-surface-200 dark:border-surface-300 py-2">
                             <div class="flex items-center gap-4">
