cf9652e14992e66e0a0c237d8d359920c82fd132
Reddy Uppathi <popvictoria@gmail.com>
2024-11-18 23:09:43 +0530

    update: update setItem key in locla storage

--- apps/showcase/app.vue
+++ apps/showcase/app.vue
@@ -9,19 +9,17 @@ import EventBus from '@/app/AppEventBus';
 
 export default {
     mounted() {
-        const currentTheme = JSON.parse(localStorage.getItem('theme__primevue'));
+        const currentTheme = JSON.parse(localStorage.getItem('theme__dark__primevue'));
 
         if (currentTheme) this.darkModeToggleListener({ dark: currentTheme });
 
         EventBus.on('dark-mode-toggle', this.darkModeToggleListener);
     },
-    // beforeUnmount() {
-    //     EventBus.off('dark-mode-toggle', this.darkModeToggleListener);
-    // },
+    beforeUnmount() {
+        EventBus.off('dark-mode-toggle', this.darkModeToggleListener);
+    },
     methods: {
         darkModeToggleListener(event) {
-            console.log('e', event);
-
             if (!document.startViewTransition) {
                 this.toggleDarkMode(event);
 
@@ -40,7 +38,7 @@ export default {
 
             this.$appState.darkTheme = isDark;
 
-            localStorage.setItem('theme__primevue', isDark);
+            localStorage.setItem('theme__dark__primevue', this.$appState.darkTheme);
 
             EventBus.emit('dark-mode-toggle-complete');
         }
