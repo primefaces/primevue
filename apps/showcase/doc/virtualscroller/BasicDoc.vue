e16a66b14d4bae1d65bb93bf0f5153f5e34dc11b
Gareth Jones <Jones258@Gmail.com>
2024-11-22 08:34:32 +1300

    docs: improve opening sentence for `VirtualScroller`

--- apps/showcase/doc/virtualscroller/BasicDoc.vue
+++ apps/showcase/doc/virtualscroller/BasicDoc.vue
@@ -1,7 +1,7 @@
 <template>
     <DocSectionText v-bind="$attrs">
         <p>
-            VirtualScroller requires <i>items</i> as the data to display, <i>itemSize</i> for the dimensions of an item and <i>item</i> template are required on component. In addition, an initial array is required based on the total number of items
+            VirtualScroller requires <i>items</i> as the data to display, <i>itemSize</i> for the dimensions of an item, an <i>item</i> template, and an initial array is required based on the total number of items
             to display. Size of the viewport is configured using <i>scrollWidth</i>, <i>scrollHeight</i> properties directly or with CSS <i>width</i> and <i>height</i> styles.
         </p>
     </DocSectionText>
