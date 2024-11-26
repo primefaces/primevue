dd19406dd370e60c8c6e723d7704ee6e67e56b48
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-11-26 12:31:07 +0300

    Fixed #6864

--- packages/primevue/src/chip/Chip.vue
+++ packages/primevue/src/chip/Chip.vue
@@ -7,7 +7,7 @@
             <div v-if="label" :class="cx('label')" v-bind="ptm('label')">{{ label }}</div>
         </slot>
         <slot v-if="removable" name="removeicon" :removeCallback="close" :keydownCallback="onKeydown">
-            <component :is="removeIcon ? 'span' : 'TimesCircleIcon'" tabindex="0" :class="[cx('removeIcon'), removeIcon]" @click="close" @keydown="onKeydown" v-bind="ptm('removeIcon')"></component>
+            <component :is="removeIcon ? 'span' : 'TimesCircleIcon'" :class="[cx('removeIcon'), removeIcon]" @click="close" @keydown="onKeydown" v-bind="ptm('removeIcon')"></component>
         </slot>
     </div>
 </template>
