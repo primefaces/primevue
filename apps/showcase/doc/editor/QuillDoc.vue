3a5fd758600700606bef6d35c2f74d336861cc46
Michael Meier <michael.meier@kit.edu>
2025-01-08 13:44:48 +0100

    feat(Editor): implement support for Quill 2.0 (disables support for older versions)

--- apps/showcase/doc/editor/QuillDoc.vue
+++ apps/showcase/doc/editor/QuillDoc.vue
@@ -1,6 +1,6 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p>Editor uses <a href="https://quilljs.com/">Quill</a> editor underneath so it needs to be installed as a dependency.</p>
+        <p>Editor uses <a href="https://quilljs.com/">Quill</a> editor underneath so it needs to be installed as a dependency. Currently PrimeVue requires version 2 or newer.</p>
     </DocSectionText>
     <DocSectionCode :code="code" hideToggleCode importCode hideStackBlitz />
 </template>
