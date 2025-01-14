0252af5a2031613aee7660011bf9c789e1ce0cca
Mert Sincan <sincan.mert@gmail.com>
2025-01-14 11:13:25 +0000

    refactor: #7090 for Tabs

--- packages/primevue/src/tabs/Tabs.vue
+++ packages/primevue/src/tabs/Tabs.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-import { UniqueComponentId } from '@primevue/core/utils';
 import BaseTabs from './BaseTabs.vue';
 
 export default {
@@ -15,20 +14,13 @@ export default {
     emits: ['update:value'],
     data() {
         return {
-            id: this.$attrs.id,
             d_value: this.value
         };
     },
     watch: {
-        '$attrs.id'(newValue) {
-            this.id = newValue || UniqueComponentId();
-        },
         value(newValue) {
             this.d_value = newValue;
         }
-    },
-    mounted() {
-        this.id = this.id || UniqueComponentId();
     },
     methods: {
         updateValue(newValue) {
