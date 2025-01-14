0e68cdf7646957b22245187918d53b79e9698ed6
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-01-14 15:20:37 +0300

    Refactor #7091

--- packages/icons/src/filterfill/FilterFillIcon.vue
+++ packages/icons/src/filterfill/FilterFillIcon.vue
@@ -1,6 +1,6 @@
 <template>
-    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M16.5 1.25H1.5L7 8.75V16.75H11V8.75L16.5 1.25Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" fill="currentColor" />
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" v-bind="pti()">
+        <path d="M16.5 1.25H1.5L7 8.75V16.75H11V8.75L16.5 1.25Z" fill="currentColor" />
     </svg>
 </template>
 
@@ -8,7 +8,7 @@
 import BaseIcon from '@primevue/icons/baseicon';
 
 export default {
-    name: 'FilterIcon',
+    name: 'FilterFillIcon',
     extends: BaseIcon
 };
 </script>
