5bb966f2e0fb6eb8cae4a637b5223fb5ed163ec0
Jakub Michálek <apple6@freelo.cz>
2025-01-18 17:11:23 +0100

    feat(menu): add exposed methods to typedef component

--- packages/primevue/src/menu/Menu.typedef.vue
+++ packages/primevue/src/menu/Menu.typedef.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts" generic="Item extends MenuItem">
 import type { MenuItem } from 'primevue/menuitem';
-import { MenuEmits, MenuProps, MenuSlots } from './Menu';
+import { MenuEmits, MenuMethods, MenuProps, MenuSlots } from './Menu';
 
 defineProps<MenuProps<Item>>();
 defineSlots<MenuSlots<Item>>();
 defineEmits<MenuEmits>();
+defineExpose<MenuMethods>();
 </script>
