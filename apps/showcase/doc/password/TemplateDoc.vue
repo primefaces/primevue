c5631d2574fed180e50b8b382f587a3f3289965b
Gordon Franke <info@nevalon.de>
2025-01-23 12:08:03 +0100

    feat: add icons to password template example
    pimp password template example add green check and red times icons

--- apps/showcase/doc/password/TemplateDoc.vue
+++ apps/showcase/doc/password/TemplateDoc.vue
@@ -82,11 +82,11 @@ export default {
             </template>
             <template #footer>
                 <Divider />
-                <ul class="pl-2 ml-2 my-0 leading-normal">
-                    <li>At least one lowercase</li>
-                    <li>At least one uppercase</li>
-                    <li>At least one numeric</li>
-                    <li>Minimum 8 characters</li>
+                <ul class="my-0 leading-normal">
+                    <li><i :class="lowercase"></i> At least one lowercase</li>
+                    <li><i :class="uppercase"></i> At least one uppercase</li>
+                    <li><i :class="number"></i> At least one numeric</li>
+                    <li><i :class="length"></i> Minimum 8 characters</li>
                 </ul>
             </template>
         </Password>
@@ -94,9 +94,26 @@ export default {
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const value = ref(null);
+
+const pass = 'pi pi-check text-green-500';
+const fail = 'pi pi-times text-red-500';
+
+const lowercase = computed(() => {
+  return /[a-z]/.test(value.value) ? pass : fail;
+});
+const uppercase = computed(() => {
+  return /[A-Z]/.test(value.value) ? pass : fail;
+});
+const number = computed(() => {
+  return /\d/.test(value.value) ? pass : fail;
+});
+const length = computed(() => {
+  return value.value.length > 8 ? pass : fail;
+});
+
 <\/script>
 `
             }
