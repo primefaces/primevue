36e418b726ac8f2c27f17bccecb2a55b41a57eb4
Mert Sincan <sincan.mert@gmail.com>
2025-01-27 09:37:20 +0000

    refactor: #7153 - For InputOtp and InputText

--- packages/primevue/src/inputotp/style/InputOtpStyle.js
+++ packages/primevue/src/inputotp/style/InputOtpStyle.js
@@ -1,27 +1,5 @@
+import { theme } from '@primeuix/styles/inputotp';
 import BaseStyle from '@primevue/core/base/style';
-
-const theme = ({ dt }) => `
-.p-inputotp {
-    display: flex;
-    align-items: center;
-    gap: ${dt('inputotp.gap')};
-}
-
-.p-inputotp-input {
-    text-align: center;
-    width: ${dt('inputotp.input.width')};
-}
-
-.p-inputotp-input.p-inputtext-sm {
-    text-align: center;
-    width: ${dt('inputotp.input.sm.width')};
-}
-
-.p-inputotp-input.p-inputtext-lg {
-    text-align: center;
-    width: ${dt('inputotp.input.lg.width')};
-}
-`;
 
 const classes = {
     root: 'p-inputotp p-component',
