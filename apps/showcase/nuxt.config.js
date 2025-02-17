18f8cfe6d40cb2feaf5e09da8b00b80b4ed9551e
atakan tepe <tepe_atakan@outlook.com>
2025-02-17 12:30:54 +0300

    update nuxt

--- apps/showcase/nuxt.config.js
+++ apps/showcase/nuxt.config.js
@@ -18,84 +18,94 @@ try {
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    typescript: false,
-    modules: ['@primevue/nuxt-module'],
-    components: {
-        path: '~/components',
-        pathPrefix: false
-    },
-    vite: {
-        resolve: {
-            dedupe: ['vue', '@primeuix/themes', '@primeuix/utils'],
-            optimizeDeps: {
-                disabled: true
+  typescript: false,
+  modules: ['@primevue/nuxt-module'],
+
+  components: {
+      path: '~/components',
+      pathPrefix: false
+  },
+
+  vite: {
+      resolve: {
+          dedupe: ['vue', '@primeuix/themes', '@primeuix/utils'],
+          optimizeDeps: {
+              disabled: true
+          },
+          alias
+      }
+  },
+
+  nitro: {
+      alias
+  },
+
+  routeRules: {
+      '/accessibility': { redirect: { to: '/guides/accessibility', statusCode: 301 } },
+      '/installation': { redirect: { to: '/vite', statusCode: 301 } }
+  },
+
+  primevue:
+      PROCESS_ENV.DEV_ENV === 'hot'
+          ? {
+                usePrimeVue: false,
+                autoImport: true,
+                loadStyles: false
+            }
+          : {
+                autoImport: true, // When enabled, the module automatically imports PrimeVue components and directives used throughout the application.
+                importTheme: { from: '@/themes/app-theme.js' }
             },
-            alias
-        }
-    },
-    nitro: {
-        alias
-    },
-    routeRules: {
-        '/accessibility': { redirect: { to: '/guides/accessibility', statusCode: 301 } },
-        '/installation': { redirect: { to: '/vite', statusCode: 301 } }
-    },
-    primevue:
-        PROCESS_ENV.DEV_ENV === 'hot'
-            ? {
-                  usePrimeVue: false,
-                  autoImport: true,
-                  loadStyles: false
+
+  app: {
+      baseURL: baseUrl,
+      head: {
+          title: 'PrimeVue - Vue UI Component Library',
+          meta: [
+              { charset: 'utf-8' },
+              { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+              { name: 'description', content: 'The ultimate collection of design-agnostic, flexible and accessible Vue UI Components.' },
+              { name: 'robots', content: 'index,follow' },
+              { name: 'twitter:card', content: 'summary_large_image' },
+              { name: 'twitter:site', content: '@primevue' },
+              { name: 'twitter:title', content: 'PrimeVue | Vue UI Component Library' },
+              { name: 'twitter:description', content: 'The ultimate collection of design-agnostic, flexible and accessible Vue UI Components.' },
+              { property: 'og:type', content: 'website' },
+              { property: 'og:title', content: 'PrimeVue | Vue UI Component Library' },
+              { property: 'og:url', content: 'https://primevue.org/' },
+              { property: 'og:description', content: 'The ultimate collection of design-agnostic, flexible and accessible Vue UI Components.' },
+              { property: 'og:image', content: 'https://www.primefaces.org/static/social/primevue-preview.jpg' },
+              { property: 'og:ttl', content: '604800' }
+          ],
+          link: [
+              { rel: 'icon', href: baseUrl + 'favicon.ico' },
+              { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
+              { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400|Roboto:500|Roboto:600|Roboto:700', fetchpriority: 'low' }
+          ],
+          script: [
+              {
+                  src: baseUrl + 'scripts/prism.js',
+                  'data-manual': true
               }
-            : {
-                  autoImport: true, // When enabled, the module automatically imports PrimeVue components and directives used throughout the application.
-                  importTheme: { from: '@/themes/app-theme.js' }
-              },
-    app: {
-        baseURL: baseUrl,
-        head: {
-            title: 'PrimeVue - Vue UI Component Library',
-            meta: [
-                { charset: 'utf-8' },
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { name: 'description', content: 'The ultimate collection of design-agnostic, flexible and accessible Vue UI Components.' },
-                { name: 'robots', content: 'index,follow' },
-                { name: 'twitter:card', content: 'summary_large_image' },
-                { name: 'twitter:site', content: '@primevue' },
-                { name: 'twitter:title', content: 'PrimeVue | Vue UI Component Library' },
-                { name: 'twitter:description', content: 'The ultimate collection of design-agnostic, flexible and accessible Vue UI Components.' },
-                { property: 'og:type', content: 'website' },
-                { property: 'og:title', content: 'PrimeVue | Vue UI Component Library' },
-                { property: 'og:url', content: 'https://primevue.org/' },
-                { property: 'og:description', content: 'The ultimate collection of design-agnostic, flexible and accessible Vue UI Components.' },
-                { property: 'og:image', content: 'https://www.primefaces.org/static/social/primevue-preview.jpg' },
-                { property: 'og:ttl', content: '604800' }
-            ],
-            link: [
-                { rel: 'icon', href: baseUrl + 'favicon.ico' },
-                { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
-                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400|Roboto:500|Roboto:600|Roboto:700', fetchpriority: 'low' }
-            ],
-            script: [
-                {
-                    src: baseUrl + 'scripts/prism.js',
-                    'data-manual': true
-                }
-            ]
-        }
-    },
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {}
-        }
-    },
-    runtimeConfig: {
-        public: {
-            contextPath: baseUrl,
-            DEV_ENV: PROCESS_ENV.DEV_ENV,
-            designerApiBase: ''
-        }
-    },
-    css: ['primeicons/primeicons.css', '@/assets/styles/flags.css', '@docsearch/css/dist/style.css', '@/assets/styles/tailwind/main.css', '@/assets/styles/layout/layout.scss']
+          ]
+      }
+  },
+
+  postcss: {
+      plugins: {
+          tailwindcss: {},
+          autoprefixer: {}
+      }
+  },
+
+  runtimeConfig: {
+      public: {
+          contextPath: baseUrl,
+          DEV_ENV: PROCESS_ENV.DEV_ENV,
+          designerApiBase: ''
+      }
+  },
+
+  css: ['primeicons/primeicons.css', '@/assets/styles/flags.css', '@docsearch/css/dist/style.css', '@/assets/styles/tailwind/main.css', '@/assets/styles/layout/layout.scss'],
+  compatibilityDate: '2025-02-17'
 });
