805fb70ea7f2d11ad9219c8502adc202069b1e2d
Cagatay Civici <cagatay.civici@gmail.com>
2025-02-24 09:58:16 +0300

    Add tailwind4 doc

--- apps/showcase/doc/tailwind/PluginDoc.vue
+++ apps/showcase/doc/tailwind/PluginDoc.vue
@@ -5,10 +5,17 @@
             PrimeVue and Tailwind CSS. It is designed to work both in styled and unstyled modes. In styled mode, for instance the semantic colors such as primary and surfaces are provided as Tailwind utilities e.g. <i>bg-primary</i>,
             <i>text-surface-500</i>, <i>text-muted-color</i>.
         </p>
-        <p>Plugin is available on npm.</p>
+        <p>
+            If not completed already, begin with setting up Tailwind in your project. Visit the <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">documentation</a> for the necessary steps. Once the Tailwind is installed
+            properly, install the primeui plugin.
+        </p>
         <DocSectionCode :code="code1" hideToggleCode importCode hideStackBlitz />
-        <p>After installation, configure the plugin at your tailwind configuration file.</p>
+        <h3>Tailwind v4</h3>
+        <p>In the CSS file that contains the tailwindcss import, add the <i>tailwindcss-primeui</i> import as well.</p>
         <DocSectionCode :code="code2" hideToggleCode importCode hideStackBlitz />
+        <h3>Tailwind v3</h3>
+        <p>After installation, configure the plugin at your tailwind configuration file.</p>
+        <DocSectionCode :code="code3" hideToggleCode importCode hideStackBlitz />
     </DocSectionText>
 </template>
 
@@ -25,10 +32,18 @@ npm i tailwindcss-primeui
             },
             code2: {
                 basic: `
+@import "tailwindcss";
+@import "tailwindcss-primeui";
+`
+            },
+            code3: {
+                basic: `
 // tailwind.config.js
-module.exports = {
+import PrimeUI from 'tailwindcss-primeui';
+
+export default {
     // ...
-    plugins: [require('tailwindcss-primeui')]
+    plugins: [PrimeUI]
 };
 `
             }
