cfa3242d7d043cd43862b298b05efd4446e82a52
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-03 23:38:34 +0300

    Fixed outline

--- apps/volt/volt/checkbox/index.vue
+++ apps/volt/volt/checkbox/index.vue
@@ -17,7 +17,8 @@ const theme = ref({
     root: `relative inline-flex select-none w-5 h-5 align-bottom
         p-small:w-4 p-small:h-4
         p-large:w-6 p-large:h-6`,
-    input: `peer cursor-pointer disabled:cursor-default appearance-none absolute start-0 top-0 w-full h-full m-0 p-0 opacity-0 z-10
+    input: `peer cursor-pointer disabled:cursor-default appearance-none 
+        absolute start-0 top-0 w-full h-full m-0 p-0 opacity-0 z-10
         border border-transparent rounded-sm`,
     box: `flex justify-center items-center rounded-sm w-5 h-5
         border border-surface-300 dark:border-surface-700
@@ -26,7 +27,7 @@ const theme = ref({
         peer-enabled:peer-hover:border-surface-400 dark:peer-enabled:peer-hover:border-surface-600
         p-checked:border-primary p-checked:bg-primary p-checked:text-primary-contrast
         peer-enabled:peer-hover:p-checked:bg-primary-emphasis peer-enabled:peer-hover:p-checked:border-primary-emphasis
-        peer-focus-visible:outline peer-focus-visible:outline-1 peer-focus-visible:outline-offset-2 peer-focus-visible:outline-primary
+        peer-focus-visible:outline-1 peer-focus-visible:outline-offset-2 peer-focus-visible:outline-primary peer-focus-visible:outline 
         p-invalid:border-red-400 dark:p-invalid:border-red-300
         p-filled:bg-surface-50 dark:p-filled:bg-surface-800
         p-disabled:bg-surface-200 dark:p-disabled:bg-surface-400 p-disabled:border-surface-300 dark:p-disabled:border-surface-700 p-disabled:text-surface-700 dark:p-disabled:text-surface-400
--- apps/volt/volt/radiobutton/index.vue
+++ apps/volt/volt/radiobutton/index.vue
@@ -25,7 +25,7 @@ const theme = ref({
         peer-enabled:peer-hover:border-surface-400 dark:peer-enabled:peer-hover:border-surface-600
         p-checked:border-primary p-checked:bg-primary
         peer-enabled:peer-hover:p-checked:bg-primary-emphasis peer-enabled:peer-hover:p-checked:border-primary-emphasis
-        peer-focus-visible:outline peer-focus-visible:outline-1 peer-focus-visible:outline-offset-2 peer-focus-visible:outline-primary
+        peer-focus-visible:outline-1 peer-focus-visible:outline-offset-2 peer-focus-visible:outline-primary peer-focus-visible:outline 
         p-filled:bg-surface-50 dark:p-filled:bg-surface-800
         p-invalid:border-red-400 dark:p-invalid:border-red-300
         p-disabled:bg-surface-200 dark:p-disabled:bg-surface-400 p-disabled:border-surface-300 dark:p-disabled:border-surface-700
