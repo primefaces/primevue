b42fb9e2c13fd91c7b16e7e7381d078d380f645d
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-03 18:00:31 +0300

    Add accessibility doc

--- apps/volt/components/doc/DocComponent.vue
+++ apps/volt/components/doc/DocComponent.vue
@@ -34,6 +34,13 @@
                                 class="inline-flex rounded-full px-3 py-1 bg-surface-200 hover:bg-surface-300 transition-all duration-200 gap-2 items-center text-sm text-surface-900 dark:bg-surface-800 dark:hover:bg-surface-700"
                                 ><span class="text-surface-900 dark:text-surface-50 font-medium">PassThrough Doc</span><i class="pi pi-external-link text-xs text-surface-900 dark:text-surface-50"></i
                             ></a>
+                            <a
+                                :href="`https://primevue.org${originPath}/#accessibility`"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                class="inline-flex rounded-full px-3 py-1 bg-surface-200 hover:bg-surface-300 transition-all duration-200 gap-2 items-center text-sm text-surface-900 dark:bg-surface-800 dark:hover:bg-surface-700"
+                                ><span class="text-surface-900 dark:text-surface-50 font-medium">Accessibility</span><i class="pi pi-external-link text-xs text-surface-900 dark:text-surface-50"></i
+                            ></a>
                         </div>
                     </div>
                     <DocSections :docs="componentDocs" />
