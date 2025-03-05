0517c75a0a7fc84654a0b2b4cc430797d28eab00
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-05 14:18:47 +0300

    Add transitions and templating

--- apps/volt/volt/panel/index.vue
+++ apps/volt/volt/panel/index.vue
@@ -6,6 +6,14 @@
             mergeProps: ptViewMerge
         }"
     >
+        <template #togglebutton="{ collapsed, toggleCallback, keydownCallback }">
+            <SecondaryButton variant="text" rounded @click="toggleCallback" @keydown="keydownCallback">
+                <template #icon>
+                    <PlusIcon v-if="collapsed" />
+                    <MinusIcon v-else="collapsed" />
+                </template>
+            </SecondaryButton>
+        </template>
         <template v-for="(_, slotName) in $slots" v-slot:[slotName]="slotProps">
             <slot :name="slotName" v-bind="slotProps ?? {}" />
         </template>
@@ -13,8 +21,11 @@
 </template>
 
 <script setup>
+import MinusIcon from '@primevue/icons/minus';
+import PlusIcon from '@primevue/icons/plus';
 import Panel from 'primevue/panel';
 import { ref } from 'vue';
+import SecondaryButton from '../button/secondary';
 import { ptViewMerge } from '../utils';
 
 const theme = ref({
@@ -24,22 +35,16 @@ const theme = ref({
     header: `flex justify-between items-center p-[1.125rem] p-toggleable:py-[0.375rem] p-toggleable:px-[1.125rem]`,
     title: `leading-none font-semibold`,
     headerActions: `flex items-center gap-1`,
-    pcToggleButton: {
-        root: `w-10 h-10 px-0 gap-0 rounded-full 
-            inline-flex cursor-pointer select-none items-center justify-center overflow-hidden relative
-            bg-transparent enabled:hover:bg-surface-50 enabled:active:bg-surface-100
-            border-transparent enabled:hover:border-transparent enabled:active:border-transparent
-            text-surface-500 enabled:hover:text-surface-500 enabled:active:text-surface-500
-            dark:bg-transparent dark:enabled:hover:bg-surface-800 dark:enabled:active:bg-surface-700
-            dark:border-transparent dark:enabled:hover:border-transparent dark:enabled:active:border-transparent
-            dark:text-surface-400 dark:enabled:hover:text-surface-400 dark:enabled:active:text-surface-400
-            focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2
-            focus-visible:outline-surface-600 dark:focus-visible:outline-surface-300`,
-        label: `p-icon-only:invisible p-icon-only:w-0`
-    },
     contentContainer: ``,
     content: `pt-0 pb-[1.125rem] px-[1.125rem] `,
     footer: `pt-0 pb-[1.125rem] px-[1.125rem] `,
-    transition: ``
+    transition: {
+        enterFromClass: 'max-h-0',
+        enterActiveClass: 'overflow-hidden transition-[max-height] duration-1000 ease-[cubic-bezier(0.42,0,0.58,1)]',
+        enterToClass: 'max-h-[1000px]',
+        leaveFromClass: 'max-h-[1000px]',
+        leaveActiveClass: 'overflow-hidden transition-[max-height] duration-[450ms] ease-[cubic-bezier(0,1,0,1)]',
+        leaveToClass: 'max-h-0'
+    }
 });
 </script>
