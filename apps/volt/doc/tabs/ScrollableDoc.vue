fff258eb335d5d8e429c015b8cbff61e0be4a3e6
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-11 10:01:52 +0300

    Add scrollable tabs demo

--- apps/volt/doc/tabs/ScrollableDoc.vue
+++ apps/volt/doc/tabs/ScrollableDoc.vue
@@ -1,32 +1,17 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p>Tabs is defined using <i>TabList</i>, <i>Tab</i>, <i>TabPanels</i> and <i>TabPanel</i> components. Tab and TabPanel components are associated with their <i>value</i> properties</p>
+        <p>Adding <i>scrollable</i> property displays navigational buttons at each side to scroll between tabs.</p>
     </DocSectionText>
     <div class="card">
-        <Tabs value="0">
+        <Tabs value="0" scrollable>
             <TabList>
-                <Tab value="0">Header I</Tab>
-                <Tab value="1">Header II</Tab>
-                <Tab value="2">Header III</Tab>
+                <Tab v-for="tab in scrollableTabs" :key="tab.title" :value="tab.value">
+                    {{ tab.title }}
+                </Tab>
             </TabList>
             <TabPanels>
-                <TabPanel value="0">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </TabPanel>
-                <TabPanel value="1">
-                    <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-                        ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-                    </p>
-                </TabPanel>
-                <TabPanel value="2">
-                    <p>
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-                        qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-                    </p>
+                <TabPanel v-for="tab in scrollableTabs" :key="tab.content" :value="tab.value">
+                    <p class="m-0!">{{ tab.content }}</p>
                 </TabPanel>
             </TabPanels>
         </Tabs>
@@ -42,33 +27,20 @@ import TabPanel from '@/volt/tabs/tabpanel';
 import TabPanels from '@/volt/tabs/tabpanels';
 import { ref } from 'vue';
 
+const scrollableTabs = ref(Array.from({ length: 50 }, (_, i) => ({ title: `Tab ${i + 1}`, content: `Tab ${i + 1} Content`, value: `${i}` })));
+
 const code = ref(`
 <template>
     <div class="card">
-        <Tabs value="0">
+        <Tabs value="0" scrollable>
             <TabList>
-                <Tab value="0">Header I</Tab>
-                <Tab value="1">Header II</Tab>
-                <Tab value="2">Header III</Tab>
+                <Tab v-for="tab in scrollableTabs" :key="tab.title" :value="tab.value">
+                    {{ tab.title }}
+                </Tab>
             </TabList>
             <TabPanels>
-                <TabPanel value="0">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </TabPanel>
-                <TabPanel value="1">
-                    <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-                        ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-                    </p>
-                </TabPanel>
-                <TabPanel value="2">
-                    <p>
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-                        qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-                    </p>
+                <TabPanel v-for="tab in scrollableTabs" :key="tab.content" :value="tab.value">
+                    <p>{{ tab.content }}</p>
                 </TabPanel>
             </TabPanels>
         </Tabs>
@@ -81,6 +53,9 @@ import TabList from '@/volt/tabs/tablist';
 import Tab from '@/volt/tabs/tab';
 import TabPanels from '@/volt/tabs/tabpanels';
 import TabPanel from '@/volt/tabs/tabpanel';
+import { ref } from 'vue';
+
+const scrollableTabs = ref(Array.from({ length: 50 }, (_, i) => ({ title: \`Tab \${i + 1}\`, content: \`Tab \${i + 1} Content\`, value: \`\${i}\` })));
 <\/script>
 `);
 </script>
--- apps/volt/volt/tabs/tablist.vue
+++ apps/volt/volt/tabs/tablist.vue
@@ -27,7 +27,7 @@ const theme = ref({
     nextButton: navButton + ` end-0`,
     content: `flex-grow
         p-scrollable:overflow-x-auto p-scrollable:overflow-y-hidden p-scrollable:overscroll-y-contain p-scrollable:overscroll-x-auto
-        p-scrollable:[scrollbar-behavior:smooth] p-scrollable:[scrollbar-width:none]`,
+        scroll-smooth [scrollbar-width:none]`,
     tabList: `relative flex bg-surface-0 dark:bg-surface-900 border-b border-surface-200 dark:border-surface-700
         p-scrollable:overflow-hidden`,
     activeBar: `z-10 block absolute -bottom-px h-px bg-primary transition-[left] duration-200 ease-[cubic-bezier(0.35,0,0.25,1)]`
