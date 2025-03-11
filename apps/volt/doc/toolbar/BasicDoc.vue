ac1ad74e8268f0c3a38887a1ad88ea4729608862
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-11 11:36:23 +0300

    Implemented Toolbar for Volt

---
+++ apps/volt/doc/toolbar/BasicDoc.vue
@@ -0,0 +1,58 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Toolbar provides <i>start</i>, <i>center</i> and <i>end</i> properties to place content at these sections.</p>
+    </DocSectionText>
+    <div class="card">
+        <Toolbar>
+            <template #start>
+                <SecondaryButton icon="pi pi-plus" class="mr-2" text />
+                <SecondaryButton icon="pi pi-print" class="mr-2" text />
+                <SecondaryButton icon="pi pi-upload" severity="secondary" text />
+            </template>
+
+            <template #center>
+                <InputText placeholder="Search" />
+            </template>
+
+            <template #end>
+                <Button label="Save" />
+            </template>
+        </Toolbar>
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script setup>
+import Button from '@/volt/button';
+import SecondaryButton from '@/volt/button/secondary';
+import InputText from '@/volt/inputtext';
+import Toolbar from '@/volt/toolbar';
+import { ref } from 'vue';
+
+const code = ref(`
+<template>
+    <Toolbar>
+        <template #start>
+            <SecondaryButton icon="pi pi-plus" class="mr-2" text />
+            <SecondaryButton icon="pi pi-print" class="mr-2" text />
+            <SecondaryButton icon="pi pi-upload" severity="secondary" text />
+        </template>
+
+        <template #center>
+            <InputText placeholder="Search" />
+        </template>
+
+        <template #end> 
+            <Button icon="pi pi-check" />
+        </template>
+    </Toolbar>
+</template>
+
+<script setup>
+import Button from '@/volt/button';
+import SecondaryButton from '@/volt/button/secondary';
+import InputText from '@/volt/inputtext';
+import Toolbar from '@/volt/toolbar';
+<\/script>
+`);
+</script>
---
+++ apps/volt/volt/toolbar/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <Toolbar
+        unstyled
+        :pt="theme"
+        :ptOptions="{
+            mergeProps: ptViewMerge
+        }"
+    >
+        <template v-for="(_, slotName) in $slots" v-slot:[slotName]="slotProps">
+            <slot :name="slotName" v-bind="slotProps ?? {}" />
+        </template>
+    </Toolbar>
+</template>
+
+<script setup>
+import Toolbar from 'primevue/toolbar';
+import { ref } from 'vue';
+import { ptViewMerge } from '../utils';
+
+const theme = ref({
+    root: `flex items-center justify-between flex-wrap p-3 gap-2
+        bg-surface-0 dark:bg-surface-900
+        text-surface-700 dark:text-surface-0
+        border border-surface-200 dark:border-surface-700 rounded-md`,
+    start: `flex items-center`,
+    center: `flex items-center`,
+    end: `flex items-center`
+});
+</script>
