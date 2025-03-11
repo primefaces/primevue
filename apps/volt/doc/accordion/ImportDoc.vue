4ffa127fc069f29407f2a5e5844e9ad18a5374ac
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-11 11:05:57 +0300

    Implemented Accordion for Volt

---
+++ apps/volt/doc/accordion/ImportDoc.vue
@@ -0,0 +1,14 @@
+<template>
+    <DocSectionText v-bind="$attrs" />
+    <DocSectionCode :code="code" lang="script" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const code = ref(`import Accordion from '@/volt/accordion';
+import AccordionPanel from '@/volt/accordion/accordionpanel';
+import AccordionHeader from '@/volt/accordion/accordionheader';
+import AccordionContent from '@/volt/accordion/accordioncontent';
+`);
+</script>
