ea2bad8ac4275251492a237b6ed8726a08edb84c
J-Michalek <71264422+J-Michalek@users.noreply.github.com>
2025-03-12 08:50:48 +0100

    fix(dynamic-dialog): use v-on instead of v-bind for emits
    I think this does not work after some version of vue.

--- packages/primevue/src/dynamicdialog/DynamicDialog.vue
+++ packages/primevue/src/dynamicdialog/DynamicDialog.vue
@@ -2,11 +2,11 @@
     <template v-for="(instance, key) in instanceMap" :key="key">
         <DDialog v-model:visible="instance.visible" :_instance="instance" v-bind="instance.options.props" @hide="onDialogHide(instance)" @after-hide="onDialogAfterHide(instance)">
             <template v-if="instance.options.templates && instance.options.templates.header" #header>
-                <component v-for="(header, index) in getTemplateItems(instance.options.templates.header)" :is="header" :key="index + '_header'" v-bind="instance.options.emits"></component>
+                <component v-for="(header, index) in getTemplateItems(instance.options.templates.header)" :is="header" :key="index + '_header'" v-on="instance.options.emits"></component>
             </template>
-            <component :is="instance.content" v-bind="instance.options.emits"></component>
+            <component :is="instance.content" v-on="instance.options.emits"></component>
             <template v-if="instance.options.templates && instance.options.templates.footer" #footer>
-                <component v-for="(footer, index) in getTemplateItems(instance.options.templates.footer)" :is="footer" :key="index + '_footer'" v-bind="instance.options.emits"></component>
+                <component v-for="(footer, index) in getTemplateItems(instance.options.templates.footer)" :is="footer" :key="index + '_footer'" v-on="instance.options.emits"></component>
             </template>
         </DDialog>
     </template>
