c0826ef36fa9098d6dc41146e729d961afaed471
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-14 12:15:56 +0300

    Implemented Popover for Volt

---
+++ apps/volt/pages/popover/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <DocComponent title="Vue Popover Component" header="Popover" description="Popover is a container component that can overlay other components on page." :componentDocs="docs" />
+</template>
+
+<script setup>
+import BasicDoc from '@/doc/popover/BasicDoc.vue';
+import ImportDoc from '@/doc/popover/ImportDoc.vue';
+import SelectDoc from '@/doc/popover/SelectDoc.vue';
+
+import { ref } from 'vue';
+
+const docs = ref([
+    {
+        id: 'import',
+        label: 'Import',
+        component: ImportDoc
+    },
+    {
+        id: 'basic',
+        label: 'Basic',
+        component: BasicDoc
+    },
+    {
+        id: 'selectdata',
+        label: 'Select',
+        component: SelectDoc
+    }
+]);
+</script>
