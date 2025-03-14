31a78e8a3eb1f921dbb018ae7e3e8cb78ed72467
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-14 22:59:59 +0300

    Implemented Breadcrumb for Volt

---
+++ apps/volt/pages/breadcrumb/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <DocComponent title="Vue Breadcrumb Component" header="Breadcrumb" description="Breadcrumb provides contextual information about page hierarchy." :componentDocs="docs" />
+</template>
+
+<script setup>
+import BasicDoc from '@/doc/breadcrumb/BasicDoc.vue';
+import ImportDoc from '@/doc/breadcrumb/ImportDoc.vue';
+import RouterDoc from '@/doc/breadcrumb/RouterDoc.vue';
+import TemplateDoc from '@/doc/breadcrumb/TemplateDoc';
+import { ref } from 'vue';
+
+const docs = ref([
+    {
+        id: 'import',
+        label: 'Import',
+        component: ImportDoc
+    },
+    {
+        id: 'basic',
+        label: 'Basic',
+        component: BasicDoc
+    },
+    {
+        id: 'template',
+        label: 'Template',
+        component: TemplateDoc
+    },
+    {
+        id: 'router',
+        label: 'Router',
+        component: RouterDoc
+    }
+]);
+</script>
---
+++ apps/volt/volt/breadcrumb/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <Breadcrumb
+        unstyled
+        :pt="theme"
+        :ptOptions="{
+            mergeProps: ptViewMerge
+        }"
+    >
+        <template v-for="(_, slotName) in $slots" v-slot:[slotName]="slotProps">
+            <slot :name="slotName" v-bind="slotProps ?? {}" />
+        </template>
+    </Breadcrumb>
+</template>
+
+<script setup>
+import Breadcrumb from 'primevue/breadcrumb';
+import { ref } from 'vue';
+import { ptViewMerge } from '../utils';
+
+const theme = ref({
+    root: `bg-surface-0 dark:bg-surface-900 p-4 overflow-x-auto`,
+    list: `m-0 p-0 list-none flex items-center flex-nowrap gap-2`,
+    item: ``,
+    itemLink: `no-underline flex items-center gap-2 transition-colors duration-200 rounded-md
+        text-surface-500 dark:text-surface-400 hover:text-surface-700 dark:hover:text-surface-0
+        focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 focus-visible:outline-primary`,
+    itemIcon: ``,
+    itemLabel: ``,
+    separator: `flex items-center text-surface-400 dark:text-surface-500`,
+    separatorIcon: ``
+});
+</script>
