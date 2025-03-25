41348f887e7bd639bcf2cee007ece1e381441c3b
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-25 14:43:03 +0300

    Update TW docs

--- apps/volt/doc/vite/TailwindDoc.vue
+++ apps/volt/doc/vite/TailwindDoc.vue
@@ -8,6 +8,7 @@
             After successfully installing Tailwind, proceed with the installation of the <a href="https://primevue.org/tailwind/#extensions" target="_blank" rel="noopener noreferrer">tailwindcss-primeui</a> plugin. This plugin extends Tailwind with
             new utility classes such as <i>text-primary</i>, <i>bg-surface-500</i>, and adds new variants with the <i>p-</i> prefix including <i>p-selected</i>.
         </p>
+        <p>In addition <a href="https://www.npmjs.com/package/tailwind-merge" target="_blank" rel="noopener noreferrer">tailwind-merge</a> utility is required for efficient handling of external class merging.</p>
         <DocSectionCode :code="code1" />
 
         <p>In the CSS file that contains the tailwindcss import, add the <i>tailwindcss-primeui</i> import as well.</p>
@@ -20,6 +21,7 @@ import { ref } from 'vue';
 
 const code1 = ref(`
 npm i tailwindcss-primeui
+npm i tailwind-merge
 `);
 
 const code2 = ref(`@import "tailwindcss";
