69e2848fba67adf349ab0cce5ca2212aa50c3c82
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-25 21:07:00 +0300

    Landing cosmetics

--- apps/volt/components/landing/LandingFeatures.vue
+++ apps/volt/components/landing/LandingFeatures.vue
@@ -9,7 +9,7 @@
                     </span>
                     <span class="font-semibold text-surface-950 dark:text-surface-0">50+ Components</span>
                 </div>
-                <span class="text-muted-color">The ultimate set of Ul Components to assist you with 80+ impressive Vue Components.</span>
+                <span class="text-muted-color">A comprehesive UI component suite crafted to meet all your requirements.</span>
             </div>
             <div class="rounded-lg p-6 shadow-md border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900">
                 <div class="flex items-center gap-2 mb-4">
@@ -18,25 +18,25 @@
                     </span>
                     <span class="font-semibold text-surface-950 dark:text-surface-0">Unstyled PrimeVue</span>
                 </div>
-                <span class="text-muted-color">The ultimate set of Ul Components to assist you with 80+ impressive Vue Components.</span>
+                <span class="text-muted-color">Powered by the Unstyled PrimeVue components with advanced customization options.</span>
             </div>
             <div class="rounded-lg p-6 shadow-md border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900">
                 <div class="flex items-center gap-2 mb-4">
                     <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md h-8 w-8 inline-flex items-center justify-center">
                         <i class="pi pi-palette" />
                     </span>
-                    <span class="font-semibold text-surface-950 dark:text-surface-0">Tailwind</span>
+                    <span class="font-semibold text-surface-950 dark:text-surface-0">Tailwind v4</span>
                 </div>
-                <span class="text-muted-color">The ultimate set of Ul Components to assist you with 80+ impressive Vue Components.</span>
+                <span class="text-muted-color">Take full control over component styling with the convenience of the Tailwind v4 CSS utilities.</span>
             </div>
             <div class="rounded-lg p-6 shadow-md border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900">
                 <div class="flex items-center gap-2 mb-4">
                     <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md h-8 w-8 inline-flex items-center justify-center">
                         <i class="pi pi-check" />
                     </span>
-                    <span class="font-semibold text-surface-950 dark:text-surface-0">Code Ownership</span>
+                    <span class="font-semibold text-surface-950 dark:text-surface-0">Accessible</span>
                 </div>
-                <span class="text-muted-color">The ultimate set of Ul Components to assist you with 80+ impressive Vue Components.</span>
+                <span class="text-muted-color">Designed with accessibility in mind featuring WCAG AA compliance.</span>
             </div>
         </div>
     </div>
--- apps/volt/components/landing/LandingHero.vue
+++ apps/volt/components/landing/LandingHero.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="flex flex-col max-w-220 p-12 mx-auto">
-        <div class="flex items-center gap-2 mb-8 justify-center">
-            <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md font-medium leading-relaxed px-2">50+ Components</span>
+    <div class="flex flex-col max-w-220 py-12 px-4 mx-auto">
+        <div class="flex items-center gap-1 sm:gap-2 mb-8 justify-center">
+            <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md font-medium leading-relaxed px-2 text-center">50+ Components</span>
             <span class="opacity-50">⋅</span>
-            <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md font-medium leading-relaxed px-2">Tailwind v4</span>
+            <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md font-medium leading-relaxed px-2 text-center">Tailwind v4</span>
             <span class="opacity-50">⋅</span>
-            <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md font-medium leading-relaxed px-2">WCAG Compliant</span>
+            <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md font-medium leading-relaxed px-2 text-center">WCAG Compliant</span>
         </div>
         <div class="text-surface-950 dark:text-surface-0 font-bold text-4xl sm:text-7xl mb-2 text-center">Craft Your Own</div>
         <div class="text-primary font-bold text-4xl sm:text-7xl text-center">UI Component Library</div>
