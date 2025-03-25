e1fa067d270ed7d2dfbd0596dcfcd6c070f484a4
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-25 23:40:41 +0300

    Update ViteConfigDoc.vue

--- apps/volt/doc/vite/ViteConfigDoc.vue
+++ apps/volt/doc/vite/ViteConfigDoc.vue
@@ -1,8 +1,8 @@
 <template>
     <DocSectionText v-bind="$attrs">
         <p>
-            Configure two aliases to simplify the import process of Volt components. The first alias <i>@</i> is used to refer to a source folder in your application where Volt components are located as a subfolder. Without this alias, tedious
-            relative paths like <i>../../</i> may be required. Second alias removes to need to add <i>index.vue</i> in components paths such as <i>@/volt/{component}/index.vue</i> so that the path becomes <i>@/volt/{components}</i>.
+            Configure two aliases to simplify the import process of Volt components. The <i>@</i> alias is used to refer to a source folder in your application where Volt components are located as a subfolder. Without this alias, tedious relative
+            paths like <i>../../</i> may be required. Other aliases removes to need to add <i>*.vue</i> suffix in components paths such as <i>@/volt/{component}/index.vue</i> so that the path becomes <i>@/volt/{components}</i>.
         </p>
     </DocSectionText>
     <DocSectionCode :code="code" lang="script" />
