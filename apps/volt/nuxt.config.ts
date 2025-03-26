2bacf25c26e3ecd5a0eead795f79cd9898a1faf8
Cagatay Civici <cagatay.civici@gmail.com>
2025-03-26 16:11:21 +0300

    Temporary check disabled

--- apps/volt/nuxt.config.ts
+++ apps/volt/nuxt.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     typescript: {
-        typeCheck: true
+        typeCheck: false
     },
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
