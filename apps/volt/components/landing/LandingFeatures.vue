c5b72a0dac0601035421bf00c45b58a91a06ed07
atakan tepe <tepe_atakan@outlook.com>
2025-03-26 10:40:41 +0300

    Update LandingFeatures.vue

--- apps/volt/components/landing/LandingFeatures.vue
+++ apps/volt/components/landing/LandingFeatures.vue
@@ -10,7 +10,7 @@
                     <div class="h-full w-full border-x border-surface-200 dark:border-surface-800 block"></div>
                 </div>
                 <div class="p-4 border border-surface-200 dark:border-surface-800 w-full">
-                    <div class="p-6 shadow-lg border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900">
+                    <div class="p-6 shadow-lg border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900 min-h-44">
                         <div class="flex items-center gap-2 mb-4">
                             <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md h-8 w-8 inline-flex items-center justify-center">
                                 <i class="pi pi-star" />
@@ -20,7 +20,7 @@
                         <span class="text-muted-color">A comprehesive UI component suite crafted to meet all your requirements.</span>
                     </div>
                 </div>
-                <div class="h-40 px-6 w-full hidden lg:block">
+                <div class="flex-1 px-6 w-full hidden lg:block">
                     <div class="h-full w-full border-x border-surface-200 dark:border-surface-800 block"></div>
                 </div>
             </div>
@@ -30,7 +30,7 @@
                     <div class="h-full w-full border-x border-surface-200 dark:border-surface-800 block"></div>
                 </div>
                 <div class="p-4 border border-surface-200 dark:border-surface-800 w-full">
-                    <div class="p-6 shadow-lg border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900">
+                    <div class="p-6 shadow-lg border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900 min-h-44">
                         <div class="flex items-center gap-2 mb-4">
                             <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md h-8 w-8 inline-flex items-center justify-center">
                                 <i class="pi pi-prime" />
@@ -40,7 +40,7 @@
                         <span class="text-muted-color">Powered by the Unstyled PrimeVue components with advanced customization options.</span>
                     </div>
                 </div>
-                <div class="h-6 lg:h-40 px-6 w-full hidden lg:block">
+                <div class="h-6 lg:h-auto lg:flex-1 px-6 w-full hidden lg:block">
                     <div class="h-full w-full border-x border-surface-200 dark:border-surface-800 block"></div>
                 </div>
             </div>
@@ -50,7 +50,7 @@
                     <div class="h-full w-full border-x border-surface-200 dark:border-surface-800 block"></div>
                 </div>
                 <div class="p-4 border border-surface-200 dark:border-surface-800 w-full">
-                    <div class="p-6 shadow-lg border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900">
+                    <div class="p-6 shadow-lg border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900 min-h-44">
                         <div class="flex items-center gap-2 mb-4">
                             <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md h-8 w-8 inline-flex items-center justify-center">
                                 <i class="pi pi-palette" />
@@ -60,7 +60,7 @@
                         <span class="text-muted-color">Take full control over component styling with the convenience of the Tailwind v4 CSS utilities.</span>
                     </div>
                 </div>
-                <div class="h-6 lg:h-40 px-6 w-full hidden lg:block">
+                <div class="h-6 hidden lg:h-40 px-6 w-full md:block">
                     <div class="h-full w-full border-x border-surface-200 dark:border-surface-800 block"></div>
                 </div>
             </div>
@@ -70,7 +70,7 @@
                     <div class="h-full w-full border-x border-surface-200 dark:border-surface-800 block"></div>
                 </div>
                 <div class="p-4 border border-surface-200 dark:border-surface-800 w-full">
-                    <div class="p-6 shadow-lg border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900">
+                    <div class="p-6 shadow-lg border border-transparent dark:border-surface-800 bg-surface-0 dark:bg-surface-900 min-h-44">
                         <div class="flex items-center gap-2 mb-4">
                             <span class="bg-primary-100 text-primary-950 dark:bg-primary-400/30 dark:text-primary-100 rounded-md h-8 w-8 inline-flex items-center justify-center">
                                 <i class="pi pi-check" />
@@ -80,7 +80,7 @@
                         <span class="text-muted-color">Designed with accessibility in mind featuring WCAG AA compliance.</span>
                     </div>
                 </div>
-                <div class="h-40 px-6 w-full">
+                <div class="h-6 lg:h-auto lg:flex-1 px-6 w-full">
                     <div class="h-full w-full border-x border-surface-200 dark:border-surface-800 block"></div>
                 </div>
             </div>
