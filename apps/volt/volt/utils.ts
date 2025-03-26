7e43f0b78de34c2d74efa2912c8666b52df475dd
Mert Sincan <sincan.mert@gmail.com>
2025-03-26 13:20:11 +0000

    Update utils.ts

--- apps/volt/volt/utils.ts
+++ apps/volt/volt/utils.ts
@@ -1,7 +1,7 @@
 import { twMerge } from 'tailwind-merge';
 import { mergeProps } from 'vue';
 
-export const ptViewMerge = (globalPTProps = {}, selfPTProps = {}, datasets) => {
+export const ptViewMerge = (globalPTProps = {} as any, selfPTProps = {} as any, datasets: any) => {
     const { class: globalClass, ...globalRest } = globalPTProps;
     const { class: selfClass, ...selfRest } = selfPTProps;
 
