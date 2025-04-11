734bbb0134c9fc7e917cbed9ad573122a7cbf083
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-04-11 14:32:48 +0300

    Typo fix

--- apps/showcase/doc/skeleton/ListDoc.vue
+++ apps/showcase/doc/skeleton/ListDoc.vue
@@ -1,6 +1,6 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p>Sample List implementation using different Skeleton components and Tailwind CSS utilities.</p>
+        <p>Sample list implementation using different Skeleton components and Tailwind CSS utilities.</p>
     </DocSectionText>
     <div class="card">
         <div class="rounded border border-surface-200 dark:border-surface-700 p-6 bg-surface-0 dark:bg-surface-900">
