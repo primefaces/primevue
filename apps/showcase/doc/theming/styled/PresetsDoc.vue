0bd665e7975d6a55bb630eec1174c433f39021ac
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-06-30 13:50:27 +0300

    chore: Link fix

--- apps/showcase/doc/theming/styled/PresetsDoc.vue
+++ apps/showcase/doc/theming/styled/PresetsDoc.vue
@@ -3,7 +3,7 @@
         <p>
             Aura, Material, Lara and Nora are the available built-in options, created to demonstrate the power of the design-agnostic theming. Aura is PrimeTek's own vision, Material follows Google Material Design v2, Lara is based on Bootstrap and
             Nora is inspired by enterprise applications. Visit the
-            <a href="https://github.com/primefaces/primevue/tree/master/packages/themes/src/presets" target="_blank" rel="noopener noreferrer">source code</a> to learn more about the structure of presets. You may use them out of the box with
+            <a href="https://github.com/primefaces/primeuix/tree/master/packages/themes/src/presets" target="_blank" rel="noopener noreferrer">source code</a> to learn more about the structure of presets. You may use them out of the box with
             modifications or utilize them as reference in case you need to build your own presets from scratch.
         </p>
     </DocSectionText>
