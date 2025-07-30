fb8ef40f0d4330eb4684d99eba5eb983e54184c1
jeromeabel <j.abel@allo-media.fr>
2025-07-30 17:24:27 +0200

    fix(textarea): improve autoResize behavior by refining height adjustment logic #4189

--- apps/showcase/doc/textarea/AutoResizeDoc.vue
+++ apps/showcase/doc/textarea/AutoResizeDoc.vue
@@ -3,7 +3,7 @@
         <p>When <i>autoResize</i> is enabled, textarea grows instead of displaying a scrollbar.</p>
     </DocSectionText>
     <div class="card flex justify-center">
-        <Textarea v-model="value" autoResize />
+        <Textarea v-model="value" autoResize style="width: 285px" />
     </div>
     <DocSectionCode :code="code" />
 </template>
@@ -15,12 +15,12 @@ export default {
             value: '',
             code: {
                 basic: `
-<Textarea v-model="value" autoResize />
+<Textarea v-model="value" autoResize style="width:285px" />
 `,
                 options: `
 <template>
     <div class="card flex justify-center">
-        <Textarea v-model="value" autoResize />
+        <Textarea v-model="value" autoResize style="width:285px"/>
     </div>
 </template>
 
@@ -37,7 +37,7 @@ export default {
                 composition: `
 <template>
     <div class="card flex justify-center">
-        <Textarea v-model="value" autoResize />
+        <Textarea v-model="value" autoResize style="width:285px" />
     </div>
 </template>
 
