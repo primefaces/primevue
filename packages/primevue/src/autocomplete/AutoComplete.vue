f3f4af33ea344779a5d9d27288bff674004d45a9
NullUserException <dankuang@gmail.com>
2025-09-15 17:37:00 -0600

    Fix selection of multiple items

--- packages/primevue/src/autocomplete/AutoComplete.vue
+++ packages/primevue/src/autocomplete/AutoComplete.vue
@@ -592,9 +592,10 @@ export default {
                 const value = this.visibleOptions
                     .slice(rangeStart, rangeEnd + 1)
                     .filter((option) => this.isValidOption(option))
+                    .filter((option) => !this.isSelected(option))                    
                     .map((option) => this.getOptionValue(option));
 
-                this.updateModel(event, value);
+                this.updateModel(event, [...(this.d_value || []), ...value]);
             }
         },
         onOverlayClick(event) {
