8a8846a3f19e43097b91cada24ca99c8fff422f7
Cagatay Civici <cagatay.civici@gmail.com>
2025-10-03 18:21:29 +0300

    add templating demo

--- apps/showcase/doc/datepicker/ButtonBarDoc.vue
+++ apps/showcase/doc/datepicker/ButtonBarDoc.vue
@@ -1,9 +1,23 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p>When <i>showButtonBar</i> is present, today and clear buttons are displayed at the footer.</p>
+        <p>When <i>showButtonBar</i> is present, today and clear buttons are displayed at the footer. The content can be fully customized with the <i>buttonbar</i> slot as well.</p>
     </DocSectionText>
-    <div class="card flex justify-center">
-        <DatePicker v-model="date" showButtonBar />
+    <div class="card flex justify-center gap-4 flex-wrap">
+        <DatePicker v-model="date" showButtonBar placeholder="Basic" />
+        <DatePicker v-model="dates" showButtonBar placeholder="Customized" selectionMode="range" :manualInput="false">
+            <template #buttonbar="{ todayCallback, clearCallback }">
+                <div class="flex justify-between w-full">
+                    <div class="flex gap-2">
+                        <Button size="small" label="Exact" severity="secondary" />
+                        <Button size="small" label="Flexible" severity="secondary" />
+                    </div>
+                    <div class="flex gap-2">
+                        <Button size="small" label="Today" @click="todayCallback" variant="outlined" />
+                        <Button size="small" icon="pi pi-times" severity="secondary" variant="outlined" @click="clearCallback" />
+                    </div>
+                </div>
+            </template>
+        </DatePicker>
     </div>
     <DocSectionCode :code="code" />
 </template>
@@ -13,14 +27,43 @@ export default {
     data() {
         return {
             date: null,
+            dates: null,
             code: {
                 basic: `
-<DatePicker v-model="date" showButtonBar />
+<DatePicker v-model="date" showButtonBar placeholder="Basic" />
+<DatePicker v-model="dates" showButtonBar placeholder="Customized" selectionMode="range" :manualInput="false">
+    <template #buttonbar="{ todayCallback, clearCallback }">
+        <div class="flex justify-between w-full">
+            <div class="flex gap-2">
+                <Button size="small" label="Exact" severity="secondary" />
+                <Button size="small" label="Flexible" severity="secondary" />
+            </div>
+            <div class="flex gap-2">
+                <Button size="small" label="Today" @click="todayCallback" variant="outlined" />
+                <Button size="small" icon="pi pi-times" severity="secondary" variant="outlined" @click="clearCallback" />
+            </div>
+        </div>
+    </template>
+</DatePicker>
 `,
                 options: `
 <template>
-    <div class="card flex justify-center">
-        <DatePicker v-model="date" showButtonBar />
+    <div class="card flex justify-center gap-4 flex-wrap">
+        <DatePicker v-model="date" showButtonBar placeholder="Basic" />
+        <DatePicker v-model="dates" showButtonBar placeholder="Customized" selectionMode="range" :manualInput="false">
+            <template #buttonbar="{ todayCallback, clearCallback }">
+                <div class="flex justify-between w-full">
+                    <div class="flex gap-2">
+                        <Button size="small" label="Exact" severity="secondary" />
+                        <Button size="small" label="Flexible" severity="secondary" />
+                    </div>
+                    <div class="flex gap-2">
+                        <Button size="small" label="Today" @click="todayCallback" variant="outlined" />
+                        <Button size="small" icon="pi pi-times" severity="secondary" variant="outlined" @click="clearCallback" />
+                    </div>
+                </div>
+            </template>
+        </DatePicker>
     </div>
 </template>
 
@@ -28,7 +71,8 @@ export default {
 export default {
     data() {
         return {
-            date: null
+            date: null,
+            dates: null
         };
     }
 };
@@ -36,8 +80,22 @@ export default {
 `,
                 composition: `
 <template>
-    <div class="card flex justify-center">
-        <DatePicker v-model="date" showButtonBar />
+    <div class="card flex justify-center gap-4 flex-wrap">
+        <DatePicker v-model="date" showButtonBar placeholder="Basic" />
+        <DatePicker v-model="dates" showButtonBar placeholder="Customized" selectionMode="range" :manualInput="false">
+            <template #buttonbar="{ todayCallback, clearCallback }">
+                <div class="flex justify-between w-full">
+                    <div class="flex gap-2">
+                        <Button size="small" label="Exact" severity="secondary" />
+                        <Button size="small" label="Flexible" severity="secondary" />
+                    </div>
+                    <div class="flex gap-2">
+                        <Button size="small" label="Today" @click="todayCallback" variant="outlined" />
+                        <Button size="small" icon="pi pi-times" severity="secondary" variant="outlined" @click="clearCallback" />
+                    </div>
+                </div>
+            </template>
+        </DatePicker>
     </div>
 </template>
 
@@ -45,6 +103,7 @@ export default {
 import { ref } from "vue";
 
 const date = ref();
+const dates = ref();
 <\/script>
 `
             }
