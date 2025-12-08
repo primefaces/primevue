f5b2d6ae7a933c77b6c8dc0cfb9f2f9d9ac0ca82
İlyas Özkurt <ilyasozkurt@gmail.com>
2025-12-08 16:58:25 +0300

    AllowEmptyDoc components added to git

---
+++ apps/showcase/doc/listbox/AllowEmptyDoc.vue
@@ -0,0 +1,77 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Use the <i>allowEmpty</i> prop to control whether the selection can be cleared. Set <i>:allowEmpty="false"</i> to prevent clearing the current selection.</p>
+    </DocSectionText>
+    <div class="card flex flex-col md:flex-row gap-4 justify-center">
+        <div class="flex justify-center">
+            <Listbox v-model="selectedCity" :options="cities" optionLabel="name" :allowEmpty="false" class="w-full md:w-56" />
+        </div>
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedCity: { name: 'Rome', code: 'RM' },
+            cities: [
+                { name: 'New York', code: 'NY' },
+                { name: 'Rome', code: 'RM' },
+                { name: 'London', code: 'LDN' },
+                { name: 'Istanbul', code: 'IST' },
+                { name: 'Paris', code: 'PRS' }
+            ],
+            code: {
+                basic: `
+<Listbox v-model="selectedCity" :options="cities" optionLabel="name" :allowEmpty="false" class="w-full md:w-56" />
+`,
+                options: `
+<template>
+    <div class="card flex justify-center">
+        <Listbox v-model="selectedCity" :options="cities" optionLabel="name" :allowEmpty="false" class="w-full md:w-56" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedCity: { name: 'Rome', code: 'RM' },
+            cities: [
+                { name: 'New York', code: 'NY' },
+                { name: 'Rome', code: 'RM' },
+                { name: 'London', code: 'LDN' },
+                { name: 'Istanbul', code: 'IST' },
+                { name: 'Paris', code: 'PRS' }
+            ]
+        };
+    }
+};
+<\/script>
+`,
+                composition: `
+<template>
+    <div class="card flex justify-center">
+        <Listbox v-model="selectedCity" :options="cities" optionLabel="name" :allowEmpty="false" class="w-full md:w-56" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const selectedCity = ref({ name: 'Rome', code: 'RM' });
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+]);
+<\/script>
+`
+            }
+        };
+    }
+};
+</script>
---
+++ apps/volt/doc/listbox/AllowEmptyDoc.vue
@@ -0,0 +1,48 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Use the <i>allowEmpty</i> prop to control if selection can be cleared. Set <i>:allowEmpty="false"</i> to disallow clearing the current selection.</p>
+    </DocSectionText>
+    <div class="card flex justify-center">
+        <Listbox v-model="selectedCity" :options="cities" optionLabel="name" :allowEmpty="false" class="w-full md:w-56" />
+    </div>
+    <DocSectionCode :code="code" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const selectedCity = ref({ name: 'Rome', code: 'RM' });
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+]);
+
+const code = {
+    basic: `
+<Listbox v-model="selectedCity" :options="cities" optionLabel="name" :allowEmpty="false" class="w-full md:w-56" />
+`,
+    composition: `
+<template>
+    <div class="card flex justify-center">
+        <Listbox v-model="selectedCity" :options="cities" optionLabel="name" :allowEmpty="false" class="w-full md:w-56" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const selectedCity = ref({ name: 'Rome', code: 'RM' });
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+]);
+<\/script>
+`
+};
+</script>
