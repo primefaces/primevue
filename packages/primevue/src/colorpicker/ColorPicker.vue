bb9c5860fa2e81e1a6b928752b274202b87a65f9
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-12-10 09:35:17 +0300

    Fixed #8323 - ColorPicker: zIndex defect

--- packages/primevue/src/colorpicker/ColorPicker.vue
+++ packages/primevue/src/colorpicker/ColorPicker.vue
@@ -358,7 +358,7 @@ export default {
             this.bindResizeListener();
 
             if (this.autoZIndex) {
-                ZIndex.set('overlay', el, this.baseZIndex, this.$primevue.config.zIndex.overlay);
+                ZIndex.set('overlay', el, this.baseZIndex + this.$primevue.config.zIndex.overlay);
             }
 
             // Issue: #7508
