450da03ec6dc481cd49787b14ce83872ca3fbe83
Atakan <tepe_atakan@outlook.com>
2025-12-15 12:20:18 +0300

    update

--- apps/showcase/server/api/upload.js
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(() => {
-    return {
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        statusCode: 200,
-        statusMessage: 'Fake Upload Process'
-    };
-});
