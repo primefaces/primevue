73443e53fc6430d56806067cf4990211fbf9f09d
antek <antek.olszewski01@gmail.com>
2026-01-26 21:07:58 +0100

    fix(InputOTP): prevent paste when readonly

--- packages/primevue/src/inputotp/InputOtp.vue
+++ packages/primevue/src/inputotp/InputOtp.vue
@@ -175,6 +175,10 @@ export default {
             }
         },
         onPaste(event) {
+            if(this.readonly) {
+                return;
+            }
+
             let paste = event.clipboardData.getData('text');
 
             if (paste.length) {
