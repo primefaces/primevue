f0e98be08ab647b2675f4d338b9af06524c79de6
Cagatay Civici <cagatay.civici@gmail.com>
2026-05-11 20:42:12 +0300

    chore: remove sales

--- apps/showcase/pages/index.vue
+++ apps/showcase/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="containerClass">
-        <AppNews />
+        <!--<AppNews />-->
         <AppTopBar :showMenuButton="false" />
         <AppDesigner />
         <HeroSection />
